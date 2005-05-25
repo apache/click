bf6b6b5b90ece16c0be7eedec74c28d7a07a38ca
malcolm_edgar <dev-null@apache.org>
2005-05-25 12:13:34 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@714381 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/click/Context.java
+++ framework/src/net/sf/click/Context.java
@@ -112,7 +112,7 @@ public class Context {
     /**
      * Return the page resouce path from the request. For example:
      * <pre class="codeHtml">
-     * <span class="blue">http://www.mycorp.com/banking/secure/login.html</span>  ->  <span class="red">secure/login.html</span> </pre>
+     * <span class="blue">http://www.mycorp.com/banking/secure/login.htm</span>  ->  <span class="red">secure/login.htm</span> </pre>
      *
      * @return the page resource path from the request
      */
