3828f01f5376443562e4e487b39a88386667d0b4
malcolm_edgar <dev-null@apache.org>
2005-08-09 12:42:33 +0000

    Javadoc changes, & version id
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@714459 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/click/panel/TabbedPanel.java
+++ framework/src/net/sf/click/panel/TabbedPanel.java
@@ -12,12 +12,12 @@ import net.sf.click.util.ClickUtils;
  * Additionally, a listener may be attached (similar to the control listeners)
  * that will be called on tab switch. This could be useful to load (or reload)
  * model related information for which ever panel is selected by the user.
- * <p/>
+ *
  * <pre class="codeHtml">
  * &lt;div id='$_tp_id'&gt;
  * &lt;table class="tp_tab"&gt;
  * &lt;tr class="tp_tab"&gt;
- * #foreach( $panel in $_tp_panels )
+ * #foreach ($panel in $_tp_panels)
  * #if( $panel.id == $_tp_activePanel.id )
  * &lt;td class="tp_tab_on"&gt;
  * $panel.label
@@ -41,7 +41,7 @@ import net.sf.click.util.ClickUtils;
  * &lt;/table&gt;
  * &lt;/div&gt;
  * </pre>
- * <p/>
+ *
  * As shown above, the the model context variables associated with a tabbed
  * panel are as follows:
  * <ul>
@@ -55,6 +55,7 @@ import net.sf.click.util.ClickUtils;
  * Also, as show above, there are a number of CSS attributes that allow some
  * customization of the output.  These are as follows (these are the exact
  * colors show in the example for tabbed panels):
+ *
  * <pre class="codeCss">
  * table.tp_tab { border-collapse: collapse; }
  * tr.tp_tab { }
@@ -62,160 +63,158 @@ import net.sf.click.util.ClickUtils;
  * td.tp_tab_off { background: #cccccc; color: #000000; border-left: 1px solid #336699; border-top: 1px solid #336699; border-right: 1px solid #336699; padding: 5px; }
  * table.tp_content { border: 1px solid #336699; }
  * tr.tp_content { }
- * td.tp_content { background: #efefef; }
- * <pre>
+ * td.tp_content { background: #efefef; } </pre>
  *
  * @author Phil Barnes
- * @since Jul 4, 2005 @ 3:59:49 PM
+ * @version $Id$
  */
 public class TabbedPanel extends BasicPanel {
-	/** The logger associated with this tabbed panel. */
-	protected static ClickLogger log = new ClickLogger();
 
-	private Object listener;
-	private String method;
-	private ActionLink tabActionLink;
+    /** The logger associated with this tabbed panel. */
+    protected static ClickLogger log = new ClickLogger();
 
-	public TabbedPanel(String id) {
-		super(id);
-		addModel("_tp_id", id);
-	}
+    private Object listener;
+    private String method;
+    private ActionLink tabActionLink;
 
-	/**
-	 * <b>NOTE:</b>  This method should <b>not</b> be called, in favor of
-	 * addPanel(Panel, boolean), to ensure the "active" panel is defined.  This
-	 * method will by default set the active panel to the current panel being
-	 * passed, to ensure that at least one active panel has been set.
-	 *
-	 * @param panel
-	 */
-	public void addPanel(Panel panel) {
-		log.warn("WARNING: TabbedPanel should have panels added ONLY via" +
-				" addPanel(panel, boolean) to ensure the active panel" +
-				" is correctly set.  Setting active panel to *this* panel ('" +
-				panel.getName() + "') as a result.");
-		this.addPanel(panel, true);
-	}
+    public TabbedPanel(String id) {
+        super(id);
+        addModel("_tp_id", id);
+    }
 
-	/**
-	 * Add's a "sub-panel" that will be rendered in a tab if the passed boolean
-	 * is true.  Otherwise, only the panel name and an associated link will be
-	 * added.  For all panels, an actionLink will be added that has a listener
-	 * (handleTabSwitch) noted in this class that will handle the cycling
-	 * through the various panels according to the clicked link.
-	 *
-	 * @param panel
-	 * @param isActivePanel
-	 */
-	public void addPanel(Panel panel, boolean isActivePanel) {
-		super.addPanel(panel);
-		if (isActivePanel) {
-			if (log.isDebugEnabled()) {
-				log.debug("Adding panel with id " + panel.getId() +
-						" as the active panel.");
-			}
-			setActivePanel(panel);
-		}
-		if (getPanels().size() > 1 && tabActionLink == null) {
-			if (log.isDebugEnabled()) {
-				log.debug("Two or more panels detected, enabling " +
-						"tabActionLink. Current listener status = " +
-						this.listener + "." + this.method + "()");
-			}
-			tabActionLink = new ActionLink("_tp_tabLink");
-			tabActionLink.setListener(this, "handleTabSwitch");
-			addControl(tabActionLink);
-			addModel("_tp_panels", getPanels());
-		}
-	}
+    /**
+     * <b>NOTE:</b> This method should <b>not</b> be called, in favor of
+     * addPanel(Panel, boolean), to ensure the "active" panel is defined.  This
+     * method will by default set the active panel to the current panel being
+     * passed, to ensure that at least one active panel has been set.
+     *
+     * @param panel
+     */
+    public void addPanel(Panel panel) {
+        log.warn("WARNING: TabbedPanel should have panels added ONLY via" +
+                " addPanel(panel, boolean) to ensure the active panel" +
+                " is correctly set.  Setting active panel to *this* panel ('" +
+                panel.getName() + "') as a result.");
+        this.addPanel(panel, true);
+    }
 
+    /**
+     * Add's a "sub-panel" that will be rendered in a tab if the passed boolean
+     * is true.  Otherwise, only the panel name and an associated link will be
+     * added.  For all panels, an actionLink will be added that has a listener
+     * (handleTabSwitch) noted in this class that will handle the cycling
+     * through the various panels according to the clicked link.
+     *
+     * @param panel
+     * @param isActivePanel
+     */
+    public void addPanel(Panel panel, boolean isActivePanel) {
+        super.addPanel(panel);
+        if (isActivePanel) {
+            if (log.isDebugEnabled()) {
+                log.debug("Adding panel with id " + panel.getId() +
+                          " as the active panel.");
+            }
+            setActivePanel(panel);
+        }
+        if (getPanels().size() > 1 && tabActionLink == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Two or more panels detected, enabling " +
+                          "tabActionLink. Current listener status = " +
+                          this.listener + "." + this.method + "()");
+            }
+            tabActionLink = new ActionLink("_tp_tabLink");
+            tabActionLink.setListener(this, "handleTabSwitch");
+            addControl(tabActionLink);
+            addModel("_tp_panels", getPanels());
+        }
+    }
 
-	/**
-	 * Set the controls event listener.  If the listener <b>and</b> method are
-	 * non-null, then the listener will be called whenever a request to switch
-	 * tabs is placed by clicking the link associated with that tab.
-	 * <p/>
-	 * The method signature of the listener is:<ul>
-	 * <li>must hava a valid Java method name</li>
-	 * <li>takes no arguments</li>
-	 * <li>returns a boolean value</li>
-	 * </ul>
-	 * <p/>
-	 * An example event listener method would be:
-	 * <p/>
-	 * <pre class="codeJava">
-	 * <span class="kw">public boolean</span> onClick() {
-	 * System.out.println(<span class="st">"onClick called"</span>);
-	 * <span class="kw">return true</span>;
-	 * } </pre>
-	 *
-	 * @param listener the listener object with the named method to invoke
-	 * @param method   the name of the method to invoke
-	 */
-	public void setListener(Object listener, String method) {
-		this.listener = listener;
-		this.method = method;
-	}
+    /**
+     * Set the controls event listener.  If the listener <b>and</b> method are
+     * non-null, then the listener will be called whenever a request to switch
+     * tabs is placed by clicking the link associated with that tab.
+     * <p/>
+     * The method signature of the listener is:<ul>
+     * <li>must hava a valid Java method name</li>
+     * <li>takes no arguments</li>
+     * <li>returns a boolean value</li>
+     * </ul>
+     * <p/>
+     * An example event listener method would be:
+     *
+     * <pre class="codeJava">
+     * <span class="kw">public boolean</span> onClick() {
+     * System.out.println(<span class="st">"onClick called"</span>);
+     * <span class="kw">return true</span>;
+     * } </pre>
+     *
+     * @param listener the listener object with the named method to invoke
+     * @param method   the name of the method to invoke
+     */
+    public void setListener(Object listener, String method) {
+        this.listener = listener;
+        this.method = method;
+    }
 
-	public void setPage(Page page) {
-		super.setPage(page);
-		// add the context to the tabActionLink control - TODO: is there a better way to do set the context?
-		tabActionLink.setContext(page.getContext());
-	}
+    public void setPage(Page page) {
+        super.setPage(page);
+        // add the context to the tabActionLink control - TODO: is there a better way to do set the context?
+        tabActionLink.setContext(page.getContext());
+    }
 
-	/**
-	 * Handle the switching between tabs.  This method will invoke the
-	 * listener.method() as set by {@link #setListener(Object, String)} if
-	 * available, otherwise will just continue processing, therefore
-	 * assume that all the information needed to "switch tabs" is already
-	 * available to the model.
-	 *
-	 * @return true if processing should continue, false if it should halt
-	 */
-	public boolean handleTabSwitch() {
+    /**
+     * Handle the switching between tabs.  This method will invoke the
+     * listener.method() as set by {@link #setListener(Object, String)} if
+     * available, otherwise will just continue processing, therefore
+     * assume that all the information needed to "switch tabs" is already
+     * available to the model.
+     *
+     * @return true if processing should continue, false if it should halt
+     */
+    public boolean handleTabSwitch() {
+        for (int i = 0; i < panels.size(); i++) {
+            Panel panel = (Panel) panels.get(i);
+            if (tabActionLink.getValue().equals(panel.getName())) {
+                setActivePanel(panel);
+            }
+        }
 
-		for (int i = 0; i < panels.size(); i++) {
-			Panel panel = (Panel) panels.get(i);
-			if (tabActionLink.getValue().equals(panel.getName())) {
-				setActivePanel(panel);
-			}
-		}
-		// if a listener has been explicitely set to handle a tab switch,
-		// then invoke it
-		if (this.listener != null && this.method != null) {
-			log.debug(
-					"Invoking listener " + this.listener + "." + this.method + "()");
-			return ClickUtils.invokeListener(listener, method);
-		}
-		// this implies that everything needed to render the next tab has been
-		// added to the "model" context already
-		else {
-			log.debug("No listener.method() found, continuing processing...");
-			return true;
-		}
-	}
+        // if a listener has been explicitely set to handle a tab switch,
+        // then invoke it
+        if (this.listener != null && this.method != null) {
+            log.debug("Invoking listener " + this.listener + "." + this.method + "()");
+            return ClickUtils.invokeListener(listener, method);
+        }
+        // this implies that everything needed to render the next tab has been
+        // added to the "model" context already
+        else {
+            log.debug("No listener.method() found, continuing processing...");
+            return true;
+        }
+    }
 
-	/**
-	 * Removes the current 'active panel' from the model, and adds the passed
-	 * panel as the new active panel.
-	 *
-	 * @param panel
-	 */
-	private void setActivePanel(Panel panel) {
-		// remove the existing 'active panel'
-		removeModel("_tp_activePanel");
-		// add the passed in panel as the 'new' active panel
-		addModel("_tp_activePanel", panel);
-	}
+    /**
+     * Removes the current 'active panel' from the model, and adds the passed
+     * panel as the new active panel.
+     *
+     * @param panel
+     */
+    private void setActivePanel(Panel panel) {
+        // remove the existing 'active panel'
+        removeModel("_tp_activePanel");
+        // add the passed in panel as the 'new' active panel
+        addModel("_tp_activePanel", panel);
+    }
 
-	/**
-	 * Overridden toString to add the path '/click/' to the beginning of the
-	 * template name, as the template file will be copied to this directory
-	 * upon first time start of ClickServlet
-	 *
-	 * @return the default template path and name of '/click/ListPanel.htm'
-	 */
-	public String toString() {
-		return "/click/" + super.toString();
-	}
+    /**
+     * Overridden toString to add the path '/click/' to the beginning of the
+     * template name, as the template file will be copied to this directory
+     * upon first time start of ClickServlet
+     *
+     * @return the default template path and name of '/click/ListPanel.htm'
+     */
+    public String toString() {
+        return "/click/" + super.toString();
+    }
 }
