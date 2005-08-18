23bbe8820e2af5c778fac7acfc86e53e225fbb47
pbarnes <pbarnes@unknown>
2005-08-18 13:54:07 +0000

    LiveGrid example additions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@714486 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webapps/click-examples/WEB-INF/src/examples/page/ajax/AjaxLiveGrid.java
@@ -0,0 +1,27 @@
+package examples.page.ajax;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import examples.domain.CustomerDAO;
+import examples.page.BorderedPage;
+
+/**
+ * Provides an Ajax demo of Rico's LiveGrid.
+ *
+ * @author Malcolm Edgar
+ */
+public class AjaxLiveGrid extends BorderedPage {
+
+	public void onInit() {
+//		addModel("head-include", "ajax/ajax-head.htm");
+		addModel("body-onload", "javascript:bodyOnLoad();");
+
+		List customerList = CustomerDAO.getAllCustomers();
+		addModel("customers", customerList);
+		addModel("totalRows",new Integer(customerList.size()));
+		// always start at a 0 offset
+		addModel("offset",new Integer(0));
+	}
+
+}
