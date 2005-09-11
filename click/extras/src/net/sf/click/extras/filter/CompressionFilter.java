a081f71f46b50d90aa12d4077b9412ac78a46be6
malcolm_edgar <dev-null@apache.org>
2005-09-11 11:01:25 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@714505 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/extras/src/net/sf/click/extras/filter/CompressionFilter.java
@@ -0,0 +1,232 @@
+/*
+* Copyright 2004 The Apache Software Foundation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package net.sf.click.extras.filter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * Provides a GZIP compression <tt>Filter</tt> to compress
+ * HTML ServletResponse content if it is bigger than a threshold.
+ * <p/>
+ * This package is derived from the Jakarta Tomcat examples compression filter
+ * and is packaged in Click Extras for convenience.
+ * <p/>
+ * To configure your application to GZIP compress HTML content include the
+ * click-extras.jar in you application and add the following filter elements to 
+ * your <tt>/WEB-INF/web.xml</tt> file:
+ * <pre class="codeConfig"/>
+ * &lt;filter&gt;
+ *  &lt;filter-name&gt;compression-filter&lt;/filter-name&gt;
+ *  &lt;filter-class&gt;net.sf.click.extras.filter.CompressionFilter&lt;/filter-class&gt;
+ * &lt;/filter&gt;
+ *
+ * &lt;filter-mapping&gt;
+ *  &lt;filter-name&gt;compression-filter&lt;/filter-name&gt;
+ *  &lt;servlet-name&gt;click-servlet&lt;/servlet-name&gt;
+ * &lt;/filter-mapping&gt; 
+ * 
+ * &lt;servlet&gt;
+ *  &lt;servlet-name&gt;click-servlet&lt;/servlet-name&gt;
+ * .. </pre>
+ *
+ * @author Amy Roh
+ * @author Dmitri Valdin
+ * @version Revision: 1.2, Date: 2004/03/18 16:40:28
+ */
+public class CompressionFilter implements Filter {
+
+    /** The class logger. */
+    private final Log log = LogFactory.getLog(getClass());
+
+    /**
+     * The filter configuration object we are associated with.  If this value
+     * is null, this filter instance is not currently configured.
+     */
+    private FilterConfig config = null;
+
+    /**
+     * Minimal reasonable threshold
+     */
+    private int minThreshold = 128;
+
+    /**
+     * The threshold number to compress
+     */
+    protected int compressionThreshold;
+
+    /**
+     * Place this filter into service.
+     *
+     * @param filterConfig The filter configuration object
+     */
+
+    public void init(FilterConfig filterConfig) {
+
+        config = filterConfig;
+        if (filterConfig != null) {
+
+            String str = filterConfig.getInitParameter("compressionThreshold");
+            if (str!=null) {
+                compressionThreshold = Integer.parseInt(str);
+                if (compressionThreshold != 0 && compressionThreshold < minThreshold) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("compressionThreshold should be either 0 - no compression or >= " + minThreshold);
+                        log.debug("compressionThreshold set to " + minThreshold);
+                    }
+                    compressionThreshold = minThreshold;
+                }
+            } else {
+                compressionThreshold = 0;
+            }
+
+        } else {
+            compressionThreshold = 0;
+        }
+
+    }
+
+    /**
+    * Take this filter out of service.
+    */
+    public void destroy() {
+
+        this.config = null;
+
+    }
+
+    /**
+     * The <code>doFilter</code> method of the Filter is called by the container
+     * each time a request/response pair is passed through the chain due
+     * to a client request for a resource at the end of the chain.
+     * The FilterChain passed into this method allows the Filter to pass on the
+     * request and response to the next entity in the chain.<p>
+     * This method first examines the request to check whether the client support
+     * compression. <br>
+     * It simply just pass the request and response if there is no support for
+     * compression.<br>
+     * If the compression support is available, it creates a
+     * CompressionServletResponseWrapper object which compresses the content and
+     * modifies the header if the content length is big enough.
+     * It then invokes the next entity in the chain using the FilterChain object
+     * (<code>chain.doFilter()</code>), <br>
+     **/
+
+    public void doFilter ( ServletRequest request, ServletResponse response,
+                        FilterChain chain ) throws IOException, ServletException {
+
+        if (log.isDebugEnabled()) {
+            log.debug("@doFilter");
+        }
+
+        if (compressionThreshold == 0) {
+            if (log.isDebugEnabled()) {
+                log.debug("doFilter gets called, but compressionTreshold is set to 0 - no compression");
+            }
+            chain.doFilter(request, response);
+            return;
+        }
+
+        boolean supportCompression = false;
+        if (request instanceof HttpServletRequest) {
+            if (log.isDebugEnabled()) {
+                log.debug("requestURI = " + ((HttpServletRequest)request).getRequestURI());
+            }
+
+            // Are we allowed to compress ?
+            String s = (String) ((HttpServletRequest)request).getParameter("gzip");
+            if ("false".equals(s)) {
+                if (log.isDebugEnabled()) {
+                    log.debug("got parameter gzip=false --> don't compress, just chain filter");
+                }
+                chain.doFilter(request, response);
+                return;
+            }
+
+            Enumeration e =
+                ((HttpServletRequest)request).getHeaders("Accept-Encoding");
+            while (e.hasMoreElements()) {
+                String name = (String)e.nextElement();
+                if (name.indexOf("gzip") != -1) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("supports compression");
+                    }
+                    supportCompression = true;
+                } else {
+                    if (log.isDebugEnabled()) {
+                        log.debug("no support for compresion");
+                    }
+                }
+            }
+        }
+
+        if (!supportCompression) {
+            if (log.isDebugEnabled()) {
+                log.debug("doFilter gets called wo compression");
+            }
+            chain.doFilter(request, response);
+            return;
+        } else {
+            if (response instanceof HttpServletResponse) {
+                CompressionServletResponseWrapper wrappedResponse =
+                    new CompressionServletResponseWrapper((HttpServletResponse)response);
+
+                wrappedResponse.setCompressionThreshold(compressionThreshold);
+                if (log.isDebugEnabled()) {
+                    log.debug("doFilter gets called with compression");
+                }
+                try {
+                    chain.doFilter(request, wrappedResponse);
+                } finally {
+                    wrappedResponse.finishResponse();
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * Set filter config
+     * This function is equivalent to init. Required by Weblogic 6.1
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        init(filterConfig);
+    }
+
+    /**
+     * Return filter config
+     * Required by Weblogic 6.1
+     */
+    public FilterConfig getFilterConfig() {
+        return config;
+    }
+
+}
+
