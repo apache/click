8d8c2ae487b1c0adff4e1909ace553fe9d73321d
malcolm_edgar <dev-null@apache.org>
2005-10-10 10:44:30 +0000

    fix fieldset form dao loading bug
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@714541 13f79535-47bb-0310-9956-ffa450edef68

--- framework/test/net/sf/click/util/ClickUtilsTest.java
+++ framework/test/net/sf/click/util/ClickUtilsTest.java
@@ -3,34 +3,79 @@ package net.sf.click.util;
 import junit.framework.TestCase;
 import net.sf.click.control.FieldSet;
 import net.sf.click.control.Form;
+import net.sf.click.control.IntegerField;
 import net.sf.click.control.TextField;
 
 public class ClickUtilsTest extends TestCase {
     
-  public void testCopyToWithFieldSet() {
-    String value = "hello";
+  public void testCopyFormToObject() {
+      
+    final String name = "hello";
+    final Integer id = new Integer(1234); 
 
     // set up the form
-    TextField name = new TextField("name");
-    name.setValue(value);
+    Form form = new Form("sample");
+    
+    IntegerField idField = new IntegerField("id");
+    idField.setValue(id);
+    form.add(idField);
     
     FieldSet fieldset = new FieldSet("fieldset");
-    fieldset.add(name);
-    
-    Form form = new Form("sample");
     form.add(fieldset);
-    form.setAttribute("name", value);
 
+    TextField nameField = new TextField("name");
+    nameField.setValue(name);
+    fieldset.add(nameField);
+    
     // copy form to object
-    SampleObject sample = new SampleObject();
-    ClickUtils.copyFormToObject(form, sample, true);
+    SampleObject sampleObject = new SampleObject();
+    ClickUtils.copyFormToObject(form, sampleObject, true);
 
     // has the object been configured correctly?
-    assertEquals(value, sample.getName());
+    assertEquals(idField.getInteger(), sampleObject.getId());
+    assertEquals(nameField.getValue(), sampleObject.getName());
+  }
+  
+  public void testCopyObjectToForm() {
+      
+      final String name = "hello";
+      final Integer id = new Integer(1234); 
+      
+      SampleObject sampleObject = new SampleObject();
+      sampleObject.setId(id);
+      sampleObject.setName(name);
+      
+      
+      // set up the form
+      Form form = new Form("sample");
+      
+      IntegerField idField = new IntegerField("id");
+      form.add(idField);
+      
+      FieldSet fieldset = new FieldSet("fieldset");
+      form.add(fieldset);
+      
+      TextField nameField = new TextField("name");
+      fieldset.add(nameField);
+      
+      // copy object to form
+      ClickUtils.copyObjectToForm(sampleObject, form, true);
+      
+      // has the object been configured correctly?
+      assertEquals(sampleObject.getId(), idField.getInteger());
+      assertEquals(sampleObject.getName(), nameField.getValue());
   }
   
   public static class SampleObject {
+      private Integer id;
       private String name;
+      
+      public Integer getId() {
+          return id;
+      }
+      public void setId(Integer id) {
+          this.id = id;
+      }
       public String getName() {
           return name;
       }
