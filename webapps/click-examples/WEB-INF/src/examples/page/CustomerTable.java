583848f72249591a514e47877e358d8b014e5a39
malcolm_edgar <dev-null@apache.org>
2005-10-11 13:25:07 +0000

    added JSP example
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@714546 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webapps/click-examples/WEB-INF/src/examples/page/CustomerTable.java
@@ -0,0 +1,23 @@
+package examples.page;
+
+import java.util.List;
+
+import net.sf.click.Page;
+import examples.domain.CustomerDAO;
+
+/**
+ * Provides JSP integration where a JSP page is used to render the results.
+ *
+ * @author Malcolm Edgar
+ */
+public class CustomerTable extends BorderedPage {
+
+    /**
+     * @see Page#onGet()
+     */
+    public void onGet() {
+        List customers = CustomerDAO.getCustomersSortedByName(10);
+        addModel("customers", customers);
+        setForward("customer-table.jsp");
+    }
+}
