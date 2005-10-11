1bef5bd289293e9a3f252c8a25df90422c045e8f
malcolm_edgar <dev-null@apache.org>
2005-10-11 13:20:34 +0000

    added Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@714542 13f79535-47bb-0310-9956-ffa450edef68

--- click/extras/src/net/sf/click/extras/spring/SpringClickServlet.java
+++ click/extras/src/net/sf/click/extras/spring/SpringClickServlet.java
@@ -25,15 +25,83 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Provides an example Spring framework integration <tt>ClickServlet</tt>.
+ * Provides an example Spring framework integration <tt>SpringClickServlet</tt>.
  * <p/>
+ * This specialized Click Servlet can inject Spring dependencies into
+ * defined spring Page beans. If a requested Page is not configured as a Spring
+ * bean, then a plain new Page instance is created.
+ * <p/>
+ * The SpringClickServlet overrides the ClickServlet method <tt>newPageInstance()</tt>
+ * to provide new Page instances:
  *
+ * <pre class="codeJava">
+ * <span class="kw">protected</span> Page newPageInstance(String path, Class pageClass) <span class="kw">throws</span> Exception {
+ *     String beanName = path.substring(0, path.indexOf(<span class="st">"."</span>));
+ *
+ *     <span class="kw">if</span> (beanFactory.containsBean(beanName)) {
+ *         <span class="kw">return</span> (Page) beanFactory.getBean(beanName);
+ *
+ *     } <span class="kw">else</span> {
+ *         <span class="kw">return</span> pageClass.newIntance();
+ *     }
+ * } </pre>
+ *
+ * Spring Page beans are defined in a Sring application context XML file.
+ * In this file Page bean id maps to the page request path excluding <tt>.htm</tt> extension:
+ *
+ * <pre class="codeConfig">
+ * &lt;beans&gt;
+ *   &lt;bean singleton="false" id="/customer-edit" class="com.mycorp.pages.CustomerEdit"
+ *         autowire="byName"/&gt;
+ *   ..
+ * &lt;/beans&gt; </pre>
+ *
+ * <p/>
+ * The servlets Spring bean factory is configured when the servlet is initialized on
+ * startup:
+ * <pre class="codeJava">
+ * <span class="kw">public void</span> init() <span class="kw">throws</span>ServletException {
+ *     <span class="kw">super</span>.init();
+ *
+ *     String springPath = getInitParameter(SPRING_PATH);
+ *     <span class="kw">if</span> (springPath == <span class="kw">null</span>) {
+ *         String msg = SPRING_PATH + <span class="st">" servlet init parameter not defined"</span>;
+ *         <span class="kw">throw new</span> UnavailableException(msg);
+ *     }
+ *     beanFactory = <span class="kw">new</span> ClassPathXmlApplicationContext(springPath);
+ * } </pre>
+ *
+ * The <tt class="blue">spring-path</tt> servlet initialization parameter
+ * specifies the class path of the Spring application context XML file.
+ * This servet initialization parameter is specified in <tt>WEB-INF/web.xml</tt>:
+ *
+ * <pre class="codeConfig">
+ * &lt;web-app&gt;
+ *
+ *    &lt;servlet&gt;
+ *       &lt;servlet-name&gt;spring-click-servlet&lt;/servlet-name&gt;
+ *       &lt;servlet-class&gt;net.sf.click.extras.spring.SpringClickServlet&lt;/servlet-class&gt;
+ *       &lt;init-param&gt;
+ *         &lt;param-name&gt;<font color="blue">spring-path</font>&lt;/param-name&gt;
+ *         &lt;param-value&gt;<font color="red">/applicationContext.xml</font>&lt;/param-value&gt;
+ *       &lt;/init-param&gt;
+ *       &lt;load-on-startup&gt;0&lt;/load-on-startup&gt;
+ *    &lt;/servlet&gt;
+ *
+ *    &lt;servlet-mapping&gt;
+ *       &lt;servlet-name&gt;spring-click-servlet&lt;/servlet-name&gt;
+ *       &lt;url-pattern&gt;*.htm&lt;/url-pattern&gt;
+ *    &lt;/servlet-mapping&gt;
+ *
+ * &lt;/web-app&gt; </pre>
  *
  * @author Phil Barnes
  * @author Paul Rule
  * @author Malcolm Edgar
  */
 public class SpringClickServlet extends ClickServlet {
+
+    private static final long serialVersionUID = -8251140780990964857L;
 
     /**
      * The path to the Spring XML appliation context definition file:
