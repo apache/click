fbe03073f570fd80b690881ac447240dbc436d5c
amohombe <amohombe@unknown>
2005-12-23 00:49:06 +0000

    Try to add the same pages for Person as for the Department, to make the example a little more complete.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@714745 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webapps/click-cayenne/webapp/WEB-INF/src/click/cayenne/page/PersonEditor.java
@@ -0,0 +1,67 @@
+package click.cayenne.page;
+
+import net.sf.click.extras.cayenne.CayenneForm;
+import net.sf.click.control.*;
+import click.cayenne.entity.Person;
+
+/**
+ * Provides a Person Editor page which can be used to create or edit
+ * Person data objects. This page uses the data aware CayenneForm control
+ * for simplified editing data objects.
+ */
+public class PersonEditor  extends BorderedPage {
+
+    /** The Person editing CayenneForm. */
+    protected CayenneForm form = new CayenneForm("form", Person.class);
+
+    /**
+     * Create a new Person Editor.
+     */
+    public PersonEditor() {
+        form.setButtonAlign("right");
+        addControl(form);
+
+        form.add(new TextField("fullName", "Full Name", 35));
+        form.add(new DateField("dateHired","Date Hired"));
+        form.add(new DoubleField("baseSalary","Base Salary"));
+
+        form.add(new Submit("ok", "   OK   ", this, "onOkClick"));
+        form.add(new Submit("cancel", this, "onCancelClick"));
+    }
+
+    /**
+     * Set the person object to edit.
+     *
+     * @param person the department to edit
+     */
+    public void setPerson(Person person) {
+        form.setDataObject(person);
+    }
+
+    /**
+     * Handle the OK button click, saving the Person if valid and
+     * redirecting to the <tt>PersonsViewer</tt> page. If the Person
+     * is not valid display form errors.
+     */
+    public boolean onOkClick() {
+        if (form.isValid()) {
+            if (form.saveChanges()) {
+                setRedirect("persons-viewer.htm");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Handle the Cancel button click, redirecting to the
+     * <tt>PersonsViwer</tt> page.
+     *
+     * @return false
+     */
+    public boolean onCancelClick() {
+        setRedirect("persons-viewer.htm");
+        return false;
+    }
+
+}
