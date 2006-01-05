394ba38471ec94e350b1673c512112a003ecfba6
amohombe <amohombe@unknown>
2006-01-05 17:36:57 +0000

    Experimental RelationShip support for Cayenne Forms
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@714765 13f79535-47bb-0310-9956-ffa450edef68

--- click/webapps/click-cayenne/webapp/WEB-INF/src/click/cayenne/page/PersonEditor.java
+++ click/webapps/click-cayenne/webapp/WEB-INF/src/click/cayenne/page/PersonEditor.java
@@ -1,10 +1,16 @@
 package click.cayenne.page;
 
 import net.sf.click.extras.cayenne.CayenneForm;
+import net.sf.click.extras.cayenne.RelationshipSelect;
 import net.sf.click.extras.control.DateField;
 import net.sf.click.extras.control.DoubleField;
 import net.sf.click.control.*;
+import net.sf.click.Context;
 import click.cayenne.entity.Person;
+import click.cayenne.entity.Department;
+import org.objectstyle.cayenne.query.SelectQuery;
+
+import java.util.List;
 
 /**
  * Provides a Person Editor page which can be used to create or edit
@@ -18,6 +24,7 @@ public class PersonEditor  extends BorderedPage {
     /** The Person editing CayenneForm. */
     protected CayenneForm form = new CayenneForm("form", Person.class);
 
+    protected RelationshipSelect department;
     /**
      * Create a new Person Editor.
      */
@@ -28,6 +35,23 @@ public class PersonEditor  extends BorderedPage {
         form.add(new TextField("fullName", "Full Name", 35));
         form.add(new DateField("dateHired","Date Hired"));
         form.add(new DoubleField("baseSalary","Base Salary"));
+
+
+        form.addRelation("department",Department.class,"department");
+        department = new RelationshipSelect("department","Department");
+        department.setEmptyOption(true);
+        department.setDecorator(new Decorator() {
+            public String render(Object row, Context context) {
+                Department departmentDataObject = (Department) row;
+                return departmentDataObject.getName();
+            }
+        });
+        form.add(department);
+
+        SelectQuery query = new SelectQuery(Department.class);
+        List departmentList = getDataContext().performQuery(query);
+        setDepartmentRelation(departmentList);
+
 
         form.add(new Submit("ok", "   OK   ", this, "onOkClick"));
         form.add(new Submit("cancel", this, "onCancelClick"));
@@ -42,6 +66,23 @@ public class PersonEditor  extends BorderedPage {
         form.setDataObject(person);
     }
 
+    public void setDepartmentRelation(List departments){
+//        System.out.println("*-----> PersonEditor.setDepartmentRelation");
+//        for (int i = 0; i < departments.size(); i++) {
+//            Object o = departments.get(i);
+//            System.out.println("*----> Element in Departments:"+o);
+//        }
+        department.setRelations(departments);
+    }
+
+/*
+    public void onGet() {
+        SelectQuery query = new SelectQuery(Department.class);
+        List departmentList = getDataContext().performQuery(query);
+        setDepartmentRelation(departmentList);
+    }
+*/
+    
     /**
      * Handle the OK button click, saving the Person if valid and
      * redirecting to the <tt>PersonsViewer</tt> page. If the Person
