8b02e1056241543e130b18ffc16c684ac6d0010e
malcolm_edgar <dev-null@apache.org>
2006-02-16 08:48:08 +0000

    added support for nested objects using OGNL
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@714868 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/test/net/sf/click/util/ClickUtilsTest.java
+++ click/framework/test/net/sf/click/util/ClickUtilsTest.java
@@ -4,13 +4,14 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import junit.framework.TestCase;
+import net.sf.click.control.Checkbox;
 import net.sf.click.control.FieldSet;
 import net.sf.click.control.Form;
 import net.sf.click.control.TextField;
 import net.sf.click.extras.control.DateField;
-import net.sf.click.extras.control.IntegerField;
 import net.sf.click.extras.control.DoubleField;
-import net.sf.click.control.Checkbox;
+import net.sf.click.extras.control.IntegerField;
+import net.sf.click.extras.control.LongField;
 
 public class ClickUtilsTest extends TestCase {
     
@@ -20,6 +21,7 @@ public class ClickUtilsTest extends TestCase {
     private static final int INT = 98;
     private static final boolean BOOLEAN = true;
     private static final double DOUBLE = -87.23;
+    private static final String TELEPHONE = "9877262";
     
     static {
         Calendar calendar = new GregorianCalendar(2000, 01, 02);
@@ -49,7 +51,10 @@ public class ClickUtilsTest extends TestCase {
         form.add(doubleField);
                
         Checkbox checkBox = new Checkbox("boolean");
-        form.add(checkBox);   
+        form.add(checkBox);  
+        
+        LongField telephoneField = new LongField("telephone");
+        form.add(telephoneField);
         
         // Populate fields
         idField.setValueObject(ID);
@@ -58,6 +63,7 @@ public class ClickUtilsTest extends TestCase {
         intField.setValue(String.valueOf(INT));
         doubleField.setValue(String.valueOf(DOUBLE));
         checkBox.setChecked(BOOLEAN);
+        telephoneField.setValue(TELEPHONE);
 
         // copy form to object
         SampleObject sampleObject = new SampleObject();
@@ -67,9 +73,23 @@ public class ClickUtilsTest extends TestCase {
         assertEquals(idField.getInteger(), sampleObject.getId());
         assertEquals(nameField.getValue(), sampleObject.getName());
         assertEquals(dateField.getValueObject(), sampleObject.getDateOfBirth());
+        assertEquals(telephoneField.getValueObject().toString(), sampleObject.getTelephone());
         assertTrue(sampleObject.getInt() == intField.getInteger().intValue());
         assertTrue(sampleObject.getDouble() == doubleField.getDouble().doubleValue());
         assertTrue(sampleObject.getBoolean() == checkBox.isChecked());
+        
+        // Test object path copying
+        
+        User user = new User();
+        user.setAddress(new Address());
+        user.getAddress().setState(new State());
+        
+        form = new Form();
+        TextField codeField = new TextField("address.state.code");
+        codeField.setValue("NSW");
+        form.add(codeField);
+        form.copyTo(user, true);
+        assertEquals("NSW", user.getAddress().getState().getCode());
     }
 
     public void testCopyObjectToForm() {
@@ -116,6 +136,19 @@ public class ClickUtilsTest extends TestCase {
         assertTrue(sampleObject.getInt() == intField.getInteger().intValue());
         assertTrue(sampleObject.getDouble() == doubleField.getDouble().doubleValue());
         assertTrue(sampleObject.getBoolean() == checkBox.isChecked());
+        
+        // Test object path copying
+        
+        User user = new User();
+        user.setAddress(new Address());
+        user.getAddress().setState(new State());
+        user.getAddress().getState().setCode("NSW");
+        
+        form = new Form();
+        TextField codeField = new TextField("address.state.code");
+        form.add(codeField);
+        form.copyFrom(user, true);
+        assertEquals("NSW", codeField.getValueObject());
     }
 
     public void testToLabel() {
@@ -124,13 +157,14 @@ public class ClickUtilsTest extends TestCase {
         assertEquals("Card PIN", ClickUtils.toLabel("cardPIN"));
     }
 
-    private static class SampleObject {
+    public static class SampleObject {
         private Integer id;
         private String name;
         private java.util.Date dateOfBirth;
         private boolean _boolean;
         private int _int;
         private double _double;
+        private String telephone;
 
         public Integer getId() {
             return id;
@@ -179,6 +213,96 @@ public class ClickUtilsTest extends TestCase {
         public void setInt(int i) {
             _int = i;
         }
+        
+        public String getTelephone() {
+            return telephone;
+        }
+        
+        public void setTelephone(String telephone) {
+            this.telephone = telephone;
+        }
+    }
+
+    public static class User {
+        private Integer id;
+        private String name;
+        private Address address;
+        private String telephone;
+        
+        public Address getAddress() {
+            return address;
+        }
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+        public Integer getId() {
+            return id;
+        }
+        public void setId(Integer id) {
+            this.id = id;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getTelephone() {
+            return telephone;
+        }
+        public void setTelephone(String telephone) {
+            this.telephone = telephone;
+        }
+    }
+
+    public static class Address {
+        private Integer id;
+        private String lineOne;
+        private String lineTwo;
+        private String lineThree;
+        private State state;
+        
+        public State getState() {
+            return state;
+        }
+        public void setState(State state) {
+            this.state = state;
+        }
+        public Integer getId() {
+            return id;
+        }
+        public void setId(Integer id) {
+            this.id = id;
+        }
+        public String getLineOne() {
+            return lineOne;
+        }
+        public void setLineOne(String lineOne) {
+            this.lineOne = lineOne;
+        }
+        public String getLineThree() {
+            return lineThree;
+        }
+        public void setLineThree(String lineThree) {
+            this.lineThree = lineThree;
+        }
+        public String getLineTwo() {
+            return lineTwo;
+        }
+        public void setLineTwo(String lineTwo) {
+            this.lineTwo = lineTwo;
+        }
+    }
+    
+    public static class State {
+        private String code;
+
+        public String getCode() {
+            return code;
+        }
+        public void setCode(String code) {
+            this.code = code;
+        } 
     }
 
 }
