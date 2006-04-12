b948138ab7035c2d548a7740ebb3f4a7d043d1a7
chrisichris <chrisichris@unknown>
2006-04-12 07:07:51 +0000

    New control ColorPicker in extras
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@714995 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webapps/click-examples/WEB-INF/src/examples/page/ColorPickerPage.java
@@ -0,0 +1,73 @@
+package examples.page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.click.control.Form;
+import net.sf.click.control.Submit;
+import net.sf.click.extras.control.ColorPicker;
+
+public class ColorPickerPage extends BorderedPage {
+
+    private ColorPicker cp1;
+    private ColorPicker cp2;
+    private ColorPicker cp3;
+    private ColorPicker cp4;
+    private Form form;
+
+    public ColorPickerPage() {
+        super();
+    }
+
+    /* (non-Javadoc)
+     * @see net.sf.click.Page#onInit()
+     */
+    public void onInit() {
+        form = new Form("form");
+        addControl(form);
+
+        cp1 = new ColorPicker("cp1");
+        cp1.setLabel("CP1 - not required");
+        form.add(cp1);
+
+        cp2 = new ColorPicker("cp2");
+        cp2.setRequired(true);
+        cp2.setValue("#999");
+        cp2.setLabel("CP2 - required");
+        form.add(cp2);
+
+        cp3 = new ColorPicker("cp3");
+        cp3.setShowTextField(false);
+        cp3.setLabel("CP3 - not required");
+        form.add(cp3);
+
+        cp4 = new ColorPicker("cp4");
+        cp4.setShowTextField(false);
+        cp4.setRequired(true);
+        cp4.setLabel("CP4 - required");
+        form.add(cp4);
+
+        form.add(new Submit("ok"," submit "));
+    }
+
+    public void onPost() {
+        if(form.isValid()) {
+            List colors = new ArrayList(4);
+
+            colors.add(makeColor(cp1.getValue()));
+            colors.add(makeColor(cp2.getValue()));
+            colors.add(makeColor(cp3.getValue()));
+            colors.add(makeColor(cp4.getValue()));
+
+            addModel("colors",colors);
+        }
+    }
+
+    private Object makeColor(String c) {
+        if(c == null || c.length() == 0) {
+            return Boolean.FALSE;
+        }
+        return c;
+    }
+
+}
