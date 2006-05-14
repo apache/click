50390c42ff68a8a1d8fe61c61dc64adb3d8f0dd4
malcolm_edgar <dev-null@apache.org>
2006-05-14 09:39:16 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@715112 13f79535-47bb-0310-9956-ffa450edef68

--- click/examples/src/examples/page/StandardControlsForm.java
+++ click/examples/src/examples/page/StandardControlsForm.java
@@ -1,16 +1,18 @@
 package examples.page;
 
+import java.util.Iterator;
 import java.util.List;
 
-import examples.domain.CustomerDAO;
 import net.sf.click.control.Button;
 import net.sf.click.control.Checkbox;
+import net.sf.click.control.Field;
 import net.sf.click.control.FieldSet;
 import net.sf.click.control.FileField;
 import net.sf.click.control.Form;
 import net.sf.click.control.HiddenField;
 import net.sf.click.control.ImageSubmit;
 import net.sf.click.control.Label;
+import net.sf.click.control.Option;
 import net.sf.click.control.PasswordField;
 import net.sf.click.control.Radio;
 import net.sf.click.control.RadioGroup;
@@ -19,21 +21,26 @@ import net.sf.click.control.Select;
 import net.sf.click.control.Submit;
 import net.sf.click.control.TextArea;
 import net.sf.click.control.TextField;
+import examples.domain.CustomerDAO;
 
 public class StandardControlsForm extends BorderedPage {
 
     private Form form = new Form("form");
+    private FieldSet fieldSet = new FieldSet("fieldSet");
     private Select select = new Select("select");
+    private Checkbox allFieldsRequired = new Checkbox("allFieldsRequired");
+    private Checkbox jsValidate = new Checkbox("jsValidate", "JavaScript Validate");
 
     public StandardControlsForm() {
-
-        FieldSet fieldSet = new FieldSet("fieldSet");
+        
+        // Controls FieldSet
         form.add(fieldSet);
 
         fieldSet.add(new Checkbox("checkbox"));
         fieldSet.add(new FileField("fileField"));
         fieldSet.add(new HiddenField("hiddenField", String.class));
-        fieldSet.add(new Label("label"));
+        String labelText = "<span style='color:blue;font-style:italic'>Label - note how label text spans both columns</span>";
+        fieldSet.add(new Label("label", labelText));
         fieldSet.add(new PasswordField("passwordField"));
         fieldSet.add(new Radio("radio", "Radio", "radio"));
         RadioGroup radioGroup = new RadioGroup("radioGroup");
@@ -44,18 +51,45 @@ public class StandardControlsForm extends BorderedPage {
         fieldSet.add(select);
         fieldSet.add(new TextArea("textArea"));
         fieldSet.add(new TextField("textField"));
-
+        
         form.add(new Button("button"));
-        form.add(new ImageSubmit("image", "images/edit-button.gif"));
+        ImageSubmit imageSubmit = new ImageSubmit("image", "images/edit-button.gif");
+        imageSubmit.setTitle("ImageSubmit");
+        form.add(imageSubmit);
         form.add(new Reset("reset"));
         form.add(new Submit("submit"));
 
+        // Settings FieldSet
+        FieldSet settingsFieldSet = new FieldSet("Settings");
+        settingsFieldSet.add(allFieldsRequired);
+        settingsFieldSet.add(jsValidate);
+        form.add(settingsFieldSet);
+        
         addControl(form);
     }
 
     public void onInit() {
         List customers = CustomerDAO.getCustomersSortedByName();
+        select.add(new Option("[Select]"));
         select.addAll(customers, "id", "name");
+        
+        applySettings();
+    }
+
+    /**
+     * Apply the settings to the form and its fields before it is processed.
+     */
+    private void applySettings() {
+        allFieldsRequired.setContext(getContext());
+        allFieldsRequired.onProcess();
+        for (Iterator i = fieldSet.getFieldList().iterator(); i.hasNext();) {
+            Field field = (Field) i.next();
+            field.setRequired(allFieldsRequired.isChecked());
+        }
+
+        jsValidate.setContext(getContext());
+        jsValidate.onProcess();
+        form.setJavaScriptValidation(jsValidate.isChecked());
     }
 
 }
