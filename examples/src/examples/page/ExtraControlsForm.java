558e93776536ab632d254390824df048c9b278bd
malcolm_edgar <dev-null@apache.org>
2006-05-15 10:29:09 +0000

    completed controls examples and removed BigForm example
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715117 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/examples/page/ExtraControlsForm.java
+++ examples/src/examples/page/ExtraControlsForm.java
@@ -1,7 +1,11 @@
 package examples.page;
 
+import java.util.Iterator;
 import java.util.List;
 
+import net.sf.click.control.Checkbox;
+import net.sf.click.control.Field;
+import net.sf.click.control.FieldSet;
 import net.sf.click.control.Form;
 import net.sf.click.control.Submit;
 import net.sf.click.extras.control.CheckList;
@@ -17,10 +21,17 @@ import net.sf.click.extras.control.RegexField;
 import net.sf.click.extras.control.TelephoneField;
 import examples.domain.CustomerDAO;
 
+/**
+ * Provides a form containing all the Click Extras Controls.
+ *
+ * @author Malcolm Edgar
+ */
 public class ExtraControlsForm extends BorderedPage {
 
     private Form form = new Form("form");
     private CheckList checkList = new CheckList("checkList");
+    private Checkbox allFieldsRequired = new Checkbox("allFieldsRequired");
+    private Checkbox jsValidate = new Checkbox("jsValidate", "JavaScript Validate");
 
     public ExtraControlsForm() {
 
@@ -39,12 +50,38 @@ public class ExtraControlsForm extends BorderedPage {
 
         form.add(new Submit("submit"));
 
+        // Settings FieldSet
+        FieldSet settingsFieldSet = new FieldSet("Settings");
+        settingsFieldSet.add(allFieldsRequired);
+        settingsFieldSet.add(jsValidate);
+        form.add(settingsFieldSet);
+
+        form.setErrorsPosition(Form.POSITION_TOP);
+
         addControl(form);
     }
 
     public void onInit() {
         List customers = CustomerDAO.getCustomersSortedByName();
         checkList.addAll(customers, "id", "name");
+
+        applySettings();
+    }
+
+    /**
+     * Apply the settings to the form and its fields before it is processed.
+     */
+    private void applySettings() {
+        allFieldsRequired.setContext(getContext());
+        allFieldsRequired.onProcess();
+        for (Iterator i = form.getFieldList().iterator(); i.hasNext();) {
+            Field field = (Field) i.next();
+            field.setRequired(allFieldsRequired.isChecked());
+        }
+
+        jsValidate.setContext(getContext());
+        jsValidate.onProcess();
+        form.setJavaScriptValidation(jsValidate.isChecked());
     }
 
 }
