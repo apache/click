8b97efb084b95d85e4d1a820b01a5f48d52d5383
malcolm_edgar <dev-null@apache.org>
2006-05-29 12:16:52 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715172 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/net/sf/click/examples/page/ajax/AjaxLiveGrid.java
+++ examples/src/net/sf/click/examples/page/ajax/AjaxLiveGrid.java
@@ -1,10 +1,8 @@
 package net.sf.click.examples.page.ajax;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.sf.click.examples.page.BorderPage;
-
 
 /**
  * Provides an Ajax demo of Rico's LiveGrid.
@@ -16,9 +14,7 @@ public class AjaxLiveGrid extends BorderPage {
     public void onInit() {
         addModel("body-onload", "javascript:bodyOnLoad();");
 
-        // TODO: getAllCustomers
-//        List customerList = getCustomerService().getAllCustomers();
-        List customerList = new ArrayList();
+        List customerList = getCustomerService().getAllCustomers();
         addModel("customers", customerList);
         addModel("totalRows",new Integer(customerList.size()));
         // Always start at a 0 offset
