cf3e41d51458aef6f3006acdd415a097fa270612
Naoki Takezoe <takezoe@apache.org>
2006-06-12 03:14:00 +0000

    Implemented: switch to HTML/class action
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@715216 13f79535-47bb-0310-9956-ffa450edef68

--- tools/eclipse/net.sf.clickide/src/net/sf/clickide/ui/actions/SwitchToHTMLAction.java
+++ tools/eclipse/net.sf.clickide/src/net/sf/clickide/ui/actions/SwitchToHTMLAction.java
@@ -1,7 +1,11 @@
 package net.sf.clickide.ui.actions;
 
 import net.sf.clickide.ClickPlugin;
+import net.sf.clickide.ClickUtils;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IType;
@@ -11,7 +15,14 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.ide.IDE;
 
+/**
+ * Switches to the HTML file from the page class.
+ * 
+ * @author Naoki Takezoe
+ */
 public class SwitchToHTMLAction implements IEditorActionDelegate {
 	
 	private IEditorPart editor;
@@ -22,14 +33,25 @@ public class SwitchToHTMLAction implements IEditorActionDelegate {
 
 	public void run(IAction action) {
 		IEditorInput input = this.editor.getEditorInput();
-		IJavaElement element = 
-			JavaPlugin.getDefault().getWorkingCopyManager().getWorkingCopy(input);
-		ICompilationUnit unit = (ICompilationUnit)element;
-		try {
-			IType type = unit.getAllTypes()[0];
-			System.out.println(type.getFullyQualifiedName());
-		} catch(Exception ex){
-			ClickPlugin.log(ex);
+		if(input instanceof IFileEditorInput){
+			IFile file = ((IFileEditorInput)input).getFile();
+			IJavaElement element = 
+				JavaPlugin.getDefault().getWorkingCopyManager().getWorkingCopy(input);
+			ICompilationUnit unit = (ICompilationUnit)element;
+			try {
+				IType type = unit.getAllTypes()[0];
+				String html = ClickUtils.getHTMLfromClass(file.getProject(), type.getFullyQualifiedName());
+				if(html!=null){
+					String root = ClickUtils.getWebAppRootFolder(file.getProject());
+					IFolder folder = file.getProject().getFolder(root);
+					IResource resource = folder.findMember(html);
+					if(resource!=null && resource instanceof IFile && resource.exists()){
+						IDE.openEditor(ClickUtils.getActivePage(), (IFile)resource);
+					}
+				}
+			} catch(Exception ex){
+				ClickPlugin.log(ex);
+			}
 		}
 	}
 
