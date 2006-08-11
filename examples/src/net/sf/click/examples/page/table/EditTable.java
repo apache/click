40186bf82c0daac8628192320897c8793cdd2c06
malcolm_edgar <dev-null@apache.org>
2006-08-11 05:19:25 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715382 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/net/sf/click/examples/page/table/EditTable.java
@@ -0,0 +1,109 @@
+package net.sf.click.examples.page.table;
+
+import java.util.List;
+
+import net.sf.click.control.ActionLink;
+import net.sf.click.control.Column;
+import net.sf.click.control.FieldSet;
+import net.sf.click.control.Submit;
+import net.sf.click.control.Table;
+import net.sf.click.control.TextField;
+import net.sf.click.examples.domain.Customer;
+import net.sf.click.examples.page.BorderPage;
+import net.sf.click.extras.cayenne.CayenneForm;
+import net.sf.click.extras.control.DateField;
+import net.sf.click.extras.control.DoubleField;
+import net.sf.click.extras.control.EmailField;
+import net.sf.click.extras.control.LinkDecorator;
+
+/**
+ * Provides an demonstration of Table and Form editor pattern, and the use
+ * CayenneForm and LinkDecorator classes.
+ *
+ * @author Malcolm Edgar
+ */
+public class EditTable extends BorderPage {
+
+    public CayenneForm form = new CayenneForm("form", Customer.class);
+    public Table table = new Table();
+    public ActionLink editLink = new ActionLink("edit", "Edit", this, "onEditClick");
+    public ActionLink deleteLink = new ActionLink("delete", "Delete", this, "onDeleteClick");
+    
+    public EditTable() {
+        // Setup customers form
+        FieldSet fieldSet = new FieldSet("customer");
+        fieldSet.add(new TextField("name"));
+        fieldSet.add(new EmailField("email"));
+        fieldSet.add(new DoubleField("holdings"));        
+        fieldSet.add(new DateField("dateJoined"));
+        form.add(fieldSet);
+        form.add(new Submit("save", this, "onSaveClick"));
+        form.add(new Submit("cancel", this, "onCancelClick"));
+        
+        // Setup customers table
+        table.setAttribute("class", "simple");
+
+        Column column = new Column("name");
+        table.addColumn(column);
+
+        column = new Column("email");
+        column.setAutolink(true);
+        table.addColumn(column);
+
+        column = new Column("holdings");
+        column.setFormat("${0,number,#,##0.00}");
+        column.setAttribute("style", "{text-align:right;}");
+        table.addColumn(column);
+
+        column = new Column("dateJoined");
+        column.setFormat("{0,date,medium}");
+        table.addColumn(column);
+        
+        column = new Column("Action");
+        ActionLink[] links = new ActionLink[]{editLink, deleteLink};
+        column.setDecorator(new LinkDecorator(links, "id"));
+        table.addColumn(column);
+        
+        deleteLink.setAttribute("onclick", "return window.confirm('Please confirm delete');");        
+    }
+
+    public boolean onEditClick() {
+        Integer id = editLink.getValueInteger();
+        Customer customer = getCustomerService().getCustomer(id);
+        if (customer != null) {
+            form.setDataObject(customer);
+        }
+        return true;
+    }
+
+    public boolean onDeleteClick() {
+        Integer id = deleteLink.getValueInteger();
+        getCustomerService().deleteCustomer(id);
+        return true;
+    }
+
+    public boolean onSaveClick() {
+        if (form.isValid()) {
+            form.getDataObject();
+            getDataContext().commitChanges();
+            form.setDataObject(null);
+        }
+        return true;
+    }
+
+    public boolean onCancelClick() {
+        getDataContext().rollbackChanges();
+        form.setDataObject(null);
+        form.clearErrors();
+        return true;
+    }
+
+    /**
+     * @see net.sf.click.Page#onRender()
+     */
+    public void onRender() {
+        List customers = getCustomerService().getCustomersSortedByName(12);
+        table.setRowList(customers);
+    }
+
+}
