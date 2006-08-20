40569decb99a20e6286ab9e6e1702b7a830a1cb1
malcolm_edgar <dev-null@apache.org>
2006-08-20 23:56:00 +0000

    formatting change
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715399 13f79535-47bb-0310-9956-ffa450edef68

--- extras/src/net/sf/click/extras/control/LinkDecorator.java
+++ extras/src/net/sf/click/extras/control/LinkDecorator.java
@@ -39,55 +39,55 @@ import ognl.OgnlException;
  * </table>
  *
  * <p/>
- * The example below uses a LinkDecorator to render the row ActionLinks in the 
+ * The example below uses a LinkDecorator to render the row ActionLinks in the
  * tables "Action" column.
  * <pre class="codeJava">
  * <span class="kw">public class</span> EditTable <span class="kw">extends</span> BorderPage {
- * 
+ *
  *     <span class="kw">public</span> Table table = <span class="kw">new</span> Table();
  *     <span class="kw">public</span> ActionLink editLink = <span class="kw">new</span> ActionLink(<span class="st">"edit"</span>, <span class="st">"Edit"</span>, <span class="kw">this</span>, <span class="st">"onEditClick"</span>);
  *     <span class="kw">public</span> ActionLink deleteLink = <span class="kw">new</span> ActionLink(<span class="st">"delete"</span>, <span class="st">"Delete"</span>, <span class="kw">this</span>, <span class="st">"onDeleteClick"</span>);
- * 
+ *
  *     public EditTable() {
  *         table.addColumn(<span class="kw">new</span> Column(<span class="st">"name"</span>));
  *         table.addColumn(<span class="kw">new</span> Column(<span class="st">"email"</span>));
  *         table.addColumn(<span class="kw">new</span> Column(<span class="st">"holdings"</span>));
  *         table.addColumn(<span class="kw">new</span> Column(<span class="st">"dateJoined"</span>));
- * 
+ *
  *         Column column = <span class="kw">new</span> Column(<span class="st">"Action"</span>);
  *         ActionLink[] links = <span class="kw">new</span> ActionLink[]{editLink, deleteLink};
  *         column.setDecorator(<span class="kw">new</span> LinkDecorator(links, <span class="st">"id"</span>));
  *         table.addColumn(column);
- * 
+ *
  *         deleteLink.setAttribute(<span class="st">"onclick"</span>, <span class="st">"return window.confirm('Please confirm delete');"</span>);
  *     }
- * 
+ *
  *     <span class="kw">public boolean</span> onEditClick() {
  *         Integer id = editLink.getValueInteger();
  *         Customer customer = getCustomerService().getCustomer(id);
- *         
- *         ..         
+ *
+ *         ..
  *     }
- * 
+ *
  *     <span class="kw">public boolean</span> onDeleteClick() {
  *         Integer id = deleteLink.getValueInteger();
  *         getCustomerService().deleteCustomer(id);
  *         <span class="kw">return true</span>;
  *     }
- *     
+ *
  *     <span class="kw">public void</span> onRender() {
  *         List customers = getCustomerService().getCustomersSortedByName(12);
  *         table.setRowList(customers);
- *     }     
+ *     }
  * } </pre>
- * 
- * 
- * This class was inspired by Richardo Lecheta's <tt>ViewDecorator</tt> design 
+ *
+ *
+ * This class was inspired by Richardo Lecheta's <tt>ViewDecorator</tt> design
  * pattern.
- * 
+ *
  * @see net.sf.click.control.ActionLink
  * @see net.sf.click.control.PageLink
- * 
+ *
  * @author Malcolm Edgar
  */
 public class LinkDecorator implements Decorator {
@@ -103,7 +103,7 @@ public class LinkDecorator implements Decorator {
 
     /** The OGNL context map. */
     protected Map ognlContext;
-    
+
     /**
      * Create a new AbstractLink table column Decorator with the given actionLink
      * and row object identifier property name.
@@ -140,12 +140,12 @@ public class LinkDecorator implements Decorator {
         this.linksArray = links;
         this.idProperty = idProperty;
     }
-    
+
     // ------------------------------------------------------ Public Properties
-    
+
     /**
      * Return the link separator string. The default value is <tt>" | "</tt>.
-     * 
+     *
      * @return the link separator string.
      */
     public String getLinkSeparator() {
@@ -154,13 +154,13 @@ public class LinkDecorator implements Decorator {
 
     /**
      * Set the link separator string with the given value.
-     * 
+     *
      * @param value the link separator string value
      */
     public void setLinkSeparator(String value) {
         this.linkSeparator = value;
-    }    
-    
+    }
+
     // --------------------------------------------------------- Public Methods
 
     /**
