71edfcf330d0f25484d3be29182914acbb568bb9
malcolm_edgar <dev-null@apache.org>
2006-10-23 07:55:43 +0000

    added database sorting example
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@715625 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/examples/src/net/sf/click/examples/page/table/TableSorting.java
@@ -0,0 +1,59 @@
+package net.sf.click.examples.page.table;
+
+import java.util.List;
+
+import net.sf.click.control.Column;
+import net.sf.click.control.Table;
+import net.sf.click.examples.page.BorderPage;
+
+/**
+ * Provides an demonstration of Table column sorting using a database.
+ *
+ * @author Malcolm Edgar
+ */
+public class TableSorting extends BorderPage {
+
+    public Table table = new Table();
+
+    public TableSorting() {
+        // Setup customers table
+        table.setAttribute("class", "mars");
+        table.setHoverRows(true);
+
+        Column column = new Column("id");
+        column.setSortable(false);
+        table.addColumn(column);
+
+        table.addColumn(new Column("name"));
+
+        column = new Column("email");
+        column.setAutolink(true);
+        table.addColumn(column);
+
+        column = new Column("age");
+        column.setDataStyle("text-align", "center");
+        table.addColumn(column);
+
+        column = new Column("holdings");
+        column.setFormat("${0,number,#,##0.00}");
+        column.setDataStyle("text-align", "right");
+        column.setHeaderStyle("text-align", "right");
+        table.addColumn(column);
+
+        column = new Column("active");
+        column.setDataStyle("text-align", "center");
+        table.addColumn(column);
+    }
+
+    /**
+     * @see net.sf.click.Page#onRender()
+     */
+    public void onRender() {
+long time = System.currentTimeMillis();
+        List customers = getCustomerService().getCustomersSortedBy(table.getSortedColumn());
+        table.setRowList(customers);
+        table.setSorted(true);
+System.out.println("onRender: " + (System.currentTimeMillis() - time));
+    }
+
+}
