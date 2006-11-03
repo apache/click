03ca2f08060835cf9a6d305e057ca9e40a27f69d
malcolm_edgar <dev-null@apache.org>
2006-11-03 05:30:15 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715665 13f79535-47bb-0310-9956-ffa450edef68

---
+++ quickstart/src/com/quickstart/page/BorderPage.java
@@ -0,0 +1,14 @@
+package com.quickstart.page;
+
+import net.sf.click.Page;
+
+public class BorderPage extends Page {
+
+	/**
+	 * @see #getTemplate()
+	 */
+	public String getTemplate() {
+		return "border-template.htm";
+	}
+	
+}
