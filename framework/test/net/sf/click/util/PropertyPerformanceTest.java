18103b1979b67d76e10cf148f5d88d67d7f85f05
shaberman <shaberman@unknown>
2006-11-07 05:52:21 +0000

    Add per-thread/without sync caches.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715681 13f79535-47bb-0310-9956-ffa450edef68

--- framework/test/net/sf/click/util/PropertyPerformanceTest.java
+++ framework/test/net/sf/click/util/PropertyPerformanceTest.java
@@ -19,7 +19,7 @@ public class PropertyPerformanceTest extends TestCase {
     public static Object getOgnlPropertyValue(Object source, String name, Map context) {
 
     	try {
-    		return Ognl.getValue(name, context, source);
+    		return Ognl.getValue(name, source);
 
     	} catch (Exception e) {
     		e.printStackTrace();
@@ -40,9 +40,24 @@ public class PropertyPerformanceTest extends TestCase {
     			}
 			}
 
-    		return Ognl.getValue(expressionTree, context, source);
+    		return Ognl.getValue(expressionTree, source);
 
 
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		throw new RuntimeException(e);
+    	}
+    }
+
+    public static Object getOgnlPropertyValueCachingWithoutSync(Object source, String name, Map cache, Map context) {
+    	try {
+    		Object expressionTree = null;
+			expressionTree = cache.get(name);
+			if (expressionTree == null) {
+				expressionTree = Ognl.parseExpression(name);
+				cache.put(name, expressionTree);
+			}
+    		return Ognl.getValue(expressionTree, source);
     	} catch (Exception e) {
     		e.printStackTrace();
     		throw new RuntimeException(e);
@@ -148,13 +163,58 @@ public class PropertyPerformanceTest extends TestCase {
             return getPropertyValueCaching(value, remainingPart, cache);
         }
     }    
+    
+    private static Object getObjectPropertyValueCachingWithoutSync(Object source, String name, Map cache) {
+        String methodNameKey = source.getClass().getName() + "." + name;;
+
+        Method method = null;
+        try {
+    		method = (Method) cache.get(methodNameKey);
+    		if (method == null) {
+    			method = source.getClass().getMethod(ClickUtils.toGetterName(name), null);
+    			cache.put(methodNameKey, method);
+    		}
+            return method.invoke(source, null);
+        } catch (NoSuchMethodException nsme) {
+            try {
+                method = source.getClass().getMethod(ClickUtils.toIsGetterName(name), null);
+        		cache.put(methodNameKey, method);
+                return method.invoke(source, null);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }    
+    
+    public static Object getPropertyValueCachingWithoutSync(Object source, String name, Map cache) {
+        String basePart = name;
+        String remainingPart = null;
+
+        int baseIndex = name.indexOf(".");
+        if (baseIndex != -1) {
+            basePart = name.substring(0, baseIndex);
+            remainingPart = name.substring(baseIndex + 1);
+        }
+
+        Object value = getObjectPropertyValueCachingWithoutSync(source, basePart, cache);
+
+        if (remainingPart == null || value == null) {
+            return value;
+
+        } else {
+            return getPropertyValueCachingWithoutSync(value, remainingPart, cache);
+        }
+    }    
 	
 	
 	private final int loopIterations = 1000;
 	private final ParentObject parentObject = new ParentObject("malcolm", null, new Date(), Boolean.TRUE, new ChildObject("edgar"));
 	
-	private int threadCount;
-	private long cumulativeTime;
+	private volatile int threadCount;
+	private volatile long cumulativeTime;
 	private List threadList = new ArrayList();
 	
     // ---------------------------------------------------------- Test Methods
@@ -179,13 +239,18 @@ public class PropertyPerformanceTest extends TestCase {
 	}
 	
 	public void testOgnl() throws Exception {
-		this.runOnThreads(OgnlTest.class);
+		// this.runOnThreads(OgnlTest.class);
 		logTime("OGNL cumulative time", cumulativeTime);
 	}
 
 	public void testOgnlCaching() throws Exception {
 		this.runOnThreads(OgnlCachingTest.class);
 		logTime("OGNL with caching cumulative time", cumulativeTime);
+	}
+	
+	public void testOgnlThreadCaching() throws Exception {
+		this.runOnThreads(OgnlThreadCachingTest.class);
+		logTime("OGNL with thread caching cumulative time", cumulativeTime);
 	}
 	
 	public void testReflection() throws Exception {
@@ -196,6 +261,11 @@ public class PropertyPerformanceTest extends TestCase {
 	public void testReflectionCaching() throws Exception {
 		this.runOnThreads(ReflectionCachingTest.class);
 		logTime("Reflection with caching cumulative time", cumulativeTime);
+	}
+	
+	public void testReflectionThreadCaching() throws Exception {
+		this.runOnThreads(ReflectionThreadCachingTest.class);
+		logTime("Reflection with thread caching cumulative time", cumulativeTime);
 	}
 	
 	private void runOnThreads(Class harness) throws Exception {
@@ -210,13 +280,14 @@ public class PropertyPerformanceTest extends TestCase {
 		
 		final Map cache = new HashMap();
 		
-		for (int i = 0; i < threadCount; i++) {
+		int copy = threadCount;
+		for (int i = 0; i < copy; i++) {
 			Runnable r = (Runnable) cstr.newInstance(new Object[] { listener, new Integer(loopIterations), parentObject, cache } );
 			Thread thread = new Thread(r);
 			thread.start();
 			threadList.add(thread);
-		}		
-		
+		}
+
 		while (threadCount > 1) {
 			try {
 				Thread.sleep(1000);
@@ -294,6 +365,35 @@ public class PropertyPerformanceTest extends TestCase {
 			listener.actionPerformed(new ActionEvent(time, ActionEvent.ACTION_PERFORMED, "complete"));
 		}
 	}
+	
+	public static class OgnlThreadCachingTest implements Runnable {
+		private final ActionListener listener;
+		private final int iterations;
+		private final ParentObject testObject;
+		
+		public OgnlThreadCachingTest(ActionListener listener, int iterations, ParentObject testObject, Map ignoredCache) {
+			this.listener = listener;
+			this.iterations = iterations;
+			this.testObject = testObject;
+		}
+		
+		public void run() {
+			long start = System.currentTimeMillis();
+			Map context = new HashMap();
+			Map cache = new HashMap();
+			for (int i = 0; i < iterations; i++) {
+				assertNotNull(getOgnlPropertyValueCachingWithoutSync(testObject, "name", cache, context));
+				assertNull(getOgnlPropertyValueCachingWithoutSync(testObject, "value", cache, context));
+				assertNotNull(getOgnlPropertyValueCachingWithoutSync(testObject, "date", cache, context));
+				assertNotNull(getOgnlPropertyValueCachingWithoutSync(testObject, "valid", cache, context));
+				assertNotNull(getOgnlPropertyValueCachingWithoutSync(testObject, "child.name", cache, context));
+			}
+
+			Long time = new Long((System.currentTimeMillis() - start));
+			
+			listener.actionPerformed(new ActionEvent(time, ActionEvent.ACTION_PERFORMED, "complete"));
+		}
+	}
 
 	public static class ReflectionTest implements Runnable {
 		private final ActionListener listener;
@@ -345,6 +445,35 @@ public class PropertyPerformanceTest extends TestCase {
 				assertNotNull(getPropertyValueCaching(testObject, "date", cache));
 				assertNotNull(getPropertyValueCaching(testObject, "valid", cache));
 				assertNotNull(getPropertyValueCaching(testObject, "child.name", cache));
+			}
+
+			Long time = new Long((System.currentTimeMillis() - start));
+			
+			listener.actionPerformed(new ActionEvent(time, ActionEvent.ACTION_PERFORMED, "complete"));
+		}
+	}	
+	
+	public static class ReflectionThreadCachingTest implements Runnable {
+		private final ActionListener listener;
+		private final int iterations;
+		private final ParentObject testObject;
+		
+		public ReflectionThreadCachingTest(ActionListener listener, int iterations, ParentObject testObject, Map ignoredCache) {
+			this.listener = listener;
+			this.iterations = iterations;
+			this.testObject = testObject;
+		}
+		
+		public void run() {
+			long start = System.currentTimeMillis();
+			Map cache = new HashMap();
+
+			for (int i = 0; i < iterations; i++) {
+				assertNotNull(getPropertyValueCachingWithoutSync(testObject, "name", cache));
+				assertNull(getPropertyValueCachingWithoutSync(testObject, "value", cache));
+				assertNotNull(getPropertyValueCachingWithoutSync(testObject, "date", cache));
+				assertNotNull(getPropertyValueCachingWithoutSync(testObject, "valid", cache));
+				assertNotNull(getPropertyValueCachingWithoutSync(testObject, "child.name", cache));
 			}
 
 			Long time = new Long((System.currentTimeMillis() - start));
