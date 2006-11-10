63724e0d58869327f417967cb28324b221a152ff
malcolm_edgar <dev-null@apache.org>
2006-11-10 11:06:08 +0000

    refactored example packaging
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@715711 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/examples/src/net/sf/click/examples/page/control/ColorPickerPage.java
@@ -0,0 +1,69 @@
+package net.sf.click.examples.page.control;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.click.control.Form;
+import net.sf.click.control.Submit;
+import net.sf.click.examples.page.BorderPage;
+import net.sf.click.examples.page.HomePage;
+import net.sf.click.extras.control.ColorPicker;
+import net.sf.click.extras.control.PageSubmit;
+
+/**
+ * Provides ColorPicker control example.
+ *
+ * @author Christian Essel
+ */
+public class ColorPickerPage extends BorderPage {
+
+    public Form form = new Form("form");
+
+    private ColorPicker cp1;
+    private ColorPicker cp2;
+    private ColorPicker cp3;
+    private ColorPicker cp4;
+
+    public ColorPickerPage() {
+        cp1 = new ColorPicker("cp1", "Color Picker 1");
+        form.add(cp1);
+
+        cp2 = new ColorPicker("cp2", "Color Picker 2");
+        cp2.setRequired(true);
+        cp2.setValue("#999");
+        form.add(cp2);
+
+        cp3 = new ColorPicker("cp3", "Color Picker 3");
+        cp3.setShowTextField(false);
+        form.add(cp3);
+
+        cp4 = new ColorPicker("cp4", "Color Picker 4");
+        cp4.setShowTextField(false);
+        cp4.setRequired(true);
+        form.add(cp4);
+
+        form.add(new Submit("ok", "  OK  "));
+        form.add(new PageSubmit("cancel", HomePage.class));
+    }
+
+    public void onPost() {
+        if (form.isValid()) {
+            List colors = new ArrayList(4);
+
+            colors.add(makeColor(cp1.getValue()));
+            colors.add(makeColor(cp2.getValue()));
+            colors.add(makeColor(cp3.getValue()));
+            colors.add(makeColor(cp4.getValue()));
+
+            addModel("colors", colors);
+        }
+    }
+
+    private Object makeColor(String c) {
+        if (c == null || c.length() == 0) {
+            return Boolean.FALSE;
+        }
+        return c;
+    }
+
+}
