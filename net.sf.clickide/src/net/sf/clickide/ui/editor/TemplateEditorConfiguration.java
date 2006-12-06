86c2375e9a29ce746e3619df1ba998be35985eef
Naoki Takezoe <takezoe@apache.org>
2006-12-06 06:37:53 +0000

    Syntax highlighting for the Velocity template.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/tools/eclipse@715795 13f79535-47bb-0310-9956-ffa450edef68

--- net.sf.clickide/src/net/sf/clickide/ui/editor/TemplateEditorConfiguration.java
+++ net.sf.clickide/src/net/sf/clickide/ui/editor/TemplateEditorConfiguration.java
@@ -3,7 +3,9 @@ package net.sf.clickide.ui.editor;
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
+import org.eclipse.wst.html.core.text.IHTMLPartitions;
 import org.eclipse.wst.html.ui.StructuredTextViewerConfigurationHTML;
+import org.eclipse.wst.sse.ui.internal.provisional.style.LineStyleProvider;
 
 /**
  * {@link SourceViewerConfiguration} for the Velocity Template Editor.
@@ -22,5 +24,25 @@ public class TemplateEditorConfiguration extends StructuredTextViewerConfigurati
 		}
 		return new IContentAssistProcessor[]{this.processor};
 	}
+
+	public LineStyleProvider[] getLineStyleProviders(ISourceViewer sourceViewer, String partitionType) {
+		LineStyleProvider[] providers = null;
+
+		if (partitionType == IHTMLPartitions.HTML_DEFAULT || partitionType == IHTMLPartitions.HTML_COMMENT || partitionType == IHTMLPartitions.HTML_DECLARATION) {
+			providers = new LineStyleProvider[]{new LineStyleProviderForVelocity()};
+		} else {
+			providers = super.getLineStyleProviders(sourceViewer, partitionType);
+		}
+//		else if (partitionType == IHTMLPartitions.SCRIPT) {
+//			providers = new LineStyleProvider[]{getLineStyleProviderForJavascript()};
+//		}
+//		else if (partitionType == ICSSPartitions.STYLE) {
+//			providers = new LineStyleProvider[]{getLineStyleProviderForEmbeddedCSS()};
+//		}
+
+		return providers;
+	}
+	
+	
 	
 }
