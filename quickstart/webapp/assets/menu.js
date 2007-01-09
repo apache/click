50a500a8c80473b6ff0131e0372d349526c81abe
malcolm_edgar <dev-null@apache.org>
2007-01-09 11:51:31 +0000

    expanded quickstart example
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715852 13f79535-47bb-0310-9956-ffa450edef68

---
+++ quickstart/webapp/assets/menu.js
@@ -0,0 +1,18 @@
+startList = function() {
+	if (document.all && document.getElementById) {
+		navRoot = document.getElementById("dmenu");
+		for (i=0; i < navRoot.childNodes.length; i++) {
+			node = navRoot.childNodes[i];
+			if (node.nodeName=="LI") {
+				node.onmouseover=function() {
+					this.className+=" over";
+				}
+				node.onmouseout=function() {
+					this.className=this.className.replace(" over", "");
+				}
+			}
+		}
+	}
+}
+
+window.onload=startList;
