342f1964feb584da3616a10ac634dce4c3b1b6b6
Bob Schellink <sabob@apache.org>
2007-02-20 08:23:37 +0000

    added AbstractControl for easier creation of custom controls.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@715948 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/src/net/sf/click/control/AbstractControl.java
@@ -0,0 +1,300 @@
+/*
+ * Copyright 2004-2007 Malcolm A. Edgar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sf.click.control;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import net.sf.click.Context;
+import net.sf.click.Control;
+import net.sf.click.util.ClickUtils;
+import net.sf.click.util.MessagesMap;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * This class provides a default implementation of the {@link Control} interface, to make it easier
+ * for developers to implement their own controls.
+ *
+ * <p/>Subclasses are expected to at least override {@link java.lang.Object#toString()}
+ * to render the control.
+ *
+ * @author Malcolm Edgar
+ * @author Bob Schellink
+ */
+public abstract class AbstractControl implements Control {
+
+    /** The Field attributes Map. */
+    protected Map attributes;
+
+    /** The Field localized messages Map. */
+    protected Map messages;
+
+    /** The Field name. */
+    protected String name;
+
+    /** The control's parent. */
+    protected transient Object parent;
+
+    /** The request context. */
+    protected transient Context context;
+
+    /**
+     * @see net.sf.click.Control#onProcess()
+     *
+     * @return true
+     */
+    public boolean onProcess() {
+        return true;
+    }
+
+    /**
+     * @see net.sf.click.Control#getContext()
+     *
+     * @return the Page request Context
+     */
+    public Context getContext() {
+        return context;
+    }
+
+    /**
+     * @see Control#setContext(Context)
+     *
+     * @param context the Page request Context
+     */
+    public void setContext(Context context) {
+        this.context = context;
+    }
+    /**
+     * @see Control#getName()
+     *
+     * @return the name of the control
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @see Control#setName(String)
+     *
+     * @param name of the control
+     * @throws IllegalArgumentException if the name is null
+     */
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Null name parameter");
+        }
+        this.name = name;
+    }
+
+    /**
+     * @see Control#getParent()
+     *
+     * @return the Control's parent
+     */
+    public Object getParent() {
+        return parent;
+    }
+
+    /**
+     * @see Control#setParent(Object)
+     *
+     * @param parent the parent of the Control
+     */
+    public void setParent(Object parent) {
+        this.parent = parent;
+    }
+
+    /**
+     * Return the "id" attribute value if defined, or the tree name otherwise.
+     *
+     * @see Control#getId()
+     *
+     * @return HTML element identifier attribute "id" value
+     */
+    public String getId() {
+        if (hasAttributes() && getAttributes().containsKey("id")) {
+            return getAttribute("id");
+        } else {
+            return getName();
+        }
+    }
+
+    /**
+     * Deploy the <tt>tree.css</tt> file to the <tt>click</tt> web
+     * directory when the application is initialized.
+     *
+     * @see Control#onDeploy(ServletContext)
+     *
+     * @param servletContext the servlet context
+     */
+    public void onDeploy(ServletContext servletContext) {}
+
+    /**
+     * Return the package resource bundle message for the named resource key
+     * and the context's request locale.
+     *
+     * @param name resource name of the message
+     * @return the named localized message for the package
+     */
+    public String getMessage(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Null name parameter");
+        }
+
+        String message = null;
+
+        Map parentMessages = ClickUtils.getParentMessages(this);
+        if (parentMessages.containsKey(name)) {
+
+            message = (String) parentMessages.get(name);
+        }
+
+        if (message == null && getMessages().containsKey(name)) {
+            message = (String) getMessages().get(name);
+        }
+
+        return message;
+    }
+
+    /**
+     * Return the formatted package message for the given resource name and
+     * message format arguments and for the context request locale.
+     *
+     * @param name resource name of the message
+     * @param args the message arguments to format
+     * @return the named localized message for the package
+     */
+    public String getMessage(String name, Object[] args) {
+        if (args == null) {
+            throw new IllegalArgumentException("Null args parameter");
+        }
+        String value = getMessage(name);
+
+        return MessageFormat.format(value, args);
+    }
+
+    /**
+     * Return a Map of localized messages for the ActionLink.
+     *
+     * @return a Map of localized messages for the ActionLink
+     * @throws IllegalStateException if the context for the link has not be set
+     */
+    public Map getMessages() {
+        if (messages == null) {
+            if (getContext() != null) {
+                messages =
+                        new MessagesMap(getClass(), CONTROL_MESSAGES, getContext());
+
+            } else {
+                String msg = "Cannot initialize messages as context not set";
+                throw new IllegalStateException(msg);
+            }
+        }
+        return messages;
+    }
+
+    /**
+     * This method returns null.
+     *
+     * @see net.sf.click.Control#getHtmlImports()
+     *
+     * @return null
+     */
+    public String getHtmlImports() {
+        return null;
+    }
+
+    /**
+     * Return the control HTML attribute with the given name, or null if the
+     * attribute does not exist.
+     *
+     * @param name the name of link HTML attribute
+     * @return the link HTML attribute
+     */
+    public String getAttribute(String name) {
+        return (String) getAttributes().get(name);
+    }
+
+    /**
+     * Set the link attribute with the given attribute name and value. You would
+     * generally use attributes if you were creating the entire Control
+     * programatically and rendering it with the {@link #toString()} method.
+     * <p/>
+     * For example given the ActionLink:
+     *
+     * <pre class="codeJava">
+     * ActionLink addLink = <span class="kw">new</span> ActionLink(<span class="red">"addLink"</span>, <span class="st">"Add"</span>);
+     * addLink.setAttribute(<span class="st">"class"</span>, <span class="st">"table"</span>); </pre>
+     *
+     * Will render the HTML as:
+     * <pre class="codeHtml">
+     * &lt;a href=".." <span class="st">class</span>=<span class="st">"table"</span>&gt;<span class="st">Add</span>&lt;/a&gt; </pre>
+     *
+     * @param name the attribute name
+     * @param value the attribute value
+     * @throws IllegalArgumentException if name parameter is null
+     */
+    public void setAttribute(String name, String value) {
+        if (name == null) {
+            throw new IllegalArgumentException("Null name parameter");
+        }
+
+        if (value != null) {
+            getAttributes().put(name, value);
+        } else {
+            getAttributes().remove(name);
+        }
+    }
+
+    /**
+     * Return the Control's attributes Map.
+     *
+     * @return the control's attributes Map.
+     */
+    public Map getAttributes() {
+        if (attributes == null) {
+            attributes = new HashMap();
+        }
+        return attributes;
+    }
+
+    /**
+     * Return true if the Control has attributes or false otherwise.
+     *
+     * @return true if the Control has attributes on false otherwise
+     */
+    public boolean hasAttributes() {
+        if (attributes != null) {
+            return !getAttributes().isEmpty();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @see Control#setListener(Object, String)
+     *
+     * @param listener the listener object with the named method to invoke
+     * @param method the name of the method to invoke
+     */
+    public void setListener(Object listener, String method) {
+        // Does nothing
+    }
+}
