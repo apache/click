161900db73abbfe44ab895aa6524cc710c55a529
malcolm_edgar <dev-null@apache.org>
2007-03-03 10:09:37 +0000

    formatting changes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716026 13f79535-47bb-0310-9956-ffa450edef68

--- click/examples/src/net/sf/click/examples/page/graph/PieChart.java
+++ click/examples/src/net/sf/click/examples/page/graph/PieChart.java
@@ -19,12 +19,12 @@ public class PieChart extends BorderPage {
         chart.setChartHeight(350);
         chart.setChartWidth(380);
 
-        chart.addPoint("Jan",new Integer(100));
-        chart.addPoint("Feb",new Integer(200));
-        chart.addPoint("Mar",new Integer(150));
-        chart.addPoint("Apr",new Integer(120));
-        chart.addPoint("May",new Integer(315));
-        chart.addPoint("Jun",new Integer(415));
-        chart.addPoint("Jul",new Integer(315));
+        chart.addPoint("Jan", new Integer(100));
+        chart.addPoint("Feb", new Integer(200));
+        chart.addPoint("Mar", new Integer(150));
+        chart.addPoint("Apr", new Integer(120));
+        chart.addPoint("May", new Integer(315));
+        chart.addPoint("Jun", new Integer(415));
+        chart.addPoint("Jul", new Integer(315));
     }
 }
