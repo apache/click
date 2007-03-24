13424cf12309aa208f369de1ddd54672a08510d6
Naoki Takezoe <takezoe@apache.org>
2007-03-24 11:51:40 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716094 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/eclipse/net.sf.clickide/src/net/sf/clickide/core/facet/ClickFacetUtil.java
@@ -0,0 +1,36 @@
+package net.sf.clickide.core.facet;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jst.j2ee.web.componentcore.util.WebArtifactEdit;
+
+/**
+ * 
+ * @author Naoki Takezoe
+ * @since 1.6.0
+ */
+public class ClickFacetUtil {
+	
+	public static String CLICK_DIR = "click-1.2";
+	
+	public static final String[] COPY_FILES = {
+		"/lib/click-1.2.jar",
+		"/lib/click-extras-1.2.jar",
+		"/click.xml",
+	};
+	
+	public static IPath getWebContentPath(IProject project) {
+		WebArtifactEdit web = null;
+		try {
+			web = WebArtifactEdit.getWebArtifactEditForRead(project);
+			IPath webxml = web.getDeploymentDescriptorPath();
+			//remove project name, WEB-INF an web.xml from path
+			IPath webContentPath = webxml.removeLastSegments(2).removeFirstSegments(1);
+			return webContentPath;
+		} finally {
+			if (web != null) {
+				web.dispose();
+			}
+		}
+	}
+}
