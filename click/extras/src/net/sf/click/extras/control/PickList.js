5e88a91a4f72f6a8a903f32f59d55d17ab2956a7
Naoki Takezoe <takezoe@apache.org>
2007-04-03 16:40:22 +0000

    keep item order.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716110 13f79535-47bb-0310-9956-ffa450edef68

--- click/extras/src/net/sf/click/extras/control/PickList.js
+++ click/extras/src/net/sf/click/extras/control/PickList.js
@@ -1,34 +1,44 @@
 function pickListMove(from, to, hidden, isSelected){
-  for(var i=0;i<from.options.length;i++){
-    if(from.options[i].selected){
-      pickListMoveItem(from, to, i, hidden, isSelected);
-      i--;
-    }
-  }
+	for(var i=0;i<from.options.length;i++){
+		if(from.options[i].selected){
+			pickListMoveItem(from, to, i, hidden, isSelected);
+			i--;
+		}
+	}
 }
 
 function pickListMoveAll(from, to, hidden, isSelected){
-  for(i=0;i<from.options.length;i++){
-    pickListMoveItem(from, to, i, hidden, isSelected);
-    i--;
-  }
+	for(i=0;i<from.options.length;i++){
+		pickListMoveItem(from, to, i, hidden, isSelected);
+		i--;
+	}
 }
 
 function pickListMoveItem(from, to, index, hidden, isSelected){
-  to.options[to.options.length] = new Option(from.options[index].text, from.options[index].value);
-  if(isSelected){
-    var option = new Option(from.options[index].text, from.options[index].value);
-    option.selected = true;
-    hidden.options[hidden.options.length] = option;
-  } else {
-    for(var i=0;i<hidden.options.length;i++){
-      if(hidden.options[i].value == from.options[index].value){
-        hidden.options[i] = null;
-        i--;
-      }
-    }
-  }
-  from.options[index] = null;
+	var insertIndex = 0;
+	if(isSelected){
+		for(var i=0;i<hidden.options.length;i++){
+			if(hidden.options[i].value == from.options[index].value){
+				hidden.options[i].selected = true;
+				insertIndex = i;
+			}
+		}
+	} else {
+		for(var i=0;i<hidden.options.length;i++){
+			if(hidden.options[i].value == from.options[index].value){
+				hidden.options[i].selected = false;
+				insertIndex = i;
+			}
+		}
+	}
+	for(var i=to.options.length-1;i>=insertIndex;i--){
+		to.options[i+1] = new Option(to.options[i].text, to.options[i].value);
+	}
+	if(insertIndex > to.options.length){
+		insertIndex = to.options.length;
+	}
+	to.options[insertIndex] = new Option(from.options[index].text, from.options[index].value);
+	from.options[index] = null;
 }
 
 function validatePickList(id, required, msgs){
@@ -48,4 +58,3 @@ function validatePickList(id, required, msgs){
 		return 'Field ' + id + ' not found.';
 	}
 }
-
