8dffc8e881756133f77c7b9b4e9e2e86944b1704
malcolm_edgar <dev-null@apache.org>
2007-04-18 23:47:43 +0000

    checkstyle fixes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716123 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/test/net/sf/click/util/Test1TextField.java
+++ click/framework/test/net/sf/click/util/Test1TextField.java
@@ -4,4 +4,6 @@ import net.sf.click.control.TextField;
 
 public class Test1TextField extends TextField {
 
+	private static final long serialVersionUID = 1L;
+
 }
--- click/framework/test/net/sf/click/util/Test2TextField.java
+++ click/framework/test/net/sf/click/util/Test2TextField.java
@@ -2,4 +2,6 @@ package net.sf.click.util;
 
 public class Test2TextField extends Test1TextField {
 
+	private static final long serialVersionUID = 1L;
+
 }
