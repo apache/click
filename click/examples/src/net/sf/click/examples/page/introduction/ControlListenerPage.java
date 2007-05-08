0d1efe3e8fe0a6a188bc20ef046b4884667b58e2
malcolm_edgar <dev-null@apache.org>
2007-05-08 11:36:49 +0000

    improved examples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716164 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/examples/src/net/sf/click/examples/page/introduction/ControlListenerPage.java
@@ -0,0 +1,33 @@
+package net.sf.click.examples.page.introduction;
+
+import net.sf.click.control.ActionLink;
+import net.sf.click.examples.page.BorderPage;
+
+/**
+ * Provides a control listener example Page.
+ *
+ * @author Malcolm Edgar
+ */
+public class ControlListenerPage extends BorderPage {
+
+    /* Public scope controls are automatically added to the page. */
+    public ActionLink myLink = new ActionLink(this, "onMyLinkClick");
+
+    /* Public scope variable are automatically added to the model. */
+    public String msg;
+
+    // --------------------------------------------------------- Event Handlers
+
+    /**
+     * Handle the myLink control click event.
+     *
+     * @return true
+     */
+    public boolean onMyLinkClick() {
+        msg = "ControlListenerPage#" + hashCode()
+            + " object method <tt>onMyLinkClick()</tt> invoked.";
+
+        return true;
+    }
+
+}
