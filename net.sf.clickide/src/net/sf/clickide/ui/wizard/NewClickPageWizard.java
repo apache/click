50ad93134408fe3af3f5c8c39750c5dd5b1972c2
Naoki Takezoe <takezoe@apache.org>
2007-09-17 14:44:36 +0000

    Multiple pages element is supported in the switch PageClass <-> HTML action and the New Page Creation Wizard.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/tools/eclipse@716417 13f79535-47bb-0310-9956-ffa450edef68

--- net.sf.clickide/src/net/sf/clickide/ui/wizard/NewClickPageWizard.java
+++ net.sf.clickide/src/net/sf/clickide/ui/wizard/NewClickPageWizard.java
@@ -68,7 +68,7 @@ public class NewClickPageWizard extends Wizard implements INewWizard {
 			IDialogSettings section = settings.addNewSection(SECTION_NEW_CLICK_PAGE);
 			section.put(SHOULD_CREATE_HTML, true);
 			section.put(SHOULD_CREATE_CLASS, true);
-			section.put(SHOULD_ADD_TO_CLICK_XML, true);
+			section.put(SHOULD_ADD_TO_CLICK_XML, false);
 			section.put(SUPERCLASS, "net.sf.click.Page");
 		}
 		setDialogSettings(settings.getSection(SECTION_NEW_CLICK_PAGE));
