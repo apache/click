be5ab54742bbcccf40359410dd8b04922ba1b73c
Bob Schellink <sabob@apache.org>
2007-10-06 22:18:11 +0000

    Removed dependencies of extras controls from ClickUtilsTest. The removed tests were placed in ExtrasClickUtilsTest in the extras project.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716440 13f79535-47bb-0310-9956-ffa450edef68

--- click/extras/test/net/sf/click/util/ExtrasClickUtilsTest.java
+++ click/extras/test/net/sf/click/util/ExtrasClickUtilsTest.java
@@ -1,23 +1,24 @@
 package net.sf.click.util;
 
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 import junit.framework.TestCase;
-import net.sf.click.Context;
 import net.sf.click.MockContext;
-import net.sf.click.Page;
 import net.sf.click.control.Checkbox;
 import net.sf.click.control.FieldSet;
 import net.sf.click.control.Form;
 import net.sf.click.control.HiddenField;
 import net.sf.click.control.TextField;
+import net.sf.click.extras.control.DateField;
+import net.sf.click.extras.control.DoubleField;
+import net.sf.click.extras.control.IntegerField;
+import net.sf.click.extras.control.LongField;
 
-public class ClickUtilsTest extends TestCase {
+public class ExtrasClickUtilsTest extends TestCase {
 
     private static final java.util.Date DATE_OF_BIRTH;
     private static final String NAME = "john smith";
@@ -31,7 +32,7 @@ public class ClickUtilsTest extends TestCase {
         Calendar calendar = new GregorianCalendar(2000, 01, 02);
         DATE_OF_BIRTH = calendar.getTime();
     }
-
+    
     protected void setUp() {
     	MockContext.initContext(Locale.ENGLISH);
     }
@@ -41,7 +42,7 @@ public class ClickUtilsTest extends TestCase {
     	// set up the form
         Form form = new Form("sample");
         
-        TextField idField = new TextField("id");
+        IntegerField idField = new IntegerField("id");
         form.add(idField);
 
         FieldSet fieldset = new FieldSet("fieldset");
@@ -49,25 +50,25 @@ public class ClickUtilsTest extends TestCase {
 
         TextField nameField = new TextField("name");
         fieldset.add(nameField);
-
-        TextField dateField = new TextField("dateOfBirth");
-        fieldset.add(dateField);
         
-        TextField intField = new TextField("int");
+        DateField dateField = new DateField("dateOfBirth");
+        fieldset.add(dateField); 
+        
+        IntegerField intField = new IntegerField("int");
         form.add(intField);
 
-        TextField doubleField = new TextField("double");
+        DoubleField doubleField = new DoubleField("double");
         form.add(doubleField);
                
         Checkbox checkBox = new Checkbox("boolean");
         form.add(checkBox);  
         
-        TextField telephoneField = new TextField("telephone");
+        LongField telephoneField = new LongField("telephone");
         form.add(telephoneField);
         
         HiddenField hidden = new HiddenField("hidden", String.class);
         form.add(hidden);
-        
+
         // Populate fields
         idField.setValueObject(ID);
         nameField.setValue(NAME);
@@ -82,16 +83,12 @@ public class ClickUtilsTest extends TestCase {
         ClickUtils.copyFormToObject(form, sampleObject, true);
 
         // has the object been configured correctly?
-        assertEquals(new Integer(idField.getValue()), sampleObject.getId());
+        assertEquals(idField.getInteger(), sampleObject.getId());
         assertEquals(nameField.getValue(), sampleObject.getName());
-        
-        //NOTE the dateField was NOT copied to the sampleObject's Date property.
-        //Use net.sf.click.extras.control.DateField in the extras project, to 
-        //copy a Date property.
-        assertEquals(null, sampleObject.getDateOfBirth());
+        assertEquals(dateField.getValueObject(), sampleObject.getDateOfBirth());
         assertEquals(telephoneField.getValueObject().toString(), sampleObject.getTelephone());
-        assertTrue(sampleObject.getInt() == new Integer(intField.getValue()).intValue());
-        assertTrue(sampleObject.getDouble() == new Double(doubleField.getValue()).doubleValue());
+        assertTrue(sampleObject.getInt() == intField.getInteger().intValue());
+        assertTrue(sampleObject.getDouble() == doubleField.getDouble().doubleValue());
         assertTrue(sampleObject.isBoolean() == checkBox.isChecked());
         
         // Test object path copying
@@ -112,7 +109,7 @@ public class ClickUtilsTest extends TestCase {
         // set up the form
         Form form = new Form("sample");
         
-        TextField idField = new TextField("id");
+        IntegerField idField = new IntegerField("id");
         form.add(idField);
 
         FieldSet fieldset = new FieldSet("fieldset");
@@ -121,13 +118,13 @@ public class ClickUtilsTest extends TestCase {
         TextField nameField = new TextField("name");
         fieldset.add(nameField);
         
-        TextField dateField = new TextField("dateOfBirth");
+        DateField dateField = new DateField("dateOfBirth");
         fieldset.add(dateField); 
         
-        TextField intField = new TextField("int");
+        IntegerField intField = new IntegerField("int");
         form.add(intField);
                 
-        TextField doubleField = new TextField("double");
+        DoubleField doubleField = new DoubleField("double");
         form.add(doubleField);
                
         Checkbox checkBox = new Checkbox("boolean");
@@ -149,15 +146,15 @@ public class ClickUtilsTest extends TestCase {
         ClickUtils.copyObjectToForm(sampleObject, form, true);
 
         // has the form been configured correctly?
-        assertEquals(sampleObject.getId(), new Integer(idField.getValue()));
+        assertEquals(sampleObject.getId(), idField.getInteger());
         assertEquals(sampleObject.getName(), nameField.getValue());
-        assertEquals(sampleObject.getDateOfBirth().toString(), dateField.getValue());
-        assertTrue(sampleObject.getInt() == new Integer(intField.getValue()).intValue());
-        assertTrue(sampleObject.getDouble() == new Double(doubleField.getValue()).doubleValue());
+        assertEquals(sampleObject.getDateOfBirth(), dateField.getDate());
+        assertTrue(sampleObject.getInt() == intField.getInteger().intValue());
+        assertTrue(sampleObject.getDouble() == doubleField.getDouble().doubleValue());
         assertTrue(sampleObject.isBoolean() == checkBox.isChecked());
-
+        
         // Test object path copying
-
+        
         User user = new User();
         user.setAddress(new Address());
         user.getAddress().setState(new State());
@@ -186,7 +183,7 @@ public class ClickUtilsTest extends TestCase {
         
         Form form = new Form();
         
-        TextField idField = new TextField("address.id");
+        IntegerField idField = new IntegerField("address.id");
         form.add(idField);
         TextField lineOneField = new TextField("address.lineOne");
         lineOneField.setValue(lineOne);
@@ -211,102 +208,4 @@ public class ClickUtilsTest extends TestCase {
         assertEquals(code, user.getAddress().getState().getCode());        
     }
 
-    public void testToLabel() {
-        assertEquals("Customer", ClickUtils.toLabel("customer"));
-        assertEquals("Customer Number", ClickUtils.toLabel("customerNumber"));
-        assertEquals("Card PIN", ClickUtils.toLabel("cardPIN"));
-    }
-    
-    public void testToMD5Hash() {
-        assertEquals("5f4dcc3b5aa765d61d8327deb882cf99", 
-                     ClickUtils.toMD5Hash("password"));
-    }
-    
-    public void testGetParentMessages() {
-        TextField textField = new TextField("test");
-        
-        Map map = ClickUtils.getParentMessages(textField);
-        assertNotNull(map);
-        assertTrue(map.isEmpty());
-        assertTrue(map == Collections.EMPTY_MAP);
-        
-        Page page = new Page();
-        page.addControl(textField);
-        
-        Map map2 = ClickUtils.getParentMessages(textField);
-        assertNotNull(map2);
-        assertEquals(1, map2.size());
-        assertFalse(map2 == Collections.EMPTY_MAP);
-        
-        Page page2 = new Page();
-        
-        Form form = new Form("form");
-        page2.addControl(form);
-        
-        TextField textField2 = new TextField("test");
-        form.add(textField2);
-
-        Map map3 = ClickUtils.getParentMessages(textField2);
-        assertNotNull(map3);
-        assertEquals(1, map3.size());
-        assertFalse(map3 == Collections.EMPTY_MAP);        
-    }
-
-    public void testEncodeUrl() {
-        String value = ClickUtils.encodeUrl("1000", Context.getThreadLocalContext());
-        assertEquals("1000", value);
-    }
-
-    public void testEscapeHtml() {
-        String value1 = "";
-        String value2 = ClickUtils.escapeHtml(value1);
-        assertEquals(value1, value2);
-        assertTrue(value1 == value2);
-
-        String value3 = "1234567890abcdefghijklmnopqrstuvwzyz";
-        String value4 = ClickUtils.escapeHtml(value3);
-        assertEquals(value3, value4);
-        assertTrue(value3 == value4);
-        
-        assertEquals("&quot;", ClickUtils.escapeHtml("\""));
-        assertEquals("&amp;", ClickUtils.escapeHtml("&"));
-        assertEquals("&lt;", ClickUtils.escapeHtml("<"));
-        assertEquals("&gt;", ClickUtils.escapeHtml(">"));
-        assertEquals("&gt;", ClickUtils.escapeHtml(">"));
-        assertEquals(" ", ClickUtils.escapeHtml(" "));
-        assertEquals("&agrave;", ClickUtils.escapeHtml("à"));
-        
-        assertFalse(ClickUtils.requiresEscape((char) 0));
-        assertTrue(ClickUtils.requiresEscape((char) 34));
-        assertTrue(ClickUtils.requiresEscape((char) 184));
-        assertFalse(ClickUtils.requiresEscape((char) 999999));
-    }
-
-    public void testGetMimeType() {
-        assertEquals("application/vnd.ms-excel", ClickUtils.getMimeType("worksheet.xls"));
-
-        try {
-	    assertNull(ClickUtils.getMimeType("broken.xxx"));
-        } catch (Exception e) {
-	    assertTrue(false);
-        }
-    }
-    
-    public void testCookiePassword() {
-        String username = "username";
-        String password = "password";
-        
-        String cookie = ClickUtils.encodePasswordCookie(username, password, 12);
-        
-        String[] result = ClickUtils.decodePasswordCookie(cookie, 12);
-        
-        assertEquals(username, result[0]);
-        assertEquals(password, result[1]);
-        
-        result = ClickUtils.decodePasswordCookie(cookie, 21);
-        
-        assertFalse(username.equals(result[0]));
-        assertFalse(password.equals(result[1]));        
-    }
-    
 }
