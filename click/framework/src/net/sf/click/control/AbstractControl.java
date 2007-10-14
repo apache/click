2e4d0589fb0bc2c548d7b7291f572e8af2e2772e
Bob Schellink <sabob@apache.org>
2007-10-14 11:53:45 +0000

    getStyle and setStyle now reads and writes directly to the underlying attributes map. CLK-249
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716468 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/src/net/sf/click/control/AbstractControl.java
+++ click/framework/src/net/sf/click/control/AbstractControl.java
@@ -17,12 +17,17 @@ package net.sf.click.control;
 
 import java.text.MessageFormat;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
 
 import net.sf.click.Context;
 import net.sf.click.Control;
 import net.sf.click.Page;
 import net.sf.click.util.ClickUtils;
+import net.sf.click.util.HtmlStringBuffer;
 import net.sf.click.util.MessagesMap;
 
 /**
@@ -49,7 +54,11 @@ public abstract class AbstractControl implements Control {
     /** The control's parent. */
     protected Object parent;
 
-    /** The Map of CSS style attributes. */
+    /**
+     * The Map of CSS style attributes.
+     *
+     * @deprecated
+     */
     protected Map styles;
 
     // ------------------------------------------------------ Public Attributes
@@ -61,8 +70,11 @@ public abstract class AbstractControl implements Control {
      * @param name the name of link HTML attribute
      * @return the link HTML attribute
      */
-    public String getAttribute(String name) {
-        return (String) getAttributes().get(name);
+     public String getAttribute(String name) {
+        if (hasAttributes()) {
+            return (String) getAttributes().get(name);
+        }
+        return null;
     }
 
     /**
@@ -74,11 +86,18 @@ public abstract class AbstractControl implements Control {
      *
      * <pre class="codeJava">
      * ActionLink addLink = <span class="kw">new</span> ActionLink(<span class="red">"addLink"</span>, <span class="st">"Add"</span>);
-     * addLink.setAttribute(<span class="st">"class"</span>, <span class="st">"table"</span>); </pre>
+     * addLink.setAttribute(<span class="st">"target"</span>, <span class="st">"_blank"</span>); </pre>
      *
      * Will render the HTML as:
      * <pre class="codeHtml">
-     * &lt;a href=".." <span class="st">class</span>=<span class="st">"table"</span>&gt;<span class="st">Add</span>&lt;/a&gt; </pre>
+     * &lt;a href=".." <span class="st">target</span>=<span class="st">"_blank"</span>&gt;<span class="st">Add</span>&lt;/a&gt; </pre>
+     *
+     * <b>Note:</b> for the <tt>style</tt> attribute please use
+     * {@link #setStyle(String, String)} and {@link #getStyle(String)}
+     * instead. These methods provides better support for working
+     * with this attribute.
+     *
+     * @see #setStyle(String, String)
      *
      * @param name the attribute name
      * @param value the attribute value
@@ -115,10 +134,20 @@ public abstract class AbstractControl implements Control {
      */
     public boolean hasAttributes() {
         if (attributes != null) {
-            return !getAttributes().isEmpty();
+            return !attributes.isEmpty();
         } else {
             return false;
         }
+    }
+
+    /**
+     * Returns true if specified attribute is defined, false otherwise.
+     *
+     * @param name the specified attribute to check
+     * @return true if name is a defined attribute
+     */
+    public boolean hasAttribute(String name) {
+        return hasAttributes() && getAttributes().containsKey(name);
     }
 
     /**
@@ -298,9 +327,10 @@ public abstract class AbstractControl implements Control {
      * @return the CSS style for the given name
      */
     public String getStyle(String name) {
-        if (hasStyles()) {
-            return (String) getStyles().get(name);
-
+        if (hasAttribute("style")) {
+            String currentStyles = getAttribute("style");
+            Map stylesMap = parseStyles(currentStyles);
+            return (String) stylesMap.get(name);
         } else {
             return null;
         }
@@ -308,6 +338,17 @@ public abstract class AbstractControl implements Control {
 
     /**
      * Set the control CSS style name and value pair.
+     * <p/>
+     * For example given the ActionLink:
+     *
+     * <pre class="codeJava">
+     * ActionLink addLink = <span class="kw">new</span> ActionLink(<span class="red">"addLink"</span>, <span class="st">"Add"</span>);
+     * addLink.setStyle(<span class="st">"color"</span>, <span class="st">"red"</span>);
+     * addLink.setStyle(<span class="st">"border"</span>, <span class="st">"1px solid black"</span>); </pre>
+     *
+     * Will render the HTML as:
+     * <pre class="codeHtml">
+     * &lt;a href=".." <span class="st">style</span>=<span class="st">"color:red;border:1px solid black;"</span>&gt;<span class="st">Add</span>&lt;/a&gt; </pre>
      *
      * @param name the CSS style name
      * @param value the CSS style value
@@ -317,15 +358,59 @@ public abstract class AbstractControl implements Control {
             throw new IllegalArgumentException("Null name parameter");
         }
 
-        if (value != null) {
-            getStyles().put(name, value);
-        } else {
-            getStyles().remove(name);
+        String currentStyles = getAttribute("style");
+
+        //If existing style and value is not null, just append the new style
+        if (currentStyles == null && value != null) {
+            getAttributes().put("style", name + ":" + value + ";");
+            return;
         }
+
+        //Create buffer and estimate the new size
+        HtmlStringBuffer buffer = new HtmlStringBuffer(
+            currentStyles.length() + 10);
+
+        //Parse the current styles into a map
+        Map stylesMap = parseStyles(currentStyles);
+
+        //Check if the new style is already present
+        if (stylesMap.containsKey(name)) {
+
+            //If the style is present and the value is null, remove the style, 
+            //otherwise replace it with the new value
+            if (value == null) {
+                stylesMap.remove(name);
+            } else {
+                stylesMap.put(name, value);
+            }
+        } else {
+            stylesMap.put(name, value);
+        }
+
+        //The styles map might be empty if the last style was removed
+        if (stylesMap.isEmpty()) {
+            getAttributes().remove("style");
+            return;
+        }
+
+        //Iterate over the stylesMap appending each entry to buffer
+        Iterator it = stylesMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry entry = (Entry) it.next();
+            String styleName = String.valueOf(entry.getKey());
+            String styleValue = String.valueOf(entry.getValue());
+            buffer.append(styleName);
+            buffer.append(":");
+            buffer.append(styleValue);
+            buffer.append(";");
+        }
+        getAttributes().put("style", buffer.toString());
     }
 
     /**
      * Return true if CSS styles are defined.
+     *
+     * @deprecated use {@link #hasAttribute("style")} instead
      *
      * @return true if CSS styles are defined
      */
@@ -336,6 +421,8 @@ public abstract class AbstractControl implements Control {
     /**
      * Return the Map of control CSS styles.
      *
+     * @deprecated use {@link #getAttribute(String)} instead
+     *
      * @return the Map of control CSS styles
      */
     public Map getStyles() {
@@ -345,4 +432,55 @@ public abstract class AbstractControl implements Control {
         return styles;
     }
 
+    // ----------------------------------protected methods
+
+    /**
+     * Append all the controls attributes to the specified buffer.
+     *
+     * @param buffer the specified buffer to append all the attributes
+     */
+    protected void appendAttributes(HtmlStringBuffer buffer) {
+        if (hasAttributes()) {
+            buffer.appendAttributes(attributes);
+        }
+    }
+
+    /**
+     * Parse the specified string of style attributes and return a Map
+     * of key/value pairs. Invalid key/value pairs will not be added to
+     * the map.
+     *
+     * @param style the string containing the styles to parse
+     * @return map containing key/value pairs of the specified style
+     */
+    protected Map parseStyles(String style) {
+        if (style == null) {
+            throw new IllegalArgumentException("style cannot be null");
+        }
+
+        //LinkHashMap is used to keep the order of the style names. Probably
+        //makes no difference to browser but it makes testing easier since the
+        //order that styles are added are kept when rendering the control.
+        Map stylesMap = new LinkedHashMap();
+        StringTokenizer tokens = new StringTokenizer(style, ";");
+        while (tokens.hasMoreTokens()) {
+            String token = tokens.nextToken();
+            int keyEnd = token.indexOf(":");
+
+            //If there is no key/value delimiter or value is empty, continue
+            if (keyEnd == -1 || (keyEnd + 1) == token.length()) {
+                continue;
+            }
+            //Check that the value part of the key/value pair not only
+            //consists of  a ';' char.
+            if (token.charAt(keyEnd + 1) == ';') {
+                continue;
+            }
+            String name = token.substring(0, keyEnd);
+            String value = token.substring(keyEnd + 1);
+            stylesMap.put(name, value);
+        }
+
+        return stylesMap;
+    }
 }
