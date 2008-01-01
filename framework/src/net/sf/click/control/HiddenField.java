0e60800e6b55fad908e84524bb5c965c76cefd8d
malcolm_edgar <dev-null@apache.org>
2008-01-01 10:28:50 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@716512 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/click/control/HiddenField.java
+++ framework/src/net/sf/click/control/HiddenField.java
@@ -216,9 +216,9 @@ public class HiddenField extends Field {
     public void setValueObject(Object value) {
         if ((value != null) && (value.getClass() != valueClass)) {
             String msg =
-                "The value.getClass() must be the same as the "
-                + "HiddenField valueClass: "
-                + ((valueClass != null) ? valueClass.getName() : "null");
+                "The value.getClass(): '" + value.getClass().getName()
+                + "' must be the same as the HiddenField valueClass: '"
+                + ((valueClass != null) ? valueClass.getName() : "null") + "'";
 
             throw new IllegalArgumentException(msg);
         }
