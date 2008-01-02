48c559dc55691b938539d0cc4b766e03ba85f547
Bob Schellink <sabob@apache.org>
2008-01-02 18:43:53 +0000

    Restored base *.properties and added tests to check that properties are picked up correctly.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@716517 13f79535-47bb-0310-9956-ffa450edef68

--- framework/test/net/sf/click/util/MessagesMapTest.java
+++ framework/test/net/sf/click/util/MessagesMapTest.java
@@ -1,8 +1,11 @@
 package net.sf.click.util;
 
+import java.lang.reflect.Field;
 import java.util.Locale;
+import java.util.Map;
 import java.util.MissingResourceException;
 
+import java.util.ResourceBundle;
 import junit.framework.TestCase;
 import net.sf.click.Control;
 import net.sf.click.MockContext;
@@ -101,7 +104,7 @@ public class MessagesMapTest extends TestCase {
         assertEquals("Test1TextField", map.get("name"));
         assertEquals("Test2TextField", map.get("classname"));
     }
-    
+
     /**
      * CLK-274
      * 
@@ -123,5 +126,99 @@ public class MessagesMapTest extends TestCase {
         MessagesMap map = new MessagesMap(Object.class, "click-control");
         assertFalse(map.isEmpty());
         assertEquals("First", map.get("table-first-label"));
-}
+    }
+
+    /**
+     * CLK-269
+     * 
+     * Test that the base properties will be picked up when the specified
+     * locale and default locale are non-existent. 
+     * 
+     * Also test that the base properties are picked up when default locale is 
+     * non-existent and specified locale is English.
+     */
+    public void testBasePropertiesUsingNonExistentDefaultLocale() {
+        Locale locale = new Locale("xx", "XX");//bogus locale
+
+        //Set bogus as the default locale
+        Locale.setDefault(locale);
+
+        MockContext.initContext(locale);
+        MessagesMap nonEnglishMessages = 
+          new ReloadableMessagesMap(Object.class, "click-control");
+        assertFalse(nonEnglishMessages.isEmpty());
+        
+        //Test that property from click-control.properties are picked up
+        //when locale is bogus locale
+        assertEquals("First", nonEnglishMessages.get("table-first-label"));
+
+        //Test that property from click-control.properties are picked up
+        //when specified locale is English and default Locale cannot be found
+        MockContext.initContext(Locale.ENGLISH);
+        MessagesMap englishMessages = new ReloadableMessagesMap(Object.class, 
+          "click-control");
+        assertFalse(englishMessages.isEmpty());
+        assertEquals("First", englishMessages.get("table-first-label"));
+    }
+
+    /**
+     * CLK-269
+     * 
+     * Test that the English locale properties will be picked up when the 
+     * default locale is French.
+     */
+    public void testEnglishMessagesUsingFrenchDefaultLocale() {
+        Locale locale = new Locale("fr", "FR");
+
+        //Set French as default locale
+        Locale.setDefault(locale);
+
+        MockContext.initContext(locale);
+        MessagesMap nonEnglishMessages = 
+          new ReloadableMessagesMap(Object.class, "click-control");
+        assertFalse(nonEnglishMessages.isEmpty());
+
+        //Test that French property is picked up
+        assertEquals("Première", nonEnglishMessages.get("table-first-label"));
+
+        //While using a default French locale, test that a English specified
+        //locale picks up properties from click-control.properties
+        MockContext.initContext(Locale.ENGLISH);
+        MessagesMap englishMessages = new ReloadableMessagesMap(Object.class, 
+          "click-control");
+        assertFalse(englishMessages.isEmpty());
+        assertEquals("First", englishMessages.get("table-first-label"));
+    }
+
+    /**
+     * This messagesMap subclass will clear its static cached properties each
+     * time a new instance is created. The properties will then be reloaded.
+     */
+    private class ReloadableMessagesMap extends MessagesMap {
+
+        public ReloadableMessagesMap(Class baseClass, String globalResource) {
+            super(baseClass, globalResource);
+            clearCache();
+        }
+
+        public void clearCache() {
+            MESSAGES_CACHE.clear();
+            clearResourceBundleCache();
+        }
+
+        private void clearResourceBundleCache() {
+            try {
+                Class type = ResourceBundle.class;
+                Field cacheList = type.getDeclaredField("cacheList");
+                cacheList.setAccessible(true);
+                ((Map) cacheList.get(ResourceBundle.class)).clear();
+                cacheList.setAccessible(false);
+            } catch (Exception jvmNotSupported) {
+                System.out.println("WARNING: Could not clear the MessagesMap " +
+                  "cache. This could lead to some test cases failing, because " +
+                  "cached properties from previous tests could be returned.");
+            }
+
+        }
+    }
 }
