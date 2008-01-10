c4d7a651817f9f64490dc9e497d209ff73aacb21
malcolm_edgar <dev-null@apache.org>
2008-01-10 23:06:59 +0000

    copyright update
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@716565 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/click/util/PageImports.java
+++ framework/src/net/sf/click/util/PageImports.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 Malcolm A. Edgar
+ * Copyright 2004-2008 Malcolm A. Edgar
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
