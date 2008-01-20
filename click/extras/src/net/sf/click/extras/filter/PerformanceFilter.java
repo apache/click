274529a484ec2c63a83f4bbdda8337211b073448
Bob Schellink <sabob@apache.org>
2008-01-20 22:35:26 +0000

    updated javadoc for PerformanceFilter. CLK-290
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716631 13f79535-47bb-0310-9956-ffa450edef68

--- click/extras/src/net/sf/click/extras/filter/PerformanceFilter.java
+++ click/extras/src/net/sf/click/extras/filter/PerformanceFilter.java
@@ -49,9 +49,8 @@ import org.w3c.dom.NodeList;
  * <p/>
  * Please see Yahoo's <a href="http://developer.yahoo.com/performance/rules.html">Exceptional Performance</a>
  * best practices for speeding up your web site. This filter will enable you to
- * apply rule:
+ * apply the rules:
  * <ul>
- * <li>Rule 1 - Make Fewer HTTP Requests</li>
  * <li>Rule 3 - Add an Expires Header</li>
  * <li>Rule 4 - Gzip Components</li>
  * </ul>
@@ -63,6 +62,175 @@ import org.w3c.dom.NodeList;
  * <li>Rule 6 - Put Scripts at the Bottom, by using $jsImports at the bottom
  * of your page</li>
  * </ul>
+ *
+ * <h3>How does compression work?</h3>
+ * Compression only works if the browser supports gzip and the resource content
+ * is bigger than a  certain threshold (see below).
+ * <p/>
+ * If compression can be applied the response output of non image resources
+ * handled by the filter will be gzipped before output to the browser.
+ * <p/>
+ * The browser will receive the gzipped resources, unzip it, and
+ * display in its original form again.
+ *
+ * <h3>How does caching work?</h3>
+ * First off, for a good explanation of how browsers and caching work read the
+ * following
+ * <a href="http://betterexplained.com/articles/how-to-optimize-your-site-with-http-caching/" action="_blank">article.</a>
+ * <p/>
+ * Only configured resource (see below) will have expiry headers added. Once an
+ * expiry header is added for a resource, the browser will not contact the
+ * server again to check if the resource content has changed. Only when the
+ * expiry date is reached will the browser ask the server for the new resource.
+ * <p/>
+ * The downside is that even if the resource content is changed the browser will
+ * not receive the updated version.
+ * <p/>
+ * This is where <tt>version indicators</tt> comes in.
+ *
+ * <h3>How does version indicators work?</h3>
+ * A <tt>version indicator</tt> is a string that is added to the resource name.
+ * If the resource content changed, the <tt>version indicator</tt> must also
+ * change (preferably incremented). The browser will then download the new
+ * resource and cache it under its new name.
+ * <p/>
+ * <b>Please note:</b> <tt>version indicators</tt> are only generated and used
+ * at runtime. The physical resource (file) is never affected.
+ * <p/>
+ * Lets look at an example. Say we have a javascript file called
+ * '/click-examples/click/control.js'. Below is how this resource will
+ * be rendered when this filter is <b>not</b> applied:
+ *
+ * <pre class="codeHtml">
+ * &lt;script type=<span class="st">"text/javascript"</span> src=<span class="st">"/click-examples/click/control.js"</span>&gt;&lt;/script&gt;
+ * </pre>
+ *
+ * and here is how '/click-examples/click/control.js' will be rendered when the
+ * filter <b>is</b> applied:
+ *
+ * <pre class="codeHtml">
+ * &lt;script type=<span class="st">"text/javascript"</span> src=<span class="st">"/click-examples/click/control<span class="red">-1.4</span>.js"</span>&gt;&lt;/script&gt;
+ * </pre>
+ *
+ * Notice the <tt>version indicator</tt> <tt>'-1.4'</tt> was added to the
+ * resource path. When the resource content is changed the
+ * <tt>version indicator</tt> needs to change as well:
+ *
+ * <pre class="codeHtml">
+ * &lt;script type=<span class="st">"text/javascript"</span> src=<span class="st">"/click-examples/click/control<span class="red">-1.5</span>.js"</span>&gt;&lt;/script&gt;
+ * </pre>
+ *
+ * Since the real physical resource does not have a <tt>version indicator</tt>,
+ * it must be stripped from the path before this filter continues down the
+ * chain.
+ * <p/>
+ * In the example above the javascript file is located at
+ * <tt>'/click-examples/click/control.js'</tt>. The <tt>version indicator</tt>
+ * <tt>'-1.4'</tt> must be removed from the path, otherwise the server will try
+ * and find a file called <tt>'/click-examples/click/control-1.4.js'</tt> which
+ * does not exist.
+ * <p/>
+ * See the method {@link #stripResourceVersionIndicator(String)}.
+ * <p/>
+ * For custom controls you can provide alternative <tt>version indicators</tt>.
+ * For example you might want to <tt>version</tt> based on your application's
+ * version number or a resource's last modified date.
+ * <p/>
+ * Please see the following resources for more information:
+ * <ul>
+ *   <li>{@link net.sf.click.control.AbstractControl#getResourceVersionIndicator()}</li>
+ *   <li>{@link net.sf.click.Control#getHtmlImports()}</li>
+ *   <li>{@link net.sf.click.util.ClickUtils#createHtmlImport(String, String, Context)}</li>
+ *   <li>{@link #getResourceVersionIndicator(String)}</li>
+ *   <li>{@link #stripResourceVersionIndicator(String)}</li>
+ * </ul>
+ * <b>Remember</b> that <tt>version indicators</tt> are generated and used only
+ * at runtime. So if you override
+ * {@link net.sf.click.control.AbstractControl#getResourceVersionIndicator()}
+ * and generate custom <tt>version indicators</tt>, you must also strip the
+ * <tt>indicator</tt> from the resource path before the filter continues down
+ * the chain. You do this by providing a custom implementation of this filter's
+ * {@link #stripResourceVersionIndicator(String)}
+ * and / or {@link #getResourceVersionIndicator(String)}.
+ *
+ * <h3>Can you provide an example of how version indicators work?</h3>
+ *
+ * Say the browser contacts the server to download a specific page for example
+ * <tt>'/click-examples/click/home.htm'</tt>. The request is forwarded to
+ * ClickServlet and the page and all its controls are rendered.
+ * <p/>
+ * When a {@link net.sf.click.Control} is rendered, its method
+ * {@link net.sf.click.Control#getHtmlImports()} is called to generate paths
+ * to external resources.
+ * <p/>
+ * If the application is running in <tt>production</tt> or <tt>profile</tt> mode
+ * and the attribute {@link net.sf.click.ClickServlet#ENABLE_RESOURCE_VERSION}
+ * is set to "true", {@link net.sf.click.Control#getHtmlImports()} will add a
+ * <tt>version indicator</tt> to the resource path.
+ * <p/>
+ * Here is an example resource path that could be generated by
+ * <tt>getHtmlImports()</tt>:
+ *
+ * <pre class="codeHtml">
+ * &lt;script type=<span class="st">"text/javascript"</span> src=<span class="st">"/click-examples/click/control<span class="red">-1.4</span>.js"</span>&gt;&lt;/script&gt;
+ * </pre>
+ *
+ * The generated page content is delivered back to the browser. The browser
+ * notices the external file referenced in the <tt>&lt;script&gt;</tt> tag.
+ * <p/>
+ * If the browser does not have the specified resource in its cache, it must
+ * be downloaded from the server. The browser opens a connection to the server
+ * and asks for the resource named '/click-examples/click/control-1.4.js'.
+ * <p/>
+ * The browser's request is first processed by the PerformanceFilter. The
+ * PerformanceFilter checks if it needs to process the request resource path.
+ * If the resource path is configured to be processed, the filter adds an
+ * expiry headers to the response so that the browser will cache the resource.
+ * <p/>
+ * Next the PerformanceFilter removes the <tt>version indicator</tt> from the
+ * path and forwards the request to the real path
+ * <tt>'/click-examples/click/control.js'</tt>.
+ * <p/>
+ * The server finds the resource and delivers the javascript file to the browser
+ * along with the expiry headers.
+ * <p/>
+ * Next time the same external file is referenced, the browser will use its
+ * cached copy of '/click-examples/click/control-1.4.js'.
+ * <p/>
+ * Now say the resource <tt>'control.js'</tt> content is changed and the
+ * <tt>version indicator</tt> for the javascript file is incremented.
+ * The resource path might now be:
+ *
+ * <pre class="codeHtml">
+ * &lt;script type=<span class="st">"text/javascript"</span> src=<span class="st">"/click-examples/click/control<span class="red">-1.5</span>.js"</span>&gt;&lt;/script&gt;
+ * </pre>
+ *
+ * When the browser again request the page <tt>'/click-examples/click/home.htm'</tt>,
+ * it notices that the resource <tt>'/click-examples/click/control-1.5.js'</tt>
+ * is not available in its cache.
+ * <p/>
+ * The browser will ask the server for the new resource and the cycle continues.
+ * <p/>
+ * PerformanceFilter will strip the <tt>version indicator</tt> <tt>'-1.5'</tt>
+ * from the path and forward the request to the path
+ * <tt>'/click-examples/click/control.js'</tt>, where the updated content will
+ * be returned to the browser.
+ *
+ * <h3>When does it work?</h3>
+ * PerformanceFilter only works in <tt>production</tt> and <tt>profile</tt>
+ * modes. In development modes, this filter will simply pass through to the
+ * ClickServlet without adding expiry headers or compressing.
+ * <p/>
+ * This ensures a smoother development experience by not having to worry about
+ * server and browser resources being out of sync. In development mode, simply
+ * edit a javascript or style sheet and the browser will pick up the latest
+ * version.
+ * <p/>
+ * <b>NOTE:</b> <tt>versioned indicators</tt> are only generated by Click in
+ * <tt>production</tt> and <tt>profile</tt> mode, <b>and</b> when the attribute
+ * {@link net.sf.click.ClickServlet#ENABLE_RESOURCE_VERSION} is set to
+ * <tt>"true"</tt>. PerformanceFilter will set this attribute automatically in
+ * <tt>production</tt> and <tt>profile</tt> mode.
  *
  * <h3>Click Static Resources</h3>
  * This filter will automatically add long expiry headers to static Click
@@ -77,7 +245,7 @@ import org.w3c.dom.NodeList;
  * <h3>Configured Resources</h3>
  * Click *.htm pages are automatically compressed by the filter.
  * <p/>
- * You can also configure your own applications static resources such CSS, JS
+ * You can also configure your own applications static resources such as CSS, JS
  * files and images to be processed by the filter.
  *
  * Provides a GZIP compression <tt>Filter</tt> to compress HTML ServletResponse
@@ -88,7 +256,7 @@ import org.w3c.dom.NodeList;
  * <h3>Configuration</h3>
  *
  * To configure your application to use the PerformanceFilter include the
- * click-extras.jar in you application and add the following filter elements to
+ * click-extras.jar in your application and add the following filter elements to
  * your <tt>/WEB-INF/web.xml</tt> file:
  * <pre class="codeConfig">
  * &lt;filter&gt;
@@ -170,7 +338,7 @@ public class PerformanceFilter implements Filter {
 
     /**
      * The application mode value:
-     * "production", "profile", "development", "debug", "trace"
+     * "production", "profile", "development", "debug", "trace".
      */
     protected String modeValue;
 
@@ -189,6 +357,7 @@ public class PerformanceFilter implements Filter {
     /** The cachable-path include files. */
     protected List includeFiles = new ArrayList();
 
+    /** The filter logger. */
     protected ClickLogger logger = new ClickLogger("PerformanceFilter");
 
     // --------------------------------------------------------- Public Methods
@@ -374,21 +543,51 @@ public class PerformanceFilter implements Filter {
     // ------------------------------------------------------ Protected Methods
 
     /**
+     * Return the <tt>version indicator</tt> for the specified path.
+     * <p/>
+     * By default this method will delegate to
+     * {@link net.sf.click.util.ClickUtils#getResourceVersionIndicator()}.
+     * <p/>
+     * You can override this method to provide an alternative implementation.
+     * For example provide a <tt>version indicator</tt> based on your
+     * <tt>application version</tt> or a resource's <tt>last modified date</tt>.
+     *
+     * @see net.sf.click.control.AbstractControl#getResourceVersionIndicator()
+     * @see #stripResourceVersionIndicator(String)
+     * @see #stripResourceVersionIndicator(String)
+     *
+     * @param path the resource path
+     * @return a version indicator for web resources
+     */
+    protected String getResourceVersionIndicator(String path) {
+        return ClickUtils.getResourceVersionIndicator();
+    }
+
+    /**
      * Removes the version indicator from the specified path.
      * <p/>
      * For example, given the path '/example/control-1.4.js, where '-1.4' is
      * the version indicator, this method will return '/example/control.js'.
+     * <p/>
+     * <b>Please note</b> a <tt>version indicator</tt> is generated and used
+     * only at runtime, thus the physical resource name does not contain a
+     * <tt>version indicator</tt>.
+     * <p/>
+     * This method is used to strip off the <tt>version indicator</tt> from the
+     * path so that the path points to the actual physical resource.
+     *
+     * @see #getResourceVersionIndicator(String)
      *
      * @param path the resource path
      * @return path without the version indicator
      */
     protected String stripResourceVersionIndicator(String path) {
         int versionIndex =
-            path.lastIndexOf(ClickUtils.getResourceVersionIndicator());
+            path.lastIndexOf(getResourceVersionIndicator(path));
 
         if (versionIndex >= 0) {
             String extension = path.substring(
-                versionIndex + ClickUtils.getResourceVersionIndicator().length());
+                versionIndex + getResourceVersionIndicator(path).length());
             return path.substring(0, versionIndex) + extension;
         }
         return path;
@@ -417,7 +616,7 @@ public class PerformanceFilter implements Filter {
             } else {
                 return null;
             }
-            
+
         } finally {
             ClickUtils.close(inputStream);
         }
@@ -478,7 +677,7 @@ public class PerformanceFilter implements Filter {
      * @return true if the response should be cached forever
      */
     protected boolean useForeverCacheHeader(String path) {
-        String versionIndicator = ClickUtils.getResourceVersionIndicator();
+        String versionIndicator = getResourceVersionIndicator(path);
         return path.startsWith("/click/") && path.indexOf(versionIndicator) != -1;
     }
 
