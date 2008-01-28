999bcea615038f5080e722f35b8b9447631d1713
Naoki Takezoe <takezoe@apache.org>
2008-01-28 17:43:31 +0000

    Initial Import.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716666 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/takezoe/net.sf.clickide.cayenne/src/net/sf/clickide/cayenne/CayenneModelerLauncher.java
@@ -0,0 +1,16 @@
+package net.sf.clickide.cayenne;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.ui.IEditorLauncher;
+
+/**
+ * 
+ * @author Naoki Takezoe
+ */
+public class CayenneModelerLauncher implements IEditorLauncher {
+
+	public void open(IPath file) {
+//		Main.main(new String[]{file.makeAbsolute().toString()});
+	}
+
+}
