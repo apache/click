f2fb9a08ab6dc0112f89c47f83280860493b2c4c
malcolm_edgar <dev-null@apache.org>
2008-01-30 12:05:13 +0000

    fixed HTML imports JavaScript issues
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@716684 13f79535-47bb-0310-9956-ffa450edef68

--- extras/src/net/sf/click/extras/graph/JSLineChart.java
+++ extras/src/net/sf/click/extras/graph/JSLineChart.java
@@ -15,14 +15,6 @@
  */
 package net.sf.click.extras.graph;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletContext;
-
-import net.sf.click.control.AbstractControl;
-import net.sf.click.util.ClickUtils;
-import net.sf.click.util.HtmlStringBuffer;
 
 /**
  * Provides a Line Chart control based on JavaScript only.
@@ -40,7 +32,7 @@ import net.sf.click.util.HtmlStringBuffer;
  *
  * @author Ahmed Mohombe
  */
-public class JSLineChart extends AbstractControl {
+public class JSLineChart extends JSChart {
 
     private static final long serialVersionUID = 1L;
 
@@ -54,20 +46,6 @@ public class JSLineChart extends AbstractControl {
             "/net/sf/click/extras/graph/jsgraph/wz_jsgraphics.js",
             "/net/sf/click/extras/graph/jsgraph/line.js"
     };
-
-    // ----------------------------------------------------- Instance Variables
-
-    /** Width of the DIV element that encloses this chart. */
-    private int chartWidth = 400; // default value for width
-
-    /** Height of the DIV element that encloses this chart. */
-    private int chartHeight = 300; // default value for height
-
-    private List xLabels = new ArrayList();
-    private List yValues = new ArrayList();
-
-    /** The chart display label. */
-    private String label = "Line Chart"; // default label
 
     // ----------------------------------------------------------- Constructors
 
@@ -99,184 +77,39 @@ public class JSLineChart extends AbstractControl {
         setLabel(label);
     }
 
-    // --------------------------------------------------------- Public Methods
+    // ------------------------------------------------------ Protected Methods
 
     /**
-     * Adds a "point" to the grapic/chart at the end of the list.
+     * Return the list of static chart resources to deploy.
      *
-     * @param pointLabel the displayed label of the "point"
-     * @param pointValue the value of the "point".
+     * @see JSChart#getChartResources()
+     *
+     * @return the list of static chart resources to deploy
      */
-    public void addPoint(String pointLabel, Integer pointValue) {
-        xLabels.add(pointLabel);
-        yValues.add(pointValue);
+    protected String[] getChartResources() {
+        return CHART_RESOURCES;
     }
 
     /**
-     * Adds a "point" to the grapic/chart at a specified position in the list.
+     * Return the HTML imports pattern string.
      *
-     * @param index index at which the specified point is to be inserted
-     * @param pointLabel the displayed label of the "point"
-     * @param pointValue the value of the "point".
+     * @see JSChart#getHtmlImportPattern()
+     *
+     * @return the HTML imports pattern string
      */
-    public void addPoint(int index, String pointLabel, Integer pointValue) {
-        xLabels.add(index, pointLabel);
-        yValues.add(index, pointValue);
+    protected String getHtmlImportPattern() {
+        return HTML_IMPORTS;
     }
 
     /**
-     * Return the width of the chart (the enclosing DIV element).
+     * Return the JavaScript Chart type.
      *
-     * @return the width of the chart
+     * @see JSChart#getJSChartType()
+     *
+     * @return the JavaScript Chart type
      */
-    public int getChartWidth() {
-        return chartWidth;
+    protected String getJSChartType() {
+        return "line_graph";
     }
 
-    /**
-     * Set the width of the chart (of the enclosing DIV element), as a
-     * pixel value.
-     *
-     * @param chartWidth the chart width in pixels.
-     */
-    public void setChartWidth(int chartWidth) {
-        this.chartWidth = chartWidth;
-    }
-
-    /**
-     * Return the height of the chart (the enclosing DIV element).
-     *
-     * @return the height of the chart
-     */
-    public int getChartHeight() {
-        return chartHeight;
-    }
-
-    /**
-     * Set the height of the chart (of the enclosing DIV element), as a
-     * pixel value.
-     *
-     * @param chartHeight the chart height in pixels.
-     */
-    public void setChartHeight(int chartHeight) {
-        this.chartHeight = chartHeight;
-    }
-
-    /**
-     * Return the label of the chart.
-     *
-     * @return the label of the chart
-     */
-    public String getLabel() {
-        return label;
-    }
-
-    /**
-     * Set the chart display caption.
-     *
-     * @param label the display label of the chart
-     */
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    /**
-     * Return the HTML head import statements for the javascript files
-     * used by this control.
-     *
-     * @see net.sf.click.Control#getHtmlImports()
-     *
-     * @return the HTML head import statements for the javascript files
-     * used by this control.
-     */
-    public String getHtmlImports() {
-        return ClickUtils.createHtmlImport(HTML_IMPORTS, getContext());
-    }
-
-    /**
-     * This method does nothing.
-     *
-     * @see net.sf.click.Control#setListener(Object, String)
-     *
-     * @param listener the listener object with the named method to invoke
-     * @param method the name of the method to invoke
-     */
-    public void setListener(Object listener, String method) {
-        // do nothing
-    }
-
-    /**
-     * Deploys the javascript files of this control to the <code>[click/graph/jsgraph]</code> directory.
-     *
-     * @see net.sf.click.Control#onDeploy(ServletContext)
-     *
-     * @param servletContext the webapplication's servlet context
-     */
-    public void onDeploy(ServletContext servletContext) {
-        ClickUtils.deployFiles(servletContext,
-                               CHART_RESOURCES,
-                               "click/graph/jsgraph");
-    }
-
-    /**
-     * This method does nothing. Subclasses may override this method to perform
-     * additional initialization.
-     *
-     * @see net.sf.click.Control#onInit()
-     */
-    public void onInit() {
-    }
-
-    /**
-     * Returns true, as javascript charts perform no server side logic.
-     *
-     * @return true
-     */
-    public boolean onProcess() {
-        return true;
-    }
-
-    /**
-     * This method does nothing.
-     *
-     * @see net.sf.click.Control#onRender()
-     */
-    public void onRender() {
-    }
-
-    /**
-     * This method does nothing.
-     *
-     * @see net.sf.click.Control#onDestroy()
-     */
-    public void onDestroy() {
-    }
-
-    /**
-     * Return the HTML rendered line chart.
-     *
-     * @return the HTML rendered line chart string
-     */
-    public String toString() {
-        HtmlStringBuffer buffer = new HtmlStringBuffer();
-
-        buffer.elementStart("div");
-        buffer.appendAttribute("id", getId());
-        buffer.appendAttribute("style", "overflow: auto; position:relative;height:" + getChartHeight() + "px;width:" + getChartWidth() + "px;");
-        buffer.closeTag();
-        buffer.elementEnd("div");
-
-        String var = "g_" + getId();
-        buffer.append("\n<script type=\"text/javascript\">\n");
-        buffer.append("var " + var + " = new line_graph();\n");
-        for (int i = 0; i < xLabels.size(); i++) {
-            String pointLabel = (String) xLabels.get(i);
-            Integer pointValue = (Integer) yValues.get(i);
-            buffer.append(var + ".add('" + pointLabel + "'," + pointValue + ");\n");
-        }
-        buffer.append(var + ".render('" + getId() + "','" + getLabel() + "');\n");
-        buffer.append("</script> \n");
-
-        return buffer.toString();
-    }
 }
