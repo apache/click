cc1099fadddbdf5f39b34b02a16311903d828d0b
Naoki Takezoe <takezoe@apache.org>
2008-02-09 05:37:19 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/tools/eclipse@716714 13f79535-47bb-0310-9956-ffa450edef68

---
+++ net.sf.clickide/src/net/sf/clickide/ui/wizard/ClickFacetWizardPage.java
@@ -0,0 +1,49 @@
+package net.sf.clickide.ui.wizard;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.common.project.facet.ui.AbstractFacetWizardPage;
+
+/**
+ * 
+ * @author Naoki Takezoe
+ */
+public class ClickFacetWizardPage extends AbstractFacetWizardPage {
+	
+	private IDataModel model;
+	private Button useSpring;
+	private Button useCayenne;
+	
+	public static final String USE_SPRING = "useSpring";
+	public static final String USE_CAYENNE = "useCayenne";
+	
+	public ClickFacetWizardPage() {
+		super("ClickFacetWizardPage");
+		setTitle("Options");
+		setDescription("Select additinal features for the Click project.");
+	}
+
+	public void setConfig(Object config) {
+		this.model = (IDataModel) config;
+	}
+
+	public void createControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		composite.setLayout(new GridLayout());
+		useSpring = new Button(composite, SWT.CHECK);
+		useSpring.setText("Use Spring Framework");
+		
+		useCayenne = new Button(composite, SWT.CHECK);
+		useCayenne.setText("Use Apache Cayenne");
+		
+		setControl(composite);
+	}
+
+	public void transferStateToConfig() {
+		model.setBooleanProperty(USE_SPRING, useSpring.getSelection());
+		model.setBooleanProperty(USE_CAYENNE, useCayenne.getSelection());
+	}
+}
