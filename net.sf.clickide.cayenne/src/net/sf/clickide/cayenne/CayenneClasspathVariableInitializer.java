b5c47f4d984ca586eebc19f2a731a7f1f19fab6c
Naoki Takezoe <takezoe@apache.org>
2008-02-12 02:30:49 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/tools/eclipse@716731 13f79535-47bb-0310-9956-ffa450edef68

---
+++ net.sf.clickide.cayenne/src/net/sf/clickide/cayenne/CayenneClasspathVariableInitializer.java
@@ -0,0 +1,45 @@
+package net.sf.clickide.cayenne;
+
+import java.io.File;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.ClasspathVariableInitializer;
+import org.eclipse.jdt.core.JavaCore;
+
+/**
+ * Provides classpath variables <tt>CAYENNE_LIB</tt> and <tt>CAYENNE_NODEPS_LIB</tt>.
+ * 
+ * @author Naoki Takezoe
+ */
+public class CayenneClasspathVariableInitializer extends
+		ClasspathVariableInitializer {
+
+	public static final String VAR_CAYENNE_LIB = "CAYENNE_LIB";
+	public static final String VAR_CAYENNE_NODEPS_LIB = "CAYENNE_NODEPS_LIB";
+
+	public void initialize(String variable) {
+		try {
+			URL url = CayennePlugin.getDefault().getBundle().getEntry("/");
+			URL local = FileLocator.toFileURL(url);
+			
+			{ //cayenne.jar
+				String fullPath = new File(
+						local.getPath(), "cayenne/cayenne.jar").getAbsolutePath();
+				JavaCore.setClasspathVariable(VAR_CAYENNE_LIB, new Path(fullPath), null);
+			}
+			{ // cayenne-nodeps.jar
+				String fullPath = new File(
+						local.getPath(), "cayenne/cayenne-nodeps.jar").getAbsolutePath();
+				JavaCore.setClasspathVariable(VAR_CAYENNE_NODEPS_LIB, new Path(fullPath), null);
+			}
+			
+		} catch(Exception ex){
+			ex.printStackTrace();
+			JavaCore.removeClasspathVariable(VAR_CAYENNE_LIB, null);
+			JavaCore.removeClasspathVariable(VAR_CAYENNE_NODEPS_LIB, null);
+		}
+	}
+
+}
---
+++ net.sf.clickide.cayenne/src/net/sf/clickide/cayenne/CayennePlugin.java
@@ -0,0 +1,148 @@
+package net.sf.clickide.cayenne;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class CayennePlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "net.sf.clickide.cayenne";
+
+	// The shared instance
+	private static CayennePlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public CayennePlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static CayennePlugin getDefault() {
+		return plugin;
+	}
+	
+	/**
+	 * JAR files for Cayenne Modeler.
+	 */
+	private static File[] files;
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static File[] getCayenneModelerClassPaths(){
+		if(files!=null){
+			return files;
+		}
+		File dir = CayennePlugin.getDefault().getStateLocation().toFile();
+		files = new File[]{
+				new File(dir, "cayenne.jar"),
+				new File(dir, "cayenne-modeler.jar"),
+				new File(dir, "commons-dbcp-1.2.1.jar"),
+				new File(dir, "commons-pool-1.2.jar"),
+				new File(dir, "forms-1.0.3.jar"),
+				new File(dir, "hsqldb-1.8.0.2.jar"),
+				new File(dir, "looks-1.3.1.jar"),
+				new File(dir, "ognl-2.6.7.jar"),
+				new File(dir, "scope-bin-1.0.1.jar"),
+		};
+		return files;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static String[] getCayenneModelerClassPathAsStringArray(){
+		File[] files = getCayenneModelerClassPaths();
+		String[] paths = new String[files.length];
+		for(int i=0;i<files.length;i++){
+			paths[i] = files[i].getAbsolutePath();
+		}
+		return paths;
+	}
+	
+	/**
+	 * 
+	 * @throws CoreException
+	 */
+	public static void copyCayenneModelerLibraries() throws CoreException {
+		File[] files = getCayenneModelerClassPaths();
+		for(int i=0;i<files.length;i++){
+			if(!files[i].exists()){
+				copyFile(CayennePlugin.getDefault().getBundle().getEntry(
+						"/cayenne/" + files[i].getName()), files[i]);
+			}
+		}
+	}
+	
+//	/**
+//	 * 
+//	 * @throws CoreException
+//	 */
+//	public static void removeCayenneModelerLibraries() throws CoreException {
+//		File[] files = getCayenneModelerClassPaths();
+//		for(int i=0;i<files.length;i++){
+//			if(files[i].exists()){
+//				files[i].delete();
+//			}
+//		}
+//	}
+	
+	private static void copyFile(URL url, File file) throws CoreException {
+		try {
+			InputStream in = url.openStream();
+			OutputStream out = new FileOutputStream(file);
+			try {
+				byte[] buf = new byte[1024 * 8];
+				int length = 0;
+				while((length = in.read(buf))!=-1){
+					out.write(buf, 0, length);
+				}
+			} finally {
+				in.close();
+				out.close();
+			}
+		} catch(Exception ex){
+			IStatus status = new Status(
+					IStatus.ERROR, CayennePlugin.PLUGIN_ID, 0, ex.toString(), ex);
+			throw new CoreException(status);
+		}
+	}	
+
+}
