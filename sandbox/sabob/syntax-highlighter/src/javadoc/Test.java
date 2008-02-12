8806d753bca3c790b3e57067bf32482639dabc00
Bob Schellink <sabob@apache.org>
2008-02-12 22:29:48 +0000

    syntax highlighter testing
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716742 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/sabob/syntax-highlighter/src/javadoc/Test.java
@@ -0,0 +1,24 @@
+package javadoc;
+
+/**
+ * <pre class="prettyprint" style="overflow:auto">
+ *  &#47;**
+ *   * Test class
+ *   *&#47;
+ *   public class Test extends Object {
+ *   
+ *   // The tests name
+ *   private String name;
+ *
+ *   public Test () {
+ *        
+ *   }
+ *    
+ *   public static void speak() {
+ *       System.out.println("But I have nothing to say!");
+ *   }
+ *} </pre>
+ * @author Bob Schellink
+ */
+public class Test {
+}
