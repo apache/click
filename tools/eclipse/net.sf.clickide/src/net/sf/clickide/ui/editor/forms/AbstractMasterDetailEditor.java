13c464f279631fcbdcda5d721d2219502afc2955
Naoki Takezoe <takezoe@apache.org>
2008-03-05 08:43:54 +0000

    Refactor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716824 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/eclipse/net.sf.clickide/src/net/sf/clickide/ui/editor/forms/AbstractMasterDetailEditor.java
@@ -0,0 +1,141 @@
+package net.sf.clickide.ui.editor.forms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.clickide.ClickPlugin;
+import net.sf.clickide.ui.editor.actions.ElementRemoveAction;
+import net.sf.clickide.ui.editor.attrs.IAttributeEditor;
+
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMElement;
+
+public abstract class AbstractMasterDetailEditor extends AbstractFormEditor {
+
+	protected SashForm sash;
+	protected TreeViewer viewer;
+	protected Composite currentEditor;
+	
+	protected MenuManager menu;
+	protected MenuManager newMenu;
+	protected ElementRemoveAction deleteAction = new ElementRemoveAction();
+	
+	protected abstract String[] getAcceptElementNames();
+	
+	protected abstract IAttributeEditor getAttributeEditor(String elementName);
+	
+	public void initModel(IStructuredModel model) {
+		sash = new SashForm(form.getBody(), SWT.HORIZONTAL);
+		sash.setLayoutData(new GridData(GridData.FILL_BOTH));
+		sash.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
+		
+		final Composite left = toolkit.createComposite(sash);
+		left.setLayoutData(new GridData(GridData.FILL_BOTH));
+		left.setLayout(new FillLayout());
+		
+		Section headerSection = toolkit.createSection(left, Section.DESCRIPTION|Section.TITLE_BAR);
+		headerSection.setText(ClickPlugin.getString("editor.clickXML.fileUpload"));
+		
+		Composite detailComposite = toolkit.createComposite(sash);
+		detailComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		detailComposite.setLayout(new FillLayout());
+		
+		Section detailSection = toolkit.createSection(detailComposite, Section.DESCRIPTION|Section.TITLE_BAR);
+		detailSection.setText(ClickPlugin.getString("editor.clickXML.details"));
+		
+		final Composite right = toolkit.createComposite(detailSection);
+		right.setLayout(new FillLayout());
+		detailSection.setClient(right);
+		
+		Tree tree = toolkit.createTree(headerSection, SWT.NULL);
+		tree.setLayoutData(new GridData(GridData.FILL_BOTH));
+		headerSection.setClient(tree);
+		
+		List acceptElements = new ArrayList();
+		String[] acceptElementNames = getAcceptElementNames();
+		for(int i=0;i<acceptElementNames.length;i++){
+			acceptElements.add(acceptElementNames[i]);
+		}
+		
+		viewer = new TreeViewer(tree);
+		viewer.setContentProvider(new ClickTreeContentProvider(acceptElements));
+		viewer.setLabelProvider(new ClickTreeLabelProvider());
+		viewer.addSelectionChangedListener(new ISelectionChangedListener(){
+			public void selectionChanged(SelectionChangedEvent event){
+				if(currentEditor!=null){
+					currentEditor.dispose();
+				}
+				IStructuredSelection selection = (IStructuredSelection)event.getSelection();
+				Object obj = selection.getFirstElement();
+				if(obj != null){
+					if(obj instanceof IDOMElement){
+						IAttributeEditor editor = getAttributeEditor(((IDOMElement)obj).getNodeName());
+						if(editor != null){
+							currentEditor = editor.createForm(toolkit, right, (IDOMElement)obj);
+						}
+					}
+				}
+				right.layout();
+			}
+		});
+		
+		viewer.setInput(model);
+		viewer.expandAll();
+		
+		menu = new MenuManager();
+		newMenu = new MenuManager(ClickPlugin.getString("action.new"));
+		menu.add(newMenu);
+		menu.add(new Separator());
+		menu.add(deleteAction);
+		tree.setMenu(menu.createContextMenu(tree));
+		tree.addSelectionListener(new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent evt){
+				updateMenu();
+			}
+		});
+		tree.addKeyListener(new KeyAdapter(){
+			public void keyReleased(KeyEvent e) {
+				if(e.keyCode == SWT.DEL && deleteAction.isEnabled()){
+					deleteAction.run();
+				}
+			}
+		});
+		
+		toolkit.paintBordersFor(left);
+	}
+
+	public void modelUpdated(IStructuredModel model) {
+		viewer.setSelection(null);
+		viewer.refresh();
+	}
+	
+	public void setFocus() {
+		form.setFocus();
+	}
+	
+	public Object getAdapter(Class adapter) {
+		if(adapter.equals(TreeViewer.class)){
+			return this.viewer;
+		}
+		return super.getAdapter(adapter);
+	}
+
+}
