6d490f0c4fbf8f5890110e2916f3d06dd152777e
Bob Schellink <sabob@apache.org>
2008-03-15 14:13:49 +0000

    Added more jsp examples.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@716882 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/net/sf/click/examples/page/jsp/NavigationB.java
@@ -0,0 +1,23 @@
+package net.sf.click.examples.page.jsp;
+
+/**
+ * Provides an navigation example Page demonstrating forward and redirect
+ * page navigation. See NavigationA page for details.
+ *
+ * @author Malcolm Edgar
+ */
+public class NavigationB extends NavigationA {
+
+    public String getTarget() {
+        return "/jsp/navigation-a.jsp";
+    }
+
+    /**
+     * Returns the name of the border template: &nbsp; <tt>"/border-template.jsp"</tt>
+     *
+     * @see Page#getTemplate()
+     */
+    public String getTemplate() {
+        return "/border-template.jsp";
+    }
+}
---
+++ examples/webapp/jsp/edit-customer.jsp
@@ -0,0 +1,8 @@
+<p>
+Example JSP submit form.
+</p>
+
+<h2>Fill out the form below</h2>
+${form}
+
+<jsp:include page='/jsp/blah.jsp'/>
