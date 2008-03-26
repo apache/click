91c8bfe63f4983e7a5f4afe325902e41428a7787
malcolm_edgar <dev-null@apache.org>
2008-03-26 01:33:07 +0000

    Fix Menu usage
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@716933 13f79535-47bb-0310-9956-ffa450edef68

--- template/src/template/page/click/ErrorPage.java
+++ template/src/template/page/click/ErrorPage.java
@@ -5,7 +5,8 @@ import net.sf.click.extras.control.Menu;
 public class ErrorPage extends net.sf.click.util.ErrorPage {
 
     public String title = "Error Page";
-    public Menu rootMenu = new Menu();
+
+    public Menu rootMenu = Menu.getRootMenu();
 
     public void onDestroy() {
         Throwable error = getError();
