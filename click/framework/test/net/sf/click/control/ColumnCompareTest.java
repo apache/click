46b63d651439230865dd893cc0d5f77736348ab8
Bob Schellink <sabob@apache.org>
2008-03-29 08:13:13 +0000

    imports cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@716962 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/test/net/sf/click/control/ColumnCompareTest.java
+++ click/framework/test/net/sf/click/control/ColumnCompareTest.java
@@ -6,78 +6,76 @@ import java.util.List;
 import java.util.Map;
 
 import junit.framework.TestCase;
-import net.sf.click.control.Column;
-import net.sf.click.control.Table;
 
 public class ColumnCompareTest extends TestCase {
 
-	public void test_1() {
-		Column column = new Column("name");
+    public void test_1() {
+        Column column = new Column("name");
 
-		Table table = new Table("table");
-		table.addColumn(column);
-		
-		Column.Comparator comparator = new Column.Comparator(column);
-		List rowList = createRowList1();
-		Collections.sort(rowList, comparator);
-		System.out.println(rowList);
-		
-		table.setSortedAscending(false);
-		Collections.sort(rowList, comparator);
-		System.out.println(rowList);
-	}
-	
-	public void test_2() {
-		Column column = new Column("name");
+        Table table = new Table("table");
+        table.addColumn(column);
 
-		Table table = new Table("table");
-		table.addColumn(column);
-		
-		Column.Comparator comparator = new Column.Comparator(column);
-		List rowList = createRowList2();
-		Collections.sort(rowList, comparator);
-		System.out.println(rowList);
-		
-		table.setSortedAscending(false);
-		Collections.sort(rowList, comparator);
-		System.out.println(rowList);
-	}
-	
-	private List createRowList1() {
-		List rowList = new ArrayList();
-		
-		rowList.add(createRow("Dht"));
-		rowList.add(createRow("DHT"));
-		rowList.add(createRow(Boolean.FALSE));
-		rowList.add(createRow(Boolean.TRUE));
-		rowList.add(createRow("Data Services"));
-		rowList.add(createRow("Data 213 Services"));
-		rowList.add(createRow("Data 123 Services"));
-		rowList.add(createRow("0123"));
-		rowList.add(createRow("1234"));
-		rowList.add(createRow("-234"));
-		rowList.add(createRow("32-34"));
-		rowList.add(createRow("23-34"));
-		rowList.add(createRow("item4"));
-		rowList.add(createRow("item3"));
-		rowList.add(createRow("item2"));
-		rowList.add(createRow("item1"));
-		rowList.add(createRow("item10"));
-		
-		return rowList;
-	}
-	
-	private List createRowList2() {
-		List rowList = new ArrayList();
-		
-		rowList.add(createRow(null));
-		rowList.add(createRow(Boolean.TRUE));
-		rowList.add(createRow(Boolean.FALSE));
-		
-		return rowList;
-	}
-	
-	private Map createRow(Object value) {
-		return Collections.singletonMap("name", value);
-	}
+        Column.Comparator comparator = new Column.Comparator(column);
+        List rowList = createRowList1();
+        Collections.sort(rowList, comparator);
+        System.out.println(rowList);
+
+        table.setSortedAscending(false);
+        Collections.sort(rowList, comparator);
+        System.out.println(rowList);
+    }
+
+    public void test_2() {
+        Column column = new Column("name");
+
+        Table table = new Table("table");
+        table.addColumn(column);
+
+        Column.Comparator comparator = new Column.Comparator(column);
+        List rowList = createRowList2();
+        Collections.sort(rowList, comparator);
+        System.out.println(rowList);
+
+        table.setSortedAscending(false);
+        Collections.sort(rowList, comparator);
+        System.out.println(rowList);
+    }
+
+    private List createRowList1() {
+        List rowList = new ArrayList();
+
+        rowList.add(createRow("Dht"));
+        rowList.add(createRow("DHT"));
+        rowList.add(createRow(Boolean.FALSE));
+        rowList.add(createRow(Boolean.TRUE));
+        rowList.add(createRow("Data Services"));
+        rowList.add(createRow("Data 213 Services"));
+        rowList.add(createRow("Data 123 Services"));
+        rowList.add(createRow("0123"));
+        rowList.add(createRow("1234"));
+        rowList.add(createRow("-234"));
+        rowList.add(createRow("32-34"));
+        rowList.add(createRow("23-34"));
+        rowList.add(createRow("item4"));
+        rowList.add(createRow("item3"));
+        rowList.add(createRow("item2"));
+        rowList.add(createRow("item1"));
+        rowList.add(createRow("item10"));
+
+        return rowList;
+    }
+
+    private List createRowList2() {
+        List rowList = new ArrayList();
+
+        rowList.add(createRow(null));
+        rowList.add(createRow(Boolean.TRUE));
+        rowList.add(createRow(Boolean.FALSE));
+
+        return rowList;
+    }
+
+    private Map createRow(Object value) {
+        return Collections.singletonMap("name", value);
+    }
 }
