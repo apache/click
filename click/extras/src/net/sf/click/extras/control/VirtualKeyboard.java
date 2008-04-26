7952bb6da5e957f23a4d77b58dd05ac91f6d0571
malcolm_edgar <dev-null@apache.org>
2008-04-26 08:38:38 +0000

    javadoc and checkstyle updates
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@717087 13f79535-47bb-0310-9956-ffa450edef68

--- click/extras/src/net/sf/click/extras/control/VirtualKeyboard.java
+++ click/extras/src/net/sf/click/extras/control/VirtualKeyboard.java
@@ -1,14 +1,27 @@
+/*
+ * Copyright 2004-2008 Malcolm A. Edgar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.click.extras.control;
-
-import net.sf.click.control.Field;
-import net.sf.click.control.TextField;
-import net.sf.click.util.ClickUtils;
 
 import javax.servlet.ServletContext;
 
+import net.sf.click.control.TextField;
+import net.sf.click.util.ClickUtils;
+
 /**
- * <code>VirtualKeyboard</code> adds a graphical keyboard interface to text fields
- * <i>(later password fields and textareas)</i> so they can be filled with mouse only:<p/>
+ * Provides a graphical Vitural Keyboard interface text field control: &nbsp; &lt;input type='text'&gt;.
  *
  * <table class='htmlHeader' cellspacing='10'>
  * <tr>
@@ -18,32 +31,46 @@ import javax.servlet.ServletContext;
  * </tr>
  * </table>
  *
+ * VirtualKeyboard enables text fields to be filled in using a mouse only. Password
+ * and textarea fields will be supported in later releases.
+ * <p/>
  * <b>Virtual keyboard interfaces</b> are generally used in websites where the highest
- * level of security is a must like online banking applications.<p/>
- * <b>Virtual keyboards</b> help to prevent any keylogging activies and/or provide
+ * level of security is a must like online banking applications.
+ * Virtual keyboards help to prevent any keylogging activies and/or provide
  * users a special keyboard which they don’t already have (like a keyboard of
- * another language).<p/>
+ * another language).
  *
- * <b>Note:</b> Comes with Arabic, Belgian, Dutch, Dvorak, French, German,
+ * <h3>Keyboard Layout Support</h3>
+ * This controls comes with support for \Arabic, Belgian, Dutch, Dvorak, French, German,
  * Greek, Hebrew, Hungarian, Italian, Lithuanian, Norwegian, Number Pad,
  * Polish Programmers, Portuguese, Russian, Slovenian, Spanish (Spain),
  * Turkish-F, Turkish-QWERTY, UK, US Standard and US International keyboard layouts,
- * dynamically selectable. <p/>
+ * dynamically selectable.
  *
- * <b>Source:</b> Based on the javascript library from: http://www.greywyvern.com/code/js/keyboard.html <p/>
+ * <h4>Credits</h4>
+ * This control based on the <a href="http://www.greywyvern.com/code/js/keyboard.html">Greywyvern</a> JavaScript library.
  *
+ * @author Ahmed Mohombe
  *
+ * @todo: add support for password fields and textareas too.
  * @author Ahmed Mohombe
  * TODO: add support for password fields and textareas too.
  */
 public class VirtualKeyboard extends TextField {
 
+    private static final long serialVersionUID = 1L;
+
+     /** The HTML import statements. */
     public static final String HTML_IMPORTS =
-            "<script type=\"text/javascript\">var keyboard_png_path=\"{0}/click/keyboard.png\";</script>"+
-            "<script type=\"text/javascript\" src=\"{0}/click/keyboard.js\" charset=\"UTF-8\"></script>\n" +
-            "<link rel=\"stylesheet\" type=\"text/css\" href=\"{0}/click/keyboard.css\">";
+          "<script type=\"text/javascript\">var keyboard_png_path=\"{0}/click/keyboard.png\";</script>"
+        + "<script type=\"text/javascript\" src=\"{0}/click/keyboard.js\" charset=\"UTF-8\"></script>\n"
+        + "<link rel=\"stylesheet\" type=\"text/css\" href=\"{0}/click/keyboard.css\">";
+
+    // ----------------------------------------------------------- Constructors
+
     /**
-     * Constructs a new VirtualKeyboard Field object with no name defined.<p/>
+     * Constructs a new VirtualKeyboard Field object with no name defined.
+     * <p/>
      * <b>Please note</b> the control's name must be defined before it is valid.
      */
     public VirtualKeyboard() {
@@ -54,7 +81,7 @@ public class VirtualKeyboard extends TextField {
     /**
      * Constructs the VirtualKeyboard Field with the given name.
      *
-     * @param name the name of the VirtualKeyboard Field
+     * @param name the name of the VirtualKeyboard field
      */
     public VirtualKeyboard(String name) {
         super(name);
@@ -64,13 +91,15 @@ public class VirtualKeyboard extends TextField {
     /**
      * Constructs the VirtualKeyboard Field with the given name and label.
      *
-     * @param name the name of the VirtualKeyboard Field
-     * @param label the label of the VirtualKeyboard Field
+     * @param name the name of the VirtualKeyboard field
+     * @param label the label of the VirtualKeyboard field
      */
     public VirtualKeyboard(String name, String label) {
         super(name, label);
         addStyleClass("keyboardInput");
     }
+
+    // --------------------------------------------------------- Public Methods
 
     /**
      * Returns the HTML head import statements for the JavaScript
@@ -88,6 +117,7 @@ public class VirtualKeyboard extends TextField {
      * Deploy the static resource files in the VirtualKeyboard control.
      *
      * @see net.sf.click.control.Field#onDeploy(javax.servlet.ServletContext)
+     *
      * @param servletContext the ServletContext
      */
     public void onDeploy(ServletContext servletContext) {
