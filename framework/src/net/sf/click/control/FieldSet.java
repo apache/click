b71f8b68e075d2bc40a1f6451da48c6fa6f0a7d1
Bob Schellink <sabob@apache.org>
2008-05-26 06:12:47 +0000

    remove(Field) should call remove(Control) for proper cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@717247 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/net/sf/click/control/FieldSet.java
+++ framework/src/net/sf/click/control/FieldSet.java
@@ -304,7 +304,7 @@ public class FieldSet extends AbstractContainer {
      * @throws IllegalArgumentException if the field is null
      */
     public void remove(Field field) {
-        super.remove(field);
+        remove((Control) field);
     }
 
     /**
