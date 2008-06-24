5806ee3103d7a026ecd7ac6514410380df4127ec
Bob Schellink <sabob@apache.org>
2008-06-24 09:40:52 +0000

    experimental javascript and css as controls
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@717326 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/framework/src/net/sf/click/control/CssImport.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2008 Malcolm A. Edgar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.click.control;
+
+import net.sf.click.util.HtmlStringBuffer;
+
+/**
+ *
+ * @author Bob Schellink
+ */
+public class CssImport extends AbstractControl {
+
+    public CssImport() {
+        this(null);
+    }
+
+    public CssImport(String source) {
+        if (source != null) {
+            setHref(source);
+        }
+        setAttribute("type", "text/css");
+        setAttribute("rel", "stylesheet");
+    }
+
+    public String getTag() {
+        return "link";
+    }
+
+    public void setHref(String href) {
+        setAttribute("href", href);
+    }
+
+    public String getHref() {
+        return getAttribute("href");
+    }
+
+    public void render(HtmlStringBuffer buffer) {
+        buffer.elementStart(getTag());
+        appendAttributes(buffer);
+        buffer.elementEnd();
+    }
+}
