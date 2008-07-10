f1e3ae0620fd355338174d6c005c8d21677cad85
Bob Schellink <sabob@apache.org>
2008-07-10 19:56:58 +0000

    add tests for CLK-371 and CLK-365
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@717370 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/test/net/sf/click/ClickServletTest.java
@@ -0,0 +1,31 @@
+package net.sf.click;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import net.sf.click.pages.ListenerPage;
+
+/**
+ * Test ClickServlet behavior.
+ * 
+ * @author Bob Schellink
+ */
+public class ClickServletTest extends TestCase {
+
+    /**
+     * CLK-365. Assert the ListenerPage that the Submit button listener is
+     * invoked *after* the TextField value was bound to the request value.
+     */
+    public void testRegisterListener() {
+        MockContainer container = new MockContainer("web");
+        container.start();
+        container.setParameter("form_name", "form"); // Simulate form submitted
+        container.setParameter("submit", "submit"); // Simulate Submit button clicked
+        container.setParameter("field", "one"); // Simulate TextField value set
+
+        ListenerPage page = (ListenerPage) container.testPage(ListenerPage.class);
+
+        // assert that the Page did successfully execute
+        Assert.assertTrue(page.success);
+        container.stop();
+    }
+}
