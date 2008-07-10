19d121f4e7a9813ee4c5099c5f83ddc008674e46
Bob Schellink <sabob@apache.org>
2008-07-10 20:12:13 +0000

    remove Ajax support from core framework
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@717374 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/src/net/sf/click/ControlRegistry.java
+++ click/framework/src/net/sf/click/ControlRegistry.java
@@ -16,17 +16,12 @@
 package net.sf.click;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 
 import java.util.List;
-import java.util.Set;
-import net.sf.click.util.Partial;
 import org.apache.commons.lang.Validate;
 
 /**
- * Provides a thread local register for managing Ajax controls and ActionListener
- * events.
+ * Provides a thread local register for ActionListener events.
  * <p/>
  * Developers who implement their own controls, should look at the following
  * example <tt>onProcess</tt> implementation. Note the call to
@@ -51,29 +46,11 @@ import org.apache.commons.lang.Validate;
  *     ...
  *
  * } </pre>
- * <p/>
- * Registering Ajax Controls for processing is another common use case:
- *
- * <pre class="prettyprint">
- * public void onInit() {
- *     Form form = new Form("form");
- *
- *     // Ajaxify the form by registering it in the ControlRegistry
- *     ControlRegistry.registerAjaxControl(form);
- *
- *     Submit submit = new Submit("submit");
- *     submit.setListener(new AjaxListener() {
- *
- *         public Partial onAjaxAction(Control control) {
- *             return new Partial("Hello World!");
- *         }
- *     });
- * } </pre>
  *
  * @author Bob Schellink
  * @author Malcolm Edgar
  */
-public final class ControlRegistry {
+public class ControlRegistry {
 
     // -------------------------------------------------------- Constants
 
@@ -82,30 +59,13 @@ public final class ControlRegistry {
 
     // -------------------------------------------------------- Variables
 
-    /** The set of unique registered Ajax Controls. */
-    private Set ajaxControlList;
-
     /** The list of registered event sources. */
-    private List eventSourceList;
+    List eventSourceList;
 
     /** The list of registered event listeners. */
-    private List eventListenerList;
+    List eventListenerList;
 
     // --------------------------------------------------------- Public Methods
-
-    /**
-     * Register the control to be processed by the ClickServlet for Ajax
-     * requests.
-     *
-     * @param control the control to register
-     */
-    public static void registerAjaxControl(Control control) {
-        Validate.notNull(control, "Null control parameter");
-
-        ControlRegistry instance = getThreadLocalRegistry();
-        Set controlList = instance.getAjaxControls();
-        controlList.add(control);
-    }
 
     /**
      * Register the event source and event ActionListener to be fired by the
@@ -128,16 +88,6 @@ public final class ControlRegistry {
     // ------------------------------------------------ Package Private Methods
 
     /**
-     * Checks if any Ajax controls have been registered.
-     */
-    boolean hasAjaxControls() {
-        if (ajaxControlList == null || ajaxControlList.isEmpty()) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
      * Checks if any Action Events have been registered.
      */
     boolean hasActionEvents() {
@@ -145,31 +95,6 @@ public final class ControlRegistry {
             return false;
         }
         return true;
-    }
-
-    /**
-     * Process all the registered controls and return true if the page should
-     * continue processing.
-     *
-     * @return true if the page should continue processing or false otherwise
-     */
-    boolean processAjaxControls(Context context) {
-
-        if (!hasAjaxControls()) {
-            return true;
-        }
-
-        for (Iterator it = ajaxControlList.iterator(); it.hasNext();) {
-            Control control = (Control) it.next();
-
-            // Check if control is targeted by this request
-            if (context.getRequestParameter(control.getId()) != null) {
-                control.onProcess();
-            }
-        }
-
-        // Fire the registered listeners
-        return fireActionEvents(context);
     }
 
     /**
@@ -189,21 +114,8 @@ public final class ControlRegistry {
             Control source = (Control) eventSourceList.get(i);
             ActionListener listener = (ActionListener) eventListenerList.get(i);
 
-            if (context.isAjaxRequest() && listener instanceof AjaxListener) {
-
-                Partial partial = ((AjaxListener) listener).onAjaxAction(source);
-                if (partial != null) {
-                    // Have to process Partial here
-                    partial.process(context);
-                }
-
-                // Ajax requests stops further processing
+            if (!listener.onAction(source)) {
                 continueProcessing = false;
-
-            } else {
-                if (!listener.onAction(source)) {
-                    continueProcessing = false;
-                }
             }
         }
 
@@ -214,9 +126,6 @@ public final class ControlRegistry {
      * Clear the registry.
      */
     void clearRegistry() {
-        if (hasAjaxControls()) {
-            ajaxControlList.clear();
-        }
         if (hasActionEvents()) {
             eventListenerList.clear();
             eventSourceList.clear();
@@ -300,18 +209,6 @@ public final class ControlRegistry {
             eventSourceList = new ArrayList();
         }
         return eventSourceList;
-    }
-
-    /**
-     * Return the set of unique Ajax Controls.
-     *
-     * @return set of unique Ajax Controls
-     */
-    private Set getAjaxControls() {
-        if (ajaxControlList == null) {
-            ajaxControlList = new LinkedHashSet();
-        }
-        return ajaxControlList;
     }
 
     // -------------------------------------------------- Inner Classes
