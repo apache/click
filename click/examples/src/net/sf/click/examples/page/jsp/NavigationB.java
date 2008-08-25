cec8d53963fd8b1254fee0cd03b377ac9e065fa7
Bob Schellink <sabob@apache.org>
2008-08-25 17:58:00 +0000

    should forward and redirect by using htm extension
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@717628 13f79535-47bb-0310-9956-ffa450edef68

--- click/examples/src/net/sf/click/examples/page/jsp/NavigationB.java
+++ click/examples/src/net/sf/click/examples/page/jsp/NavigationB.java
@@ -8,9 +8,26 @@ package net.sf.click.examples.page.jsp;
  */
 public class NavigationB extends NavigationA {
 
+    /**
+     * Target template to forward to.
+     * <p/>
+     * In order to forward to a Page with a JSP template, we specify the target
+     * with an htm extension so that ClickServlet will process the Page.
+     * After the Page NavigationA.java is processed, Click will forward to the
+     * underlying template /jsp/navigation-a.jsp.
+     */
     public String getTarget() {
-        return "/jsp/navigation-a.jsp";
+        return "/jsp/navigation-a.htm";
     }
+
+    /**
+     * Note one can also forward and redirect using the Page class instead of
+     * the path as seen below.
+     */
+    /*
+    public Class getTargetPageClass() {
+        return NavigationA.class;
+    }*/
 
     /**
      * Returns the name of the border template: &nbsp; <tt>"/border-template.jsp"</tt>
