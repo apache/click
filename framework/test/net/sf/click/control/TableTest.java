41ee4ac9ab32a6b000c6571c0850a696c7781650
Bob Schellink <sabob@apache.org>
2008-09-06 16:21:26 +0000

    add table tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@717644 13f79535-47bb-0310-9956-ffa450edef68

--- framework/test/net/sf/click/control/TableTest.java
+++ framework/test/net/sf/click/control/TableTest.java
@@ -3,6 +3,7 @@ package net.sf.click.control;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Map;
 import junit.framework.TestCase;
 import net.sf.click.MockContext;
 
@@ -41,6 +42,58 @@ public class TableTest extends TestCase {
         String row2 = "<tr class=\"even\">\n<td id=\"Foos-Name_1\">foo2</td></tr>";
         String body = "<tbody>\n" + row1 + row2 + "</tbody>";
         assertEquals("<table id=\"Foos\">\n" + header + body + "</table>\n", table.toString());
+    }
+
+    public void testPagingCurrentPage() {
+        MockContext.initContext();
+
+        List foos = new ArrayList();
+        for (int i = 0; i < 1000; i++) {
+            foos.add(new Foo("foo" + i));
+        }
+
+        Table table = new Table("table");
+        table.setRowList(foos);
+        table.setPageSize(10);
+        table.setPageNumber(0);
+        Column column = new Column("name");
+        column.setSortable(false);
+        table.addColumn(column);
+
+        // Since page number is zero based check that if page number is 0,
+        // Page 1 is the current page
+        assertTrue(table.toString().indexOf("<strong>1</strong>") > 0);
+
+        table.setPageNumber(99);
+
+        // Check that if page number is 99, Page 100 is the current page
+        assertTrue(table.toString().indexOf("<strong>100</strong>") > 0);
+    }
+
+    public void testSetRowAttributes() {
+        MockContext.initContext();
+
+        List foos = new ArrayList();
+        for (int i = 0; i < 3; i++) {
+            foos.add(new Foo("foo" + i));
+        }
+
+        Table table = new Table("table") {
+            protected void setRowAttributes(Map attributes, Object row, int rowIndex) {
+                Foo foo = (Foo) row;
+                attributes.put("id", foo.getName());
+                attributes.put("class", "foo bar");
+            }
+        };
+        table.setRowList(foos);
+        Column column = new Column("name");
+        table.addColumn(column);
+
+        // Check that a row with the id=foo0 is available
+        assertTrue(table.toString().indexOf("<tr id=\"foo0\"") > 0);
+
+        // Check that a row with the class=foo bar is available
+        assertTrue(table.toString().indexOf("<tr id=\"foo0\" class=\"foo bar") > 0);
     }
 
     /**
