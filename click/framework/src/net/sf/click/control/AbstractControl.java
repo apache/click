c6af1e98f6a86fa1dcc84bfdff0cd3e8292535b0
Bob Schellink <sabob@apache.org>
2008-09-09 16:03:23 +0000

    fix getMessage to return null instead of throwing exception
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@717658 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/src/net/sf/click/control/AbstractControl.java
+++ click/framework/src/net/sf/click/control/AbstractControl.java
@@ -356,10 +356,11 @@ public abstract class AbstractControl implements Control {
      * Return the localized message for the given key, or null if not found.
      * <p/>
      * This method will attempt to lookup the localized message in the
-     * parent's messages, which by resolves to the Page's resource bundle.
+     * parent's messages, which resolves to the Page's resource bundle.
      * <p/>
-     * If the message was not found, the this method will attempt to look up the
-     * value in the <tt>/click-control.properties</tt> message properties file.
+     * If the message was not found, this method will attempt to look up the
+     * value in the <tt>/click-control.properties</tt> message properties file,
+     * through the method {@link #getMessages()}.
      * <p/>
      * If still not found, this method will return null.
      *
@@ -383,8 +384,12 @@ public abstract class AbstractControl implements Control {
     }
 
     /**
-     * Return the formatted message for the given resource name
-     * and message format argument and for the context request locale.
+     * Return the formatted message for the given resource name,
+     * message format argument and the context request locale, or null
+     * if no message was found.
+     * <p/>
+     * {@link #getMessage(java.lang.String, java.lang.Object[])} is invoked to
+     * retrieve the message for the specified name.
      *
      * @param name resource name of the message
      * @param arg the message argument to format
@@ -396,19 +401,26 @@ public abstract class AbstractControl implements Control {
     }
 
     /**
-     * Return the formatted package message for the given resource name and
-     * message format arguments and for the context request locale.
+     * Return the formatted message for the given resource name,
+     * message format arguments and the context request locale, or null if
+     * no message was found.
+     * <p/>
+     * {@link #getMessage(java.lang.String)} is invoked to retrieve the message
+     * for the specified name.
      *
      * @param name resource name of the message
      * @param args the message arguments to format
-     * @return the named localized message for the package
+     * @return the named localized message for the package or null if no message
+     * was found
      */
     public String getMessage(String name, Object[] args) {
         if (args == null) {
             throw new IllegalArgumentException("Null args parameter");
         }
         String value = getMessage(name);
-
+        if (value == null) {
+            return null;
+        }
         return MessageFormat.format(value, args);
     }
 
