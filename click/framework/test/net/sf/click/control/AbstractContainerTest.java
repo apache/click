775d39067fc69d4f671393786bd60afa31f66ff5
Bob Schellink <sabob@apache.org>
2008-10-15 21:24:52 +0000

    Add regression tests for Container #add and #insert behavior. CLK-447
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@717785 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/framework/test/net/sf/click/control/AbstractContainerTest.java
@@ -0,0 +1,138 @@
+package net.sf.click.control;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for AbstractContainer.
+ */
+public class AbstractContainerTest extends TestCase {
+
+    /**
+     * Test AbstractContainer#add(Control) and #insert(Control, int)
+     * pre conditions.
+     */
+    public void testInsertPreCondistion() {
+
+        AbstractContainer container = new AbstractContainer("form") {};
+
+        // Check that adding null control fails
+        try {
+            container.add(null);
+            fail("Control cannot be null");
+        } catch (Exception expected) {
+        }
+        
+        // Check that inserting into invalid negative index is caught
+        try {
+            container.insert(new TextField("field"), -1);
+            fail("insert index is out of bounds");
+        } catch (Exception expected) {
+        }
+        
+        // Check that inserting into invalid positive index is caught
+        try {
+            container.insert(new TextField("field"), 1);
+            fail("insert index is out of bounds");
+        } catch (Exception expected) {
+        }
+        
+        // Check that inserting into valid index succeeds
+        try {
+            container.insert(new TextField("field"), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("index 0 should be valid");
+        }
+        
+        // Create new container for testing
+        container = new AbstractContainer("form") {};
+
+        // Check that adding TextField with name succeeds
+        try {
+            container.add(new TextField("field"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Field has name defined and is valid");
+        }
+        
+        // Check that adding TextField with duplicate name fails
+        try {
+            container.add(new TextField("field"));
+            fail("Field with name 'field' already exists on Form");
+        } catch (Exception expected) {
+            // Should fail since field with name 'field' already exist on container
+        }
+
+        // Check that adding TextField without name fails
+        try {
+            container.add(new TextField());
+            fail("Field must define a name");
+        } catch (Exception expected) {
+            // Should fail since Field does not have name defined
+        }
+
+        // Check that adding Container without name succeeds
+        try {
+            container.add(new AbstractContainer() {});
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Container does not need to define name");
+        }
+
+        // Check that adding FieldSet with name succeeds
+        try {
+            container.add(new FieldSet("fieldSet"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("FieldSet has name defined and is valid");
+        }
+        
+        // Check that adding FieldSet with duplicate name fails
+        try {
+            container.add(new FieldSet("fieldSet"));
+            fail("FieldSet with name 'fieldSet' already exists on Form");
+        } catch (Exception expected) {
+            // Should fail since fieldSet with name 'fieldSet' already exist on container
+        }
+
+        // Check that adding FieldSet without name fails
+        try {
+            container.add(new FieldSet());
+            fail("FieldSet must define a name");
+        } catch (Exception expected) {
+            // Should fail since FieldSet does not have name defined
+        }
+
+        // Check that adding label without name fails
+        try {
+            container.add(new Label());
+            fail("Label must define a name");
+        } catch (Exception expected) {
+            // Should fail since Label does not have name defined
+        }
+
+        // Check that adding label with name succeeds
+        try {
+            container.add(new Label("label"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Label has name defined and is valid");
+        }
+
+        // Check that adding label with duplicate name also succeeds
+        try {
+            container.add(new Label("label"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Form should accept labels with duplicate names");
+        }
+        
+        // Check that adding container to itself fails
+        try {
+            container.add(container);
+            fail("Cannot add container to itself");
+        } catch (Exception expected) {
+            // Should fail since container cannot be added to itself
+        }
+    }
+}
--- click/framework/test/net/sf/click/control/FormTest.java
+++ click/framework/test/net/sf/click/control/FormTest.java
@@ -7,7 +7,7 @@ import net.sf.click.Page;
 import net.sf.click.servlet.MockRequest;
 
 /**
- * @author Bob Schellink
+ * Tests for Form.
  */
 public class FormTest extends TestCase {
 
@@ -115,32 +115,542 @@ public class FormTest extends TestCase {
         Assert.assertEquals(requestValue, nameField.getValueObject());
     }
 
-    /**
-     * Test that form inserts controls before hidden fields
-     */
-    public void testInsertOrderWithHiddenFields() {
+    /** Form which index position to test. */
+    private Form form;
+    
+    /** Field which position is tracked in the Form. */
+    private HiddenField trackField;
 
-        // Create form and fields
+    /**
+     * Setup the form and trackField instances for testing the Control index
+     * positions after being added or inserted.
+     */
+    public void setUp() {
+        // Create form.
+        form = new Form("form");
+        
+        // Form automatically creates and adds a HiddenField for storing the
+        // form name between requests. This field is at index 0 at the start
+        // of each test.
+        // The tests below checks the trackField index position in the Form
+        // for the lists Form#controls, Form#fieldList and Form#buttonList
+        trackField = (HiddenField) form.getField(Form.FORM_NAME);
+
+        // trackField index: #controls=0
+        assertTrue(form.getControls().indexOf(trackField) == 0);
+        // trackField index: #fieldList=0
+        assertTrue(form.getFieldList().indexOf(trackField) == 0);
+    }
+
+    /**
+     * Test that Form#add(Control) inserts field at the correct index.
+     *
+     * The Form#controls list should be affected by the insert index, while
+     * Form#fieldList should not.
+     */
+    public void testInsertOrderAfterAddingField() {
+        TextField nameField = new TextField("name");
+
+        // Add new field
+        form.add(nameField);
+
+        // nameField index: #controls=1, #fieldList=1
+        assertTrue(form.getControls().indexOf(nameField) == 1);
+        assertTrue(form.getFieldList().indexOf(nameField) == 1);
+        
+        // trackField index: #controls=0, #fieldList=0
+        assertTrue(form.getControls().indexOf(trackField) == 0);
+        assertTrue(form.getFieldList().indexOf(trackField) == 0);
+    }
+
+    /**
+     * Test that Form#insert(Control, int) inserts field at the correct index.
+     *
+     * The Form#controls list should be affected by the insert index, while
+     * Form#fieldList should not.
+     */
+    public void testInsertOrderAfterInsertingField() {
+        TextField nameField = new TextField("name");
+
+        // Insert field at index 0
+        form.insert(nameField, 0);
+
+        // nameField index: #controls=0
+        assertTrue(form.getControls().indexOf(nameField) == 0);
+        // nameField index: #fieldList=1
+        assertTrue(form.getFieldList().indexOf(nameField) == 1);
+        
+        // trackField index: #controls=1
+        assertTrue(form.getControls().indexOf(trackField) == 1);
+        // trackField index: #fieldList=0
+        assertTrue(form.getFieldList().indexOf(trackField) == 0);
+    }
+
+    /**
+     * Test that Form#insert(Control, int) inserts table at the correct index.
+     *
+     * The Form#controls list should be affected by the insert index, while
+     * Form#fieldList should not contain table as Table is not a Field.
+     */
+    public void testInsertOrderAfterInsertingTable() {
+        Table table = new Table("table");
+
+        // Insert table at index 0
+        form.insert(table, 0);
+
+        // table index: #controls=0
+        assertTrue(form.getControls().indexOf(table) == 0);
+        // table index: #fieldList=-1
+        assertTrue(form.getFieldList().indexOf(table) == -1);
+        
+        // trackField index: #controls=1
+        assertTrue(form.getControls().indexOf(trackField) == 1);
+        // trackField index: #fieldList=0
+        assertTrue(form.getFieldList().indexOf(trackField) == 0);
+    }
+
+    /**
+     * Test that Form#insert(Control, int) inserts Button at the correct index.
+     *
+     * The Form#controls list should be affected by the insert index, while
+     * Form#buttonList should not.
+     */
+    public void testInsertOrderAfterInsertingButton() {
+        // Check that the fieldList includes only trackField thus its size is 1
+        assertTrue(form.getFieldList().size() == 1);
+
+        Button button = new Button("button1");
+
+        form.insert(button, 0);
+
+        // button index: #controls=0
+        assertTrue(form.getControls().indexOf(button) == 0);
+        // button index: #buttonList=0
+        assertTrue(form.getButtonList().indexOf(button) == 0);
+        // Check that button was not added to fieldList accidentally
+        assertTrue(form.getFieldList().size() == 1);
+    }
+
+    /**
+     * Test that Form#insert(Control, int) inserts HiddenField at the correct index.
+     *
+     * The Form#controls list should be affected by the insert index, while
+     * Form#fieldList should not.
+     */
+    public void testInsertOrderAfterInsertingHiddenField() {
+        HiddenField hidden = new HiddenField("hidden", Boolean.class);
+
+        // Insert hidden at index 0
+        form.insert(hidden, 0);
+
+        // hidden index: #controls=0
+        assertTrue(form.getControls().indexOf(hidden) == 0);
+        // hidden index: #fieldList=1
+        assertTrue(form.getFieldList().indexOf(hidden) == 1);
+        
+        // trackField index: #controls=1
+        assertTrue(form.getControls().indexOf(trackField) == 1);
+        // trackField index: #fieldList=0
+        assertTrue(form.getFieldList().indexOf(trackField) == 0);
+    }
+
+    /**
+     * Test that Form#remove(Control) properly cleans up.
+     */
+    public void testRemove() {
+        TextField field = new TextField("field");
+
+        form.insert(field, 0);
+
+        // field index: #controls=0
+        assertTrue(form.getControls().indexOf(field) == 0);
+        // field index: #fieldList=1
+        assertTrue(form.getFieldList().indexOf(field) == 1);
+
+        // trackField index: #controls=1
+        assertTrue(form.getControls().indexOf(trackField) == 1);
+        // trackField index: #fieldList=0
+        assertTrue(form.getFieldList().indexOf(trackField) == 0);
+        
+        int expectedSize = 2;
+        // Check the list sizes to be 2
+        assertTrue(form.getControls().size() == expectedSize);
+        assertTrue(form.getFieldList().size() == expectedSize);
+        
+        // Removing field should shift up trackField index
+        form.remove(field);
+
+        expectedSize = 1;
+        // Check the list sizes to be 1
+        assertTrue(form.getControls().size() == expectedSize);
+        assertTrue(form.getFieldList().size() == expectedSize);
+        
+        // trackField index: #controls=0
+        assertTrue(form.getControls().indexOf(trackField) == 0);
+        // trackField index: #fieldList=0
+        assertTrue(form.getFieldList().indexOf(trackField) == 0);
+    }
+
+    /**
+     * Test that Form#add(Control, int) and Form#remove(Control) properly
+     * sets and removes the fieldWidth for a Field.
+     */
+    public void testFieldWidthForField() {
+        TextField field = new TextField("field");
+
+        // Check that fieldWidth is empty
+        assertTrue(form.getFieldWidths().isEmpty());
+            
+        int colspan = 4;
+        form.add(field, colspan);
+
+        // Check that fieldWidth has entry for field
+        assertTrue(form.getFieldWidths().size() == 1);
+        
+        Integer width = (Integer) form.getFieldWidths().get(field.getName());
+        assertEquals(4, width.intValue());
+
+        form.remove(field);
+        
+        // Check that fieldWidth is empty
+        assertTrue(form.getFieldWidths().isEmpty());
+    }
+
+    /**
+     * Test that Form#add(Control, int) and Form#remove(Control) properly
+     * sets and removes the fieldWidth for a Control.
+     */
+    public void testFieldWidthForTable() {
+        Table table = new Table("table");
+
+        // Check that fieldWidth is empty
+        assertTrue(form.getFieldWidths().isEmpty());
+            
+        int colspan = 4;
+        form.add(table, colspan);
+
+        // Check that fieldWidth has entry for table
+        assertTrue(form.getFieldWidths().size() == 1);
+        
+        Integer width = (Integer) form.getFieldWidths().get(table.getName());
+        assertEquals(4, width.intValue());
+
+        form.remove(table);
+        
+        // Check that fieldWidth is empty
+        assertTrue(form.getFieldWidths().isEmpty());
+    }
+
+    /**
+     * Test that Form.getFieldList() return only Fields directly added to the
+     * field list. The list should include FieldSets but exclude Buttons.
+     *
+     * The main use case for Form.getFieldList() is that it enables one to use
+     * Velocity to render Fields e.g.:
+     * 
+     * #foreach ($field in $form.fieldList)
+     *   <td>$field.label:</td> <td>$field</td>
+     * #end
+     *
+     * Also check that Form.getFieldList() returns a cached List so that access
+     * to Form.getFieldList() is fast.
+     *
+     * Q: Why not include fields recursively?
+     * A: Its not really needed for Velocity usage. For example one would rarely
+     *    wrap a field inside a "border container" when the rendering will be
+     *    done with Velocity. Instead one would rather wrap the field inside
+     *    a "border container" in the Velocity template e.g. wrapping it inside a
+     *    span element.
+     *        <span>$field</span>
+     *
+     *    If recursive fields are needed users can use ContainerUtils.getFields().
+     */
+    public void testGetFieldList() {
         Form form = new Form("form");
+
+        // Assemble Fields
         TextField nameField = new TextField("name");
         form.add(nameField);
 
-        form.add(new HiddenField("hidden", Boolean.class));
+        Div div = new Div("div");
+        form.add(div);
 
-        TextField scoreField = new TextField("score");
-        form.add(scoreField);
+        Button button = new Button("button");
+        form.add(button);
 
-        // Add table to form
-        Table table = new Table("table");
-        form.add(table);
+        // Assemble FieldSet
+        FieldSet fieldSet = new FieldSet("fieldSet");
+        form.add(fieldSet);
 
-        TextField ageField = new TextField("age");
-        form.add(ageField);
+        // Add Field to FieldSet
+        TextField fieldSetField = new TextField("fieldSetField");
+        fieldSet.add(fieldSetField);
 
-        // If form correctly places hidden fields at the bottom, then field 5
-        // should be named "hidden". field 5 is at index 4 since fieldList uses 
-        // a zero based index
-        Field hiddenField = ((Field) form.getFieldList().get(4));
-        Assert.assertEquals("hidden", hiddenField.getName());
+        // Check that list contains TextField
+        assertTrue(form.getFieldList().contains(nameField));
+
+        // Check that list contains FieldSet
+        assertTrue(form.getFieldList().contains(fieldSet));
+        
+        // Check that list does *not* contains the FieldSet's Field
+        assertFalse(form.getFieldList().contains(fieldSetField));
+
+        // Check that list does *not* contain Div
+        assertFalse(form.getFieldList().contains(div));
+        
+        // Check that list does *not* contain Button
+        assertFalse(form.getFieldList().contains(button));
+
+        // Check that field list is cached
+        assertSame(form.getFieldList(), form.getFieldList());
+    }
+
+    /**
+     * Test that Form.getButtonList() return only Buttons directly added to
+     * Form and exclude all other Controls.
+     *
+     * The main use case for Form.getButtonList() is that it enables one to use
+     * Velocity to render Buttons e.g.:
+     *
+     * #foreach ($button in $form.buttonList)
+     *   $button
+     * #end
+     *
+     * Also check that Form.getButtonList() returns a cached List so that access
+     * to Form.getButtonList() is fast.
+     *
+     * Q: Why not include buttons recursively?
+     * A: Its not really needed for Velocity usage. For example one would rarely
+     *    wrap a button inside a "border container" when the rendering will be
+     *    done with Velocity. Instead one would rather wrap the button inside
+     *    a "border container" in the Velocity template e.g. wrapping it inside a
+     *    span element:
+     *        <span>$button</span>
+     *
+     *    If recursive buttons are needed users can use ContainerUtils.getButtons().
+     */
+    public void testGetButtonList() {
+        Form form = new Form("form");
+
+        // Assemble Fields
+        TextField nameField = new TextField("name");
+        form.add(nameField);
+
+        Div div = new Div("div");
+        form.add(div);
+
+        Button button = new Button("button");
+        form.add(button);
+
+        // Assemble FieldSet
+        FieldSet fieldSet = new FieldSet("fieldSet");
+        form.add(fieldSet);
+
+        // Add Field to FieldSet
+        TextField fieldSetField = new TextField("fieldSetField");
+        fieldSet.add(fieldSetField);
+        
+        // Add Button to FieldSet
+        Button fieldSetButton = new Button("fieldSetButton");
+        fieldSet.add(fieldSetButton);
+
+        // Check that button list does *not* contains TextField
+        assertFalse(form.getButtonList().contains(nameField));
+
+        // Check that button list does *not* contains FieldSet
+        assertFalse(form.getButtonList().contains(fieldSet));
+        
+        // Check that button list does *not* contains the FieldSet's Field
+        assertFalse(form.getButtonList().contains(fieldSetField));
+
+        // Check that button list does *not* contain Div
+        assertFalse(form.getButtonList().contains(div));
+        
+        // Check that button list does contain Button
+        assertTrue(form.getButtonList().contains(button));
+        
+        // Check that button list does *not* contain FieldSetButton since that
+        // button was not directly added to Form
+        assertFalse(form.getButtonList().contains(fieldSetButton));
+
+        // Check that button list is cached
+        assertSame(form.getButtonList(), form.getButtonList());
+    }
+
+    /**
+     * Test that Form.getFields() return all Controls by delegating to
+     * AbstractContainer#getControlMap().
+     *
+     * The main use case for Form.getFields() is that it enables one to use
+     * Velocity to render Controls and Fields e.g.:
+     *
+     * $form.fields.name   <- name this is a TextField
+     * $form.fields.div    <- div is a AbstractContainer
+     * $form.fields.button <- button is a Submit
+     * 
+     * Also check that Form.getFields() returns a cached Map so that access to
+     * Form.getFields() is fast.
+     * 
+     * Q: Why not return Fields only?
+     * A: Form will then have to maintain another map besides #controlMap.
+     *    However it could lead to issues when users iterate the map and receive
+     *    ClassCastExceptions when casting to Field. In future release this issue
+     *    could be revisited.
+     */
+    public void testGetFields() {
+        Form form = new Form("form");
+
+        // Assemble Fields
+        String fieldName = "name";
+        form.add(new TextField(fieldName));
+
+        String divName = "div";
+        form.add(new Div(divName));
+
+        String buttonName = "button";
+        form.add(new Button(buttonName));
+        
+        // Assemble FieldSet
+        String fieldSetName = "fieldSet";
+        FieldSet fieldSet = new FieldSet(fieldSetName);
+        form.add(fieldSet);
+
+        // Add Field to FieldSet
+        String fieldSetFieldName = "fieldSetField";
+        fieldSet.add(new TextField(fieldSetFieldName));
+
+        // Check that map contains both TextField, Button and Div
+        assertTrue(form.getFields().containsKey(fieldName));
+        assertTrue(form.getFields().containsKey(divName));
+        assertTrue(form.getFields().containsKey(buttonName));
+        
+        // Check that map contains FieldSet
+        assertTrue(form.getFields().containsKey(fieldSetName));
+        
+        // Check that map does *not* contain the FieldSet's Field
+        assertFalse(form.getFields().containsKey(fieldSetFieldName));
+
+        // Check that field map is cached
+        assertSame(form.getFields(), form.getFields());
+        assertSame(form.getFields(), form.getControlMap());
+    }
+
+    /**
+     * Test that insert(int, Control) works properly.
+     */
+    public void testInsert() {
+        Form form = new Form("form");
+        
+        int defaultFieldSize = 17;
+        form.setDefaultFieldSize(defaultFieldSize);
+        
+        TextField field = new TextField("name");
+        
+        // Assert that defaultFieldSize is not equal to field size
+        assertFalse(defaultFieldSize == field.getSize());
+
+        form.add(field);
+        
+        // Assert that after adding field, defaultFieldSize is equal to field size
+        assertTrue(defaultFieldSize == field.getSize());
+    }
+
+    /**
+     * Test that add(Control) works properly.
+     */
+    public void testAdd() {
+        Form form = new Form("form");
+        form.add(new TextField("name"), 1);
+
+        // Check that add does not allow null arguments
+        try {
+            form.add(null);
+            fail("Control cannot be null");
+        } catch (Exception expected) {
+        }
+
+        // Check that field can be added
+        try {
+            form.add(new TextField("field"));
+        } catch (Exception e) {
+            fail("TextField can be added");
+        }
+
+        // Check that button can be added
+        try {
+            form.add(new Button("button"));
+        } catch (Exception e) {
+            fail("Button can be added");
+        }
+        
+        // Check that table can be added
+        try {
+            form.add(new Table("table"));
+        } catch (Exception e) {
+            fail("Table can be added");
+        }
+    }
+    
+    /**
+     * Test that add(Control, int) works properly.
+     */
+    public void testAddWidth() {
+        Form form = new Form("form");
+        form.add(new TextField("name"), 1);
+
+        // Check that add does not allow null arguments
+        try {
+            form.add(null, 1);
+            fail("Control cannot be null");
+        } catch (Exception expected) {
+        }
+
+        // Check that field can be added
+        try {
+            form.add(new TextField("field"), 1);
+        } catch (Exception e) {
+            fail("TextField can be added");
+        }
+        
+        // Check that table can be added
+        try {
+            form.add(new Table("table"), 1);
+        } catch (Exception e) {
+            fail("Table can be added");
+        }
+
+        // Check that add fails when width is less than 1
+        try {
+            form.add(new TextField("name"), 0);
+            fail("Width cannot be less than 1");
+        } catch (Exception expected) {
+        }
+
+        // Check that add fails when button is added
+        try {
+            form.add(new Button("button"), 1);
+            fail("Button cannot have width set");
+        } catch (Exception expected) {
+        }
+    }
+    
+    /**
+     * Div container used for testing.
+     */
+    static class Div extends AbstractContainer {
+
+        /**
+         * Constructor.
+         */
+        public Div(String name) {
+            super(name);
+        }
+
+        /**
+         * @return div tag
+         */
+        public String getTag() {
+            return "div";
+        }
     }
 }
