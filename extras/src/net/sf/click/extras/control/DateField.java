f028fc8df4323fe7a8d94eb00ad35580b01c4ed2
Bob Schellink <sabob@apache.org>
2008-11-28 12:56:06 +0000

    add Romanian language
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@721464 13f79535-47bb-0310-9956-ffa450edef68

--- extras/src/net/sf/click/extras/control/DateField.java
+++ extras/src/net/sf/click/extras/control/DateField.java
@@ -105,7 +105,7 @@ public class DateField extends TextField {
         { ".gif", ".js", "-al.js", "-bg.js", "-cs.js", "-da.js", "-de.js",
           "-el.js", "-en.js", "-es.js", "-fi.js", "-fr.js", "-he.js", "-it.js",
           "-ja.js", "-ko.js", "-lt.js", "-lv.js", "-nl.js", "-no.js", "-pl.js",
-          "-pt.js", "-ru.js", "-sk.js", "-sv.js", "-zh.js", "-blue.css",
+          "-pt.js", "-ro.js", "-ru.js", "-sk.js", "-sv.js", "-zh.js", "-blue.css",
           "-blue2.css", "-brown.css", "-green.css", "-system.css", "-tas.css",
           "-win2k-1.css", "-win2k-2.css", "-win2k-cold-1.css",
           "-win2k-cold-2.css", "-menuarrow.gif", "-menuarrow2.gif" };
@@ -113,7 +113,8 @@ public class DateField extends TextField {
     /** Supported locales. */
     static final String[] SUPPORTTED_LANGUAGES =
         {"al", "bg", "cs", "da", "de", "el", "en", "es", "fi", "fr", "he", "it",
-         "ja", "ko", "lt", "lv", "nl", "no", "pl", "pt", "ru", "sk", "sv", "zh"};
+         "ja", "ko", "lt", "lv", "nl", "no", "pl", "pt", "ro", "ru", "sk", "sv",
+         "zh"};
 
     // ----------------------------------------------------- Instance Variables
 
