ab20af358e3eea5a223945068013b8ce787527d4
Bob Schellink <sabob@apache.org>
2008-12-19 15:16:04 +0000

    Removed duplicate java template files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@728047 13f79535-47bb-0310-9956-ffa450edef68

--- click/template/WebContent/WEB-INF/classes/com/quickstart/page/LoginPage.java
+++
@@ -1,7 +0,0 @@
-package com.quickstart.page;
-
-public class LoginPage extends BorderPage {
-
-	public String title = "Login";
-
-}
--- click/template/WebContent/WEB-INF/classes/com/quickstart/page/user/User1Page.java
+++
@@ -1,9 +0,0 @@
-package com.quickstart.page.user;
-
-import com.quickstart.page.BorderPage;
-
-public class User1Page extends BorderPage {
-    
-    public String title = "User 1 Page";
-
-}
