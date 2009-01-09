004eb6f4d467b6459a6c0274b4f377a2df68e62d
Bob Schellink <sabob@apache.org>
2009-01-09 15:34:00 +0000

    Rewrote the "Custom Form Layout" example, because SitePoint Terms and Conditions isn't corporate friendly.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@733062 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/org/apache/click/examples/control/html/cssform/CssForm.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.click.examples.control.html.cssform;
+
+import org.apache.click.extras.control.HtmlForm;
+import org.apache.click.util.ClickUtils;
+import org.apache.click.util.HtmlStringBuffer;
+
+/**
+ * A custom Form that adds the Html import, "cssform.css", to the page.
+ *
+ * @author Bob Schellink
+ */
+public class CssForm extends HtmlForm {
+
+    // ---------------------------------------------------- Public Constructors
+
+    public CssForm(String name) {
+        super(name);
+    }
+
+    // --------------------------------------------------------- Public Methods
+
+    public String getHtmlImports() {
+        HtmlStringBuffer buffer = new HtmlStringBuffer(512);
+
+        // Include the Form default imports
+        buffer.append(super.getHtmlImports());
+
+        // Include CSS for ContactDetailsForm
+        String imports = "<link type=\"text/css\" rel=\"stylesheet\" href=\"{0}/assets/css/cssform{1}.css\"/>\n";
+        buffer.append(ClickUtils.createHtmlImport(imports, getContext()));
+        return buffer.toString();
+    }
+}
---
+++ examples/src/org/apache/click/examples/control/html/cssform/VerticalFieldSet.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.click.examples.control.html.cssform;
+
+import org.apache.click.Control;
+import org.apache.click.control.Field;
+import org.apache.click.examples.control.html.FeedbackBorder;
+import org.apache.click.examples.control.html.FieldLabel;
+import org.apache.click.examples.control.html.list.HtmlList;
+import org.apache.click.examples.control.html.list.ListItem;
+import org.apache.click.extras.control.HtmlFieldSet;
+
+/**
+ * A custom FieldSet that renders its fields vertically using an HtmlList
+ * instance.
+ */
+public class VerticalFieldSet extends HtmlFieldSet {
+
+    // -------------------------------------------------------------- Variables
+
+    private HtmlList htmlList = new HtmlList();
+
+    // ---------------------------------------------------- Public Constructors
+
+    public VerticalFieldSet(String name) {
+        super(name);
+        add(htmlList);
+    }
+
+    // --------------------------------------------------------- Public Methods
+
+    public Control insert(Control control, int index) {
+        if (!(control instanceof HtmlList)) {
+            throw new IllegalArgumentException("only HtmlLists can be added to CssFieldSet.");
+        }
+        return super.insert(control, index);
+    }
+
+    public Field add(Field field) {
+        return add(field, null);
+    }
+
+    public Field add(Field field, String labelStr) {
+        ListItem item = new ListItem();
+        htmlList.add(item);
+
+        field.setAttribute("class", "text");
+        FieldLabel label = null;
+        if (labelStr != null) {
+            label = new FieldLabel(field, labelStr);
+        } else {
+            label = new FieldLabel(field);
+        }
+        item.add(label);
+
+        FeedbackBorder border = new FeedbackBorder();
+        border.add(field);
+        item.add(border);
+        return field;
+    }
+
+    public HtmlList getHtmlList() {
+        return htmlList;
+    }
+}
