0d35b6d1f1a25d765ed49024aa149c3493dd3f1e
Bob Schellink <sabob@apache.org>
2009-01-11 14:40:55 +0000

    migrated click-cayenne to Apache namespace
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@733478 13f79535-47bb-0310-9956-ffa450edef68

--- examples/click-cayenne/webapp/WEB-INF/src/click/cayenne/entity/Department.java
+++ examples/click-cayenne/webapp/WEB-INF/src/click/cayenne/entity/Department.java
@@ -1,11 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package click.cayenne.entity;
 
 import java.util.List;
 
-import org.objectstyle.cayenne.exp.ExpressionFactory;
-import org.objectstyle.cayenne.query.SelectQuery;
-import org.objectstyle.cayenne.validation.SimpleValidationFailure;
-import org.objectstyle.cayenne.validation.ValidationResult;
+import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.query.SelectQuery;
+import org.apache.cayenne.validation.SimpleValidationFailure;
+import org.apache.cayenne.validation.ValidationResult;
 
 import click.cayenne.entity.auto._Department;
 
--- examples/click-cayenne/webapp/WEB-INF/src/click/cayenne/entity/auto/_Person.java
+++ examples/click-cayenne/webapp/WEB-INF/src/click/cayenne/entity/auto/_Person.java
@@ -8,7 +8,7 @@ import java.util.List;
  * since it may be overwritten next time code is regenerated. 
  * If you need to make any customizations, please use subclass. 
  */
-public class _Person extends org.objectstyle.cayenne.CayenneDataObject {
+public class _Person extends org.apache.cayenne.CayenneDataObject {
 
     private static final long serialVersionUID = -4456863269273520419L;
     
