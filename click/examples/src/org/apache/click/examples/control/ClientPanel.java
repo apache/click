6a1a2c27ad434a932ff2c0ea677f73562dddd954
Bob Schellink <sabob@apache.org>
2009-03-16 15:19:33 +0000

    Added reusable Panel demo
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@754912 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/examples/src/org/apache/click/examples/control/ClientPanel.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.click.examples.control;
+
+import org.apache.click.control.Form;
+import org.apache.click.control.Panel;
+import org.apache.click.control.Submit;
+import org.apache.click.control.TextField;
+import org.apache.click.extras.control.DateField;
+import org.apache.click.extras.control.DoubleField;
+
+/**
+ * Provides a reusable Panel to capture Client details.
+ *
+ * @author Bob Schellink
+ */
+public class ClientPanel extends Panel {
+
+    private static final long serialVersionUID = 1L;
+
+    private Form form = new Form("form");
+
+    public ClientPanel(String name) {
+        super(name);
+    }
+
+    public void onInit() {
+        form.add(new TextField("name")).setRequired(true);
+        form.add(new DateField("dateJoined"));
+        form.add(new DoubleField("holdings"));
+
+        form.add(new Submit("submit", this, "onSubmit"));
+        form.add(new Submit("cancel", this, "onCancel"));
+
+        add(form);
+    }
+
+    public boolean onSubmit() {
+        if (form.isValid()) {
+            // In real app one would store client in database
+            addModel("msg", "Successfully created new Client: '"
+                + form.getFieldValue("name") + "'.");
+        }
+        return true;
+    }
+
+    public boolean onCancel() {
+        addModel("msg", "Cancelled.");
+
+        return true;
+    }
+}
