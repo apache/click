e4b0c18eb58debb18b973193ad45f64fcbc460c6
Bob Schellink <sabob@apache.org>
2009-04-22 21:45:57 +0000

    added demo and javadoc for custom notification messages
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@767682 13f79535-47bb-0310-9956-ffa450edef68

--- click/examples/src/org/apache/click/examples/page/control/SubmitLinkDemo.java
+++ click/examples/src/org/apache/click/examples/page/control/SubmitLinkDemo.java
@@ -36,21 +36,29 @@ import org.apache.click.extras.control.SubmitLink;
  */
 public class SubmitLinkDemo extends BorderPage {
 
-    /** A submit link. */
-    private SubmitLink submitLink = new SubmitLink("submit");
-
-    /** A submit link which includes parameters. */
-    private SubmitLink paramLink = new SubmitLink("paramLink");
-
-    /** A submit link used outside of a Form control. */
-    private SubmitLink standaloneLink = new SubmitLink("standaloneLink");
-
     public String demo1Msg;
 
     public String demo2Msg;
 
+    public String demo3Msg;
+
+    public String demo4Msg;
+
     public SubmitLinkDemo() {
-        Form form = new Form("form");
+        demo1();
+
+        demo2();
+
+        demo3();
+
+        demo4();
+    }
+
+    public void demo1() {
+        // Create a submit link.
+        final SubmitLink submitLink = new SubmitLink("submit");
+
+        Form form = new Form("demo1");
         addControl(form);
 
         FieldSet fieldSet = new FieldSet("fieldSet");
@@ -61,6 +69,29 @@ public class SubmitLinkDemo extends BorderPage {
         // Add the submit link to the fieldSet
         fieldSet.add(submitLink);
 
+        // The SubmitLink action listener
+        submitLink.setActionListener(new ActionListener() {
+
+            public boolean onAction(Control source) {
+                demo1Msg = submitLink.getName() + ".onAction invoked at "
+                    + (new Date());
+                return true;
+            }
+        });
+    }
+
+    public void demo2() {
+        // Create a submit link which includes parameters.
+        final SubmitLink paramLink = new SubmitLink("paramLink");
+
+        Form form = new Form("demo2");
+        addControl(form);
+
+        FieldSet fieldSet = new FieldSet("fieldSet");
+        form.add(fieldSet);
+
+        fieldSet.add(new TextField("name"));
+
         // Add some parameters to the parameterized submit link
         paramLink.setValue("myValue");
         paramLink.setParameter("x", "100");
@@ -68,26 +99,21 @@ public class SubmitLinkDemo extends BorderPage {
         // Add the parameterized submit link to the FieldSet
         fieldSet.add(paramLink);
 
-        // The SubmitLink action listener
-        submitLink.setActionListener(new ActionListener() {
-
-            public boolean onAction(Control source) {
-                demo1Msg = source.getName() + ".onAction invoked at " +
-                    (new Date());
-                return true;
-            }
-        });
-
         // The Parameterized SubmitLink action listener
         paramLink.setActionListener(new ActionListener() {
 
             public boolean onAction(Control source) {
-                demo1Msg = source.getName() + ".onAction invoked at " +
-                    (new Date());
-                demo1Msg += "<br>Parameters:" + paramLink.getParameters();
+                demo2Msg = paramLink.getName() + ".onAction invoked at "
+                    + (new Date());
+                demo2Msg += "<br>Parameters:" + paramLink.getParameters();
                 return true;
             }
         });
+    }
+
+    public void demo3() {
+        // Create a standalone submit link.
+        final SubmitLink standaloneLink = new SubmitLink("standaloneLink");
 
         // Add the Standalone SubmitLink to the Page
         addControl(standaloneLink);
@@ -96,8 +122,39 @@ public class SubmitLinkDemo extends BorderPage {
         standaloneLink.setActionListener(new ActionListener() {
 
             public boolean onAction(Control source) {
-                demo2Msg = source.getName() + ".onAction invoked at " +
+                demo3Msg = source.getName() + ".onAction invoked at " +
                     (new Date());
+                return true;
+            }
+        });
+    }
+
+    public void demo4() {
+        // Create a submit link
+        final SubmitLink confirmationLink = new SubmitLink("confirmationLink");
+
+        Form form = new Form("demo4");
+        addControl(form);
+
+        FieldSet fieldSet = new FieldSet("fieldSet");
+        form.add(fieldSet);
+
+        fieldSet.add(new TextField("name"));
+
+        // Add the submit link to the FieldSet
+        fieldSet.add(confirmationLink);
+
+        // Set custom JavaScript for the onclick event. The confirmSubmit function
+        // is defined in the page template -> submit-link-demo.htm
+        String clickEvent = "return confirmSubmit(this, '" + form.getName() + "', 'Are you sure?');";
+        confirmationLink.setOnClick(clickEvent);
+
+        // The Parameterized SubmitLink action listener
+        confirmationLink.setActionListener(new ActionListener() {
+
+            public boolean onAction(Control source) {
+                demo4Msg = confirmationLink.getName() + ".onAction invoked at "
+                    + (new Date());
                 return true;
             }
         });
