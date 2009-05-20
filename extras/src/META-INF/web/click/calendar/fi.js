64b6a3513fe0428182a56fd7c523533f9b2e8a02
Bob Schellink <sabob@apache.org>
2009-05-20 20:16:48 +0000

    added Clear localized message
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@776827 13f79535-47bb-0310-9956-ffa450edef68

--- extras/src/META-INF/web/click/calendar/fi.js
+++ extras/src/META-INF/web/click/calendar/fi.js
@@ -17,12 +17,13 @@
 // THE SOFTWARE.
 
 Date.weekdays = $w("Ma Ti Ke To Pe La Su");
-Date.months = $w("Tammikuu Helmikuu Maaliskuu Huhtikuu Toukokuu Kes�kuu Hein�kuu Elokuu Syyskuu Lokakuu Marraskuu Joulukuu" );
+Date.months = $w("Tammikuu Helmikuu Maaliskuu Huhtikuu Toukokuu Kesäkuu Heinäkuu Elokuu Syyskuu Lokakuu Marraskuu Joulukuu" );
 
 Date.first_day_of_week = 1
 
 _translations = {
   "OK": "OK",
   "Now": "Nyt",
-  "Today": "T�n��n"
+  "Today": "Tänään",
+  "Clear" : "Tyhjennä"
 }
