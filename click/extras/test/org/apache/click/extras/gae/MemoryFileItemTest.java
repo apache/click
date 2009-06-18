d58b7a9d45017b34fb6522c35b4c38ce08fdd5f6
Bob Schellink <sabob@apache.org>
2009-06-18 21:54:32 +0000

    added MemoryFileItem tests. CLK-560
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@786306 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/extras/test/org/apache/click/extras/gae/MemoryFileItemTest.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.click.extras.gae;
+
+import java.io.File;
+import junit.framework.TestCase;
+import org.apache.click.MockContainer;
+import org.apache.click.control.FileField;
+import org.apache.click.control.Form;
+import org.apache.click.extras.pages.MemoryFileItemUploadPage;
+import org.apache.click.servlet.MockRequest;
+
+/**
+ * Provides tests for the Google App Engine MemoryFileItem.
+ *
+ * @author Bob Schellink
+ */
+public class MemoryFileItemTest extends TestCase {
+
+    /**
+     * Test Google App Engine (GAE) MemoryFileItem support.
+     */
+    public void testMemoryFileItem() {
+        MockContainer container = new MockContainer("web");
+        container.start();
+
+        String firstname = "Steve";
+        String lastname = "Jones";
+
+        // Lookup path to files
+        String file1Path = container.getServletContext().getRealPath("upload1.txt");
+        String file2Path = container.getServletContext().getRealPath("upload2.txt");
+
+        File upload1 = new File(file1Path);
+        File upload2 = new File(file2Path);
+        MockRequest request = container.getRequest();
+
+        // Add couple of files to be uploaded
+        request.addFile("upload1", upload1, "utf-8");
+        request.addFile("upload2", upload2, "utf-8");
+
+        // Set firstname and lastname request parameters
+        request.setParameter("firstname", firstname);
+        request.setParameter("lastname", lastname);
+
+        // Set FORM_NAME parameter to ensure Form is submitted
+        request.setParameter(Form.FORM_NAME, "form");
+
+        MemoryFileItemUploadPage page = (MemoryFileItemUploadPage) container.testPage(MemoryFileItemUploadPage.class);
+
+        // Check that field parameters were processed
+        assertEquals(firstname, page.getForm().getFieldValue("firstname"));
+        assertEquals(lastname, page.getForm().getFieldValue("lastname"));
+
+        // Check that file field parameters were processed
+        FileField fileField1 = (FileField) page.getForm().getField("upload1");
+        FileField fileField2 = (FileField) page.getForm().getField("upload2");
+        assertEquals("upload1 success", fileField1.getFileItem().getString());
+        assertEquals("upload2 success", fileField2.getFileItem().getString());
+
+        container.stop();
+    }
+}
