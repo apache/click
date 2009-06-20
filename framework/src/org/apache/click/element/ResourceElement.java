1182d852168d2a86c7236317810d24a1e473ca2e
Bob Schellink <sabob@apache.org>
2009-06-20 13:11:26 +0000

    Added serialversionuid to element subclasses
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk/click@786810 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/org/apache/click/element/ResourceElement.java
+++ framework/src/org/apache/click/element/ResourceElement.java
@@ -171,6 +171,8 @@ import org.apache.commons.lang.StringUtils;
  */
 public class ResourceElement extends Element {
 
+    private static final long serialVersionUID = 1L;
+
     // -------------------------------------------------------------- Constants
 
     /**
