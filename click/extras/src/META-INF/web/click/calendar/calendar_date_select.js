a3a34789c7c5b8e934383536e932fddb585f1434
Bob Schellink <sabob@apache.org>
2009-07-12 13:48:41 +0000

    fixed misspelled name
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@793350 13f79535-47bb-0310-9956-ffa450edef68

--- click/extras/src/META-INF/web/click/calendar/calendar_date_select.js
+++ click/extras/src/META-INF/web/click/calendar/calendar_date_select.js
@@ -1,4 +1,4 @@
-// Copyright 2009 Tim Charper (http://code.google.com/p/calendardateselect/)
+// Copyright 2009 Tim Harper (http://code.google.com/p/calendardateselect/)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
