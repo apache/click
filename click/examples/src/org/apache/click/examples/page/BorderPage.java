ebecc261853ff82bafd50e88716047aa264acf56
Bob Schellink <sabob@apache.org>
2010-01-12 14:36:47 +0000

    add comment
    git-svn-id: https://svn.apache.org/repos/asf/incubator/click/trunk@898343 13f79535-47bb-0310-9956-ffa450edef68

--- click/examples/src/org/apache/click/examples/page/BorderPage.java
+++ click/examples/src/org/apache/click/examples/page/BorderPage.java
@@ -78,6 +78,9 @@ public class BorderPage extends Page {
         addControl(rootMenu);
     }
 
+    /**
+     * @see org.apache.click.Page#onDestroy()
+     */
     @Override
     public void onDestroy() {
         // Remove menu for when BorderPage is Serialized to disk
