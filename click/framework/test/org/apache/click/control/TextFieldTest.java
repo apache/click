e1b95f255cc20897e3c5adb7b3c674a1439b7abf
Bob Schellink <sabob@apache.org>
2010-03-14 02:18:56 +0000

    added Field trim property. CLK-627
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk@922727 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/test/org/apache/click/control/TextFieldTest.java
+++ click/framework/test/org/apache/click/control/TextFieldTest.java
@@ -103,4 +103,151 @@ public class TextFieldTest extends TestCase {
         // Check that the value <script> is not rendered
         assertTrue(field.toString().indexOf(value) < 0);
     }
+
+    /**
+     * Check that TextField label style is rendered by Form and FieldSet.
+     *
+     * CLK-595
+     */
+    public void testLabelStyle() {
+        MockContext.initContext();
+
+        // Check that Form renders the field label style
+        Form form = new Form("form");
+        Field field = new TextField("field");
+        form.add(field);
+
+        field.setLabelStyle("color: green");
+        assertTrue(form.toString().contains("<label for=\"form_field\" style=\"color: green\">"));
+
+        // Check that FieldSet renders the field label style
+        form = new Form("form");
+        FieldSet fs = new FieldSet("fs");
+        form.add(fs);
+        field = new TextField("field");
+        fs.add(field);
+
+        field.setLabelStyle("color: green");
+        assertTrue(fs.toString().contains("<label for=\"form_field\" style=\"color: green\">"));
+    }
+
+    /**
+     * Check that TextField label style class is rendered by Form and FieldSet.
+     *
+     * CLK-595
+     */
+    public void testLabelStyleClass() {
+        MockContext.initContext();
+
+        // Check that Form renders the field label style
+        Form form = new Form("form");
+        Field field = new TextField("field");
+        form.add(field);
+
+        field.setLabelStyleClass("autumn");
+        assertTrue(form.toString().contains("<label for=\"form_field\" class=\"autumn\">"));
+
+        // Check that FieldSet renders the field label style
+        form = new Form("form");
+        // FieldStyle value should be overridden by the parentStyleHint below
+        form.setFieldStyle("font-weight:bold");
+        FieldSet fs = new FieldSet("fs");
+        form.add(fs);
+        field = new TextField("field");
+        fs.add(field);
+
+        field.setLabelStyleClass("autumn");
+        assertTrue(fs.toString().contains("<label for=\"form_field\" class=\"autumn\">"));
+    }
+
+    /**
+     * Check that TextField parent style hint is rendered by Form and FieldSet.
+     *
+     * CLK-595
+     */
+    public void testParentStyleHint() {
+        MockContext.initContext();
+
+        // Check that Form renders the field label style
+        Form form = new Form("form");
+        Field field = new TextField("field");
+        form.add(field);
+
+        field.setParentStyleHint("color: green");
+        // Check that style hint is rendered on the label and field cells
+        assertTrue(form.toString().contains("<td class=\"fields\" align=\"left\" style=\"color: green\"><label"));
+        assertTrue(form.toString().contains("<td align=\"left\" style=\"color: green\"><input"));
+
+        // Check that FieldSet renders the field label style
+        form = new Form("form");
+        // FieldStyle value should be overridden by the parentStyleHint below
+        form.setFieldStyle("font-weight:bold");
+        FieldSet fs = new FieldSet("fs");
+        form.add(fs);
+        field = new TextField("field");
+        fs.add(field);
+
+        field.setParentStyleHint("color: green");
+        // Check that style hint is rendered on the label and field cells
+        assertTrue(fs.toString().contains("<td class=\"fields\" align=\"left\" style=\"color: green\"><label"));
+        assertTrue(fs.toString().contains("<td align=\"left\" style=\"color: green\"><input"));
+    }
+
+    /**
+     * Check that TextField parent style class hint is rendered by Form and FieldSet.
+     *
+     * CLK-595
+     */
+    public void testParentStyleClassHint() {
+        MockContext.initContext();
+
+        // Check that Form renders the field label style
+        Form form = new Form("form");
+        Field field = new TextField("field");
+        form.add(field);
+
+        field.setParentStyleClassHint("autumn");
+        // Check that style class hint is rendered on the label and field cells
+        assertTrue(form.toString().contains("<td class=\"fields autumn\" align=\"left\"><label"));
+        assertTrue(form.toString().contains("<td class=\"autumn\" align=\"left\"><input"));
+
+        // Check that FieldSet renders the field label style
+        form = new Form("form");
+        FieldSet fs = new FieldSet("fs");
+        form.add(fs);
+        field = new TextField("field");
+        fs.add(field);
+
+        field.setParentStyleClassHint("autumn");
+        // Check that style class hint is rendered on the label and field cells
+        assertTrue(fs.toString().contains("<td class=\"fields autumn\" align=\"left\"><label"));
+        assertTrue(fs.toString().contains("<td class=\"autumn\" align=\"left\"><input"));
+    }
+
+    /**
+     * Test the TextField trim property behavior.
+     *
+     * CLK-627
+     */
+    public void testTrim() {
+         String trimmedValue = "value";
+         String value = " " + trimmedValue + " ";
+
+         MockContext context = MockContext.initContext();
+         context.getMockRequest().setParameter("field", value);
+
+        Field field = new TextField("field");
+        field.onProcess();
+
+        // Check that the field trims its request value
+        assertEquals(trimmedValue, field.getValue());
+
+        // Test again, this time switching off the trim property
+        field = new TextField("field");
+        field.setTrim(false);
+        field.onProcess();
+
+        // Check that field does not trim its request value
+        assertEquals(value, field.getValue());
+    }
 }
