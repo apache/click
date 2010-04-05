4081dbf7b3589e57211fd83e00bae680edf8ff65
Bob Schellink <sabob@apache.org>
2010-04-05 08:11:38 +0000

    updated tests
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk/click@930825 13f79535-47bb-0310-9956-ffa450edef68

--- extras/test/org/apache/click/extras/control/TabbedFormTest.java
+++ extras/test/org/apache/click/extras/control/TabbedFormTest.java
@@ -24,21 +24,22 @@ import org.apache.click.MockContext;
 
 public class TabbedFormTest extends TestCase {
 
-    public void testGetHtmlImport() {
+    public void testGetHeadElements() {
         MockContainer container = new MockContainer("web");
         container.start();
 
         // MockContext is created when a container tests a page. There
         // are no pages to test here so we manually create a MockContext,
         // but reuse the Mock Servlet objects created in the container.
-        MockContext.initContext(container.getServletConfig(), container.getRequest(), container.getResponse(), container.getClickServlet());
+        MockContext.initContext(container.getServletConfig(),
+            container.getRequest(), container.getResponse(), container.getClickServlet());
 
         TabbedForm form = new TabbedForm("form");
 
         assertTrue(form.toString().indexOf("<form") > 0);
-        assertTrue(form.getHtmlImports().indexOf("/control.js") > 0);
-        assertTrue(form.getHtmlImports().indexOf("/control.css") > 0);
-        assertTrue(form.getHtmlImports().indexOf("/extras-control.css") > 0);
+        assertTrue(form.getHeadElements().toString().indexOf("/control.js") > 0);
+        assertTrue(form.getHeadElements().toString().indexOf("/control.css") > 0);
+        assertTrue(form.getHeadElements().toString().indexOf("/extras-control.css") > 0);
         
         container.stop();
     }
