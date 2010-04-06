08829ee53b7925e4050b4c8923fab4432066f518
Bob Schellink <sabob@apache.org>
2010-04-06 10:32:42 +0000

    added polish and dutch translations. CLK-624
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk/click@931077 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extras/src/META-INF/resources/click/calendar/nl.js
@@ -0,0 +1,41 @@
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// Full month names
+Date.months = new Array('Januari','Februari','Maart','April','Mei','Juni','Juli','Augustus','September','Oktober','November','December');
+
+// Month abbreviations
+Date.monthAbbreviations = new Array('Jan','Feb','Mrt','Apr','Mei','Jun','Jul','Aug','Sep','Okt','Nov','Dec');
+
+// Full day names
+Date.dayNames = new Array('Zondag','Maandag','Dinsdag','Woensdag','Donderdag','Vrijdag','Zaterdag');
+
+// Day abbreviations
+Date.dayAbbreviations = new Array('Zo','Ma','Di','Wo','Do','Vr','Za');
+
+// Weekdays displayed by popup calendar
+Date.weekdays = new Array('Z','M','D','W','D','V','Z');
+
+Date.first_day_of_week = 0;
+
+_translations = {
+    "OK": "OK",
+    "Now": "Nu",
+    "Today": "Vandaag",
+    "Clear": "Wissen"
+}
