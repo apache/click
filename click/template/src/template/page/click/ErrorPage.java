c2e4af85316b0913ccae9a1014715ee3d9de7922
Bob Schellink <sabob@apache.org>
2010-04-12 12:02:44 +0000

    updated quickproject
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk@933204 13f79535-47bb-0310-9956-ffa450edef68

--- click/template/src/template/page/click/ErrorPage.java
+++ click/template/src/template/page/click/ErrorPage.java
@@ -1,14 +1,24 @@
 package template.page.click;
 
 import org.apache.click.extras.control.Menu;
+import org.apache.click.extras.control.MenuFactory;
 
 public class ErrorPage extends org.apache.click.util.ErrorPage {
 
     public String title = "Error Page";
 
-    public Menu rootMenu = Menu.getRootMenu();
+    private Menu rootMenu;
 
+    public ErrorPage() {
+        MenuFactory menuFactory = new MenuFactory();
+        rootMenu = menuFactory.getRootMenu();
+        addControl(rootMenu);
+    }
+
+    @Override
     public void onDestroy() {
+        super.onDestroy();
+
         Throwable error = getError();
 
         // TODO: Log error to Log4J or Commons Logger
