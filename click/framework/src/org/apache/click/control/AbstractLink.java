99e8d44bbf728dbb36fcf56d6cd5ca7293568f50
Adrian Antal <aadrian@apache.org>
2010-05-23 18:06:42 +0000

    CLK-545  - Better rendering of disabled link controls
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk@947454 13f79535-47bb-0310-9956-ffa450edef68

--- click/framework/src/org/apache/click/control/AbstractLink.java
+++ click/framework/src/org/apache/click/control/AbstractLink.java
@@ -22,9 +22,12 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import org.apache.click.Context;
+import org.apache.click.element.CssImport;
+import org.apache.click.element.Element;
 import org.apache.click.util.ClickUtils;
 import org.apache.click.util.HtmlStringBuffer;
 
@@ -518,6 +521,38 @@ public abstract class AbstractLink extends AbstractControl {
 
         if (isDisabled()) {
 
+            buffer.elementStart(getTag());
+
+            buffer.appendAttribute("href", getHref());
+            buffer.appendAttribute("id", getId());
+            buffer.appendAttribute("title", "Disabled Link!");
+            addStyleClass("controlDisabled");
+
+            buffer.appendAttribute("class", getAttribute("class"));
+            if (hasAttribute("style")) {
+                buffer.appendAttribute("style", getAttribute("style"));
+            }
+            buffer.appendAttribute("onClick", "return false;");
+            appendAttributes(buffer);
+
+            buffer.closeTag();
+
+            if (StringUtils.isBlank(getImageSrc())) {
+                buffer.append(getLabel());
+
+            } else {
+                renderImgTag(buffer);
+                if (isRenderLabelAndImage()) {
+                    buffer.elementStart("span").closeTag();
+                    buffer.append(getLabel());
+                    buffer.elementEnd("span");
+                }
+            }
+
+            buffer.elementEnd(getTag());
+            
+            
+/*
             buffer.elementStart("span");
 
             addStyleClass("disabled");
@@ -537,6 +572,7 @@ public abstract class AbstractLink extends AbstractControl {
             }
 
             buffer.elementEnd("span");
+*/
 
         } else {
             buffer.elementStart(getTag());
@@ -568,6 +604,30 @@ public abstract class AbstractLink extends AbstractControl {
         }
     }
 
+    /**
+     * Return the Form HTML HEAD elements for the following resources:
+     *
+     * <ul>
+     * <li><tt>click/control.css</tt></li>
+     * </ul>
+     *
+     * @see org.apache.click.Control#getHeadElements()
+     *
+     * @return the form list of HEAD elements to be included in the page
+     */
+    @Override
+    public List<Element> getHeadElements() {
+        if (headElements == null) {
+            headElements = super.getHeadElements();
+
+            Context context = getContext();
+            String versionIndicator = ClickUtils.getResourceVersionIndicator(context);
+
+            headElements.add(new CssImport("/click/control.css", versionIndicator));
+        }
+        return headElements;
+    }
+    
     // Protected Methods ------------------------------------------------------
 
     /**
