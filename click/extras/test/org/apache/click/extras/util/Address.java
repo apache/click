b891e9f0482649e3bc2cc4bf0857b1d22e1616af
Bob Schellink <sabob@apache.org>
2010-06-16 09:33:04 +0000

    moved tests under extras package
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk@955165 13f79535-47bb-0310-9956-ffa450edef68

---
+++ click/extras/test/org/apache/click/extras/util/Address.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.click.extras.util;
+
+public class Address {
+    private Integer id;
+    private String lineOne;
+    private String lineTwo;
+    private String lineThree;
+    private State state;
+    private boolean active;
+    private Boolean registered;
+    
+    public Boolean isRegistered() {
+        return registered;
+    }
+    public void setRegistered(Boolean registered) {
+        this.registered = registered;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public State getState() {
+        return state;
+    }
+    public void setState(State state) {
+        this.state = state;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getLineOne() {
+        return lineOne;
+    }
+    public void setLineOne(String lineOne) {
+        this.lineOne = lineOne;
+    }
+    public String getLineThree() {
+        return lineThree;
+    }
+    public void setLineThree(String lineThree) {
+        this.lineThree = lineThree;
+    }
+    public String getLineTwo() {
+        return lineTwo;
+    }
+    public void setLineTwo(String lineTwo) {
+        this.lineTwo = lineTwo;
+    }
+}
---
+++ click/extras/test/org/apache/click/extras/util/SampleObject.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.click.extras.util;
+
+public class SampleObject {
+    private Integer id;
+    private String name;
+    private java.util.Date dateOfBirth;
+    private boolean _boolean;
+    private int _int;
+    private double _double;
+    private String telephone;
+    private boolean active;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public java.util.Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(java.util.Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    public boolean isBoolean() {
+        return _boolean;
+    }
+    
+    public void setBoolean(boolean b) {
+        _boolean = b;
+    }
+    
+    public double getDouble() {
+        return _double;
+    }
+    
+    public void setDouble(double d) {
+        _double = d;
+    }
+    
+    public int getInt() {
+        return _int;
+    }
+    
+    public void setInt(int i) {
+        _int = i;
+    }
+    
+    public String getTelephone() {
+        return telephone;
+    }
+    
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+}
