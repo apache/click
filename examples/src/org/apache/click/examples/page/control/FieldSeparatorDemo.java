a832c0bbb3b050e85bd975b49b57c43cf96a6b60
Adrian Antal <aadrian@apache.org>
2010-06-20 11:00:21 +0000

    improve FieldSeparator example layout.
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk/click@956344 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/org/apache/click/examples/page/control/FieldSeparatorDemo.java
+++ examples/src/org/apache/click/examples/page/control/FieldSeparatorDemo.java
@@ -10,20 +10,49 @@ import org.apache.click.util.Bindable;
 import org.apache.click.util.ContainerUtils;
 
 /**
- * Provides a form FieldSet usage as Separator example.
+ * Provides a FieldSeparator usage example vs the classical FieldSet usage.
+ *
+ * @see org.apache.click.examples.control.FieldSeparator
+ * @see org.apache.click.control.FieldSet
  */
 public class FieldSeparatorDemo extends BorderPage {
     private static final long serialVersionUID = 1L;
 
-    @Bindable protected Form classicForm = new Form();
     @Bindable protected Form newForm = new Form();
+    @Bindable protected Form classicForm = new Form();
 
     public FieldSeparatorDemo() {
-        // field set based form
-        makeFieldSetForm();
-
-        // separator based form
+        // a separator based form
         makeSeparatorForm();
+        
+        // a field set based form
+        makeFieldSetForm();
+    }
+
+    private void makeSeparatorForm() {
+        newForm.setLabelAlign(Form.ALIGN_RIGHT);
+        newForm.setButtonAlign(Form.ALIGN_RIGHT);
+        FieldSeparator contactSeparator = new FieldSeparator("contactDetails");
+        newForm.add(contactSeparator); // unlike the FieldSet, we just add the separator to the Form
+        TextField name = new TextField("name", "Name", 30);
+        newForm.add(name); // without adding the fields to it.
+        EmailField email = new EmailField("email");
+        newForm.add(email);
+
+        FieldSeparator feedbackSeparator = new FieldSeparator("feedbackDetails");
+        newForm.add(feedbackSeparator);
+        TextArea comment = new TextArea("Comment");
+        newForm.add(comment);
+
+        Checkbox inform = new Checkbox("inform","Yes, I agree!");
+        newForm.add(inform);
+
+        // Separator without a "legend": much like a HR element but with a consistent style with the other separators.
+        FieldSeparator separator = new FieldSeparator("separator","");
+        newForm.add(separator);
+
+        newForm.add(new Submit("ok", "  OK  ",  this, "onOkClick1"));
+        newForm.add(new PageSubmit("cancel", HomePage.class));
     }
 
     private void makeFieldSetForm() {
@@ -31,7 +60,7 @@ public class FieldSeparatorDemo extends BorderPage {
         classicForm.setButtonAlign(Form.ALIGN_RIGHT);
         FieldSet contactFieldSet = new FieldSet("contactDetails");
         classicForm.add(contactFieldSet);
-        TextField name = new TextField("name");
+        TextField name = new TextField("name", "Name", 30);
         contactFieldSet.add(name);
         EmailField email = new EmailField("email");
         contactFieldSet.add(email);
@@ -43,47 +72,22 @@ public class FieldSeparatorDemo extends BorderPage {
 
         Checkbox inform = new Checkbox("inform", "Yes, I agree!");
         feedbackFieldSet.add(inform);
-        classicForm.add(new Submit("ok", "  OK  ",  this, "onOkClick1"));
+        classicForm.add(new Submit("ok", "  OK  ",  this, "onOkClick2"));
         classicForm.add(new PageSubmit("cancel", HomePage.class));
     }
 
-    private void makeSeparatorForm() {
-        newForm.setLabelAlign(Form.ALIGN_RIGHT);
-        newForm.setButtonAlign(Form.ALIGN_RIGHT);
-        FieldSeparator contactFieldSet = new FieldSeparator("contactDetails");
-        newForm.add(contactFieldSet);
-        TextField name = new TextField("name");
-        newForm.add(name);
-        EmailField email = new EmailField("email");
-        newForm.add(email);
-
-        FieldSeparator feedbackFieldSet = new FieldSeparator("feedbackDetails");
-        newForm.add(feedbackFieldSet);
-        TextArea comment = new TextArea("Comment");
-        newForm.add(comment);
-
-        Checkbox inform = new Checkbox("inform","Yes, I agree!");
-        newForm.add(inform);
-
-        FieldSeparator sepo = new FieldSeparator("separator","");
-        newForm.add(sepo);
-
-        newForm.add(new Submit("ok", "  OK  ",  this, "onOkClick2"));
-        newForm.add(new PageSubmit("cancel", HomePage.class));
-    }
-
     public boolean onOkClick1(){
-        if(classicForm.isValid()){
-            for (Field field : ContainerUtils.getInputFields(classicForm)) {
+        if(newForm.isValid()){
+            for (Field field : ContainerUtils.getInputFields(newForm)) {
                 System.out.println(field.getName() + "=" + field.getValue());
-            }            
+            }
         }
         return true;
     }
 
     public boolean onOkClick2(){
-        if(newForm.isValid()){
-            for (Field field : ContainerUtils.getInputFields(newForm)) {
+        if(classicForm.isValid()){
+            for (Field field : ContainerUtils.getInputFields(classicForm)) {
                 System.out.println(field.getName() + "=" + field.getValue());
             }
         }
