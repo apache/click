fcb851101cbd6011548f4ef735f2758ab7ca1794
Bob Schellink <sabob@apache.org>
2010-09-12 01:28:15 +0000

    added Ajax demos showing how to redirect and implement an onSecurityCheck implementation
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk/click@996232 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/org/apache/click/examples/page/ajax/AjaxRedirectPage.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.click.examples.page.ajax;
+
+import java.util.Date;
+import org.apache.click.ActionResult;
+import org.apache.click.Context;
+import org.apache.click.Control;
+import org.apache.click.ajax.AjaxBehavior;
+import org.apache.click.control.ActionLink;
+import org.apache.click.examples.page.BorderPage;
+
+/**
+ * Provides an <tt>onSecurityCheck</tt> example secure Page for handling Ajax
+ * requests. Two links are presented to the user. Clicking on the first link will
+ * redirect the user to this page and show an error message. Clicking on the
+ * second link will show an error message without redirecting to another page.
+ */
+public class AjaxRedirectPage extends BorderPage {
+
+    private static final long serialVersionUID = 1L;
+    private ActionLink redirectLink = new ActionLink("redirectLink");
+
+    @Override
+    public void onInit() {
+        super.onInit();
+        redirectLink.setId("redirectLinkId");
+        addControl(redirectLink);
+
+        redirectLink.addBehavior(new AjaxBehavior() {
+
+            @Override
+            public ActionResult onAction(Control source) {
+                Context context = getContext();
+                context.setFlashAttribute("flash", "Redirected at " + new Date());
+
+            String redirectUrl = context.getPagePath(AjaxRedirectPage.class);
+            String contextPath = context.getRequest().getContextPath();
+            redirectUrl = contextPath + redirectUrl;
+
+            context.getResponse().setHeader("REDIRECT_URL",redirectUrl);
+
+            // We are redirecting so no response is returned
+            return null;
+            }
+        });
+    }
+}
