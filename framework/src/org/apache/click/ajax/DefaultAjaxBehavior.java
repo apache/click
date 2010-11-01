fda5c6016254d0ca065819f9cb03672a9f1107e1
Bob Schellink <sabob@apache.org>
2010-11-01 10:19:10 +0000

    renamed addHeadElements to addHeadElementsOnce
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk/click@1029601 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/org/apache/click/ajax/DefaultAjaxBehavior.java
+++ framework/src/org/apache/click/ajax/DefaultAjaxBehavior.java
@@ -25,21 +25,29 @@ import org.apache.click.ActionResult;
 
 /**
  * Provides a default implementation of the AjaxBehavior interface.
- *
- * TODO: javadoc
+ * <p/>
+ * This class also provides the method,
+ * {@link #addHeadElementsOnce(org.apache.click.Control) addHeadElementsOnce},
+ * that subclasses can implement if they need to add HTML HEAD elements only to
+ * the <tt>first</tt> Control that this Behavior is registered with.
+ * <p/>
+ * If this Behavior should add HTML HEAD elements to all the Controls it is
+ * registered with, rather implement
+ * {@link #preRenderHeadElements(org.apache.click.Control) preRenderHeadElements}.
  */
 public class DefaultAjaxBehavior implements AjaxBehavior {
 
     // Variables --------------------------------------------------------------
 
+    /** Indicates whether the Behavior HEAD elements have been processed or not. */
     protected boolean headElementsProcessed = false;
 
-    // Behavior Methods--------------------------------------------------------
+    // Behavior Methods -------------------------------------------------------
 
     /**
      * @see org.apache.click.Behavior#onAction(org.apache.click.Control)
      *
-     * @param source the control the behavior is attached to
+     * @param source the control the behavior is registered with
      * @return the action result
      */
     public ActionResult onAction(Control source) {
@@ -47,10 +55,10 @@ public class DefaultAjaxBehavior implements AjaxBehavior {
     }
 
     /**
-     * TODO: javadoc
+     * @see org.apache.click.ajax.AjaxBehavior#isAjaxTarget(org.apache.click.Context)
      *
-     * @param context
-     * @return
+     * @param context the request context
+     * @return true if the behavior is the request target, false otherwise
      */
     public boolean isAjaxTarget(Context context) {
         return true;
@@ -59,19 +67,25 @@ public class DefaultAjaxBehavior implements AjaxBehavior {
     // Callback Methods -------------------------------------------------------
 
     /**
+     * @see org.apache.click.Behavior#preResponse(org.apache.click.Control)
      *
-     * @param source
+     * @param source the control the behavior is registered with
      */
     public void preResponse(Control source) {
     }
 
+    /**
+     * @see org.apache.click.Behavior#preRenderHeadElements(org.apache.click.Control)
+     *
+     * @param source the control the behavior is registered with
+     */
     public void preRenderHeadElements(Control source) {
         // Guard against adding HEAD elements to more than one control
         if (headElementsProcessed) {
             return;
         }
 
-        addHeadElements(source);
+        addHeadElementsOnce(source);
 
         headElementsProcessed = true;
     }
@@ -82,7 +96,19 @@ public class DefaultAjaxBehavior implements AjaxBehavior {
 
     // Protected methods ------------------------------------------------------
 
-    protected void addHeadElements(Control source) {
+    /**
+     * Provides a method for adding HTML HEAD elements to the first Control
+     * this Behavior was registered with. This method will only be called once,
+     * passing in the first Control the Behavior was registered with.
+     * <p/>
+     * Subclasses can implement this method instead of
+     * {@link #preRenderHeadElements(org.apache.click.Control)} if HTML HEAD
+     * elements should only be added to one Control, even if the Behavior is
+     * added to multiple Controls.
+     *
+     * @param source the control the behavior is registered with
+     */
+    protected void addHeadElementsOnce(Control source) {
         // Subclasses can override the default to add head specific elements
         // NOTE: if this method is ever made public the headElementsProcessed
         // check should be done here
