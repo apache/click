ad853cedb92745140eb50fe2bafde0f7b2e2497c
Bob Schellink <sabob@apache.org>
2010-11-04 13:31:20 +0000

    cleanup
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk/click@1030959 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/org/apache/click/control/Select.java
+++ framework/src/org/apache/click/control/Select.java
@@ -20,7 +20,6 @@ package org.apache.click.control;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
