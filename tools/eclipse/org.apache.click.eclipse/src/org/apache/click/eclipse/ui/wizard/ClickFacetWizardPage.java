7430aea3a0661920e77daf0a7868ca0a197bbff4
Naoki Takezoe <takezoe@apache.org>
2011-03-28 10:28:00 +0000

    The root package of page classes does not required in Click 2.3.0.
    git-svn-id: https://svn.apache.org/repos/asf/click/trunk@1086175 13f79535-47bb-0310-9956-ffa450edef68

--- tools/eclipse/org.apache.click.eclipse/src/org/apache/click/eclipse/ui/wizard/ClickFacetWizardPage.java
+++ tools/eclipse/org.apache.click.eclipse/src/org/apache/click/eclipse/ui/wizard/ClickFacetWizardPage.java
@@ -22,8 +22,6 @@ import org.apache.click.eclipse.ClickPlugin;
 import org.apache.click.eclipse.ClickUtils;
 import org.apache.click.eclipse.core.facet.ClickFacetInstallDataModelProvider;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -33,18 +31,18 @@ import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.ui.AbstractFacetWizardPage;
 
 /**
- * 
+ *
  * @author Naoki Takezoe
  * @since 2.0.0
  */
 public class ClickFacetWizardPage extends AbstractFacetWizardPage {
-	
+
 	private IDataModel model;
 	private Text rootPackage;
 	private Button useSpring;
 	private Button useCayenne;
 	private Button usePerformanceFilter;
-	
+
 	public ClickFacetWizardPage() {
 		super("ClickFacetWizardPage");
 		setTitle(ClickPlugin.getString("wizard.facet.title"));
@@ -58,50 +56,50 @@ public class ClickFacetWizardPage extends AbstractFacetWizardPage {
 	public void createControl(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NULL);
 		composite.setLayout(new GridLayout(2, false));
-		
+
 		ClickUtils.createLabel(composite, ClickPlugin.getString("wizard.facet.rootPackage"));
 		rootPackage = new Text(composite, SWT.BORDER);
 		rootPackage.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		rootPackage.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				doValidate();
-			}
-		});
-		
+//		rootPackage.addModifyListener(new ModifyListener() {
+//			public void modifyText(ModifyEvent e) {
+//				doValidate();
+//			}
+//		});
+
 		usePerformanceFilter = new Button(composite, SWT.CHECK);
 		usePerformanceFilter.setText(ClickPlugin.getString("wizard.facet.usePerformanceFilter"));
 		usePerformanceFilter.setLayoutData(ClickUtils.createGridData(2, GridData.FILL_HORIZONTAL));
-		
+
 		useSpring = new Button(composite, SWT.CHECK);
 		useSpring.setText(ClickPlugin.getString("wizard.facet.useSpring"));
 		useSpring.setLayoutData(ClickUtils.createGridData(2, GridData.FILL_HORIZONTAL));
-		
+
 		useCayenne = new Button(composite, SWT.CHECK);
 		useCayenne.setText(ClickPlugin.getString("wizard.facet.useCayenne"));
 		useCayenne.setLayoutData(ClickUtils.createGridData(2, GridData.FILL_HORIZONTAL));
-		
+
 		setControl(composite);
-		doValidate();
-	}
-	
-	private void doValidate(){
-		if(rootPackage.getText().length() == 0){
-			setErrorMessage(ClickPlugin.getString("wizard.facet.error.rootPackage.empty"));
-			setPageComplete(false);
-		} else {
-			setErrorMessage(null);
-			setPageComplete(true);
-		}
+//		doValidate();
 	}
 
+//	private void doValidate(){
+//		if(rootPackage.getText().length() == 0){
+//			setErrorMessage(ClickPlugin.getString("wizard.facet.error.rootPackage.empty"));
+//			setPageComplete(false);
+//		} else {
+//			setErrorMessage(null);
+//			setPageComplete(true);
+//		}
+//	}
+
 	public void transferStateToConfig() {
-		model.setBooleanProperty(ClickFacetInstallDataModelProvider.USE_PERFORMANCE_FILTER, 
+		model.setBooleanProperty(ClickFacetInstallDataModelProvider.USE_PERFORMANCE_FILTER,
 				usePerformanceFilter.getSelection());
-		model.setBooleanProperty(ClickFacetInstallDataModelProvider.USE_SPRING, 
+		model.setBooleanProperty(ClickFacetInstallDataModelProvider.USE_SPRING,
 				useSpring.getSelection());
-		model.setBooleanProperty(ClickFacetInstallDataModelProvider.USE_CAYENNE, 
+		model.setBooleanProperty(ClickFacetInstallDataModelProvider.USE_CAYENNE,
 				useCayenne.getSelection());
-		model.setStringProperty(ClickFacetInstallDataModelProvider.ROOT_PACKAGE, 
+		model.setStringProperty(ClickFacetInstallDataModelProvider.ROOT_PACKAGE,
 				rootPackage.getText());
 	}
 }
